730cafcb465950e4a3e050af26cc0d97e9f2636c
Yusuke Wada <yusuke@kamawada.com>
2021-12-21 17:37:02 +0900

    Middlewares works!

--- src/middleware/filter.js
+++ src/middleware/filter.js
@@ -1,11 +1,13 @@
-const filter = (req, res, next) => {
-  const url = new URL(req.url)
-  req.query = (key) => {
+const filter = (c, next) => {
+  const url = new URL(c.req.url)
+  c.req.query = (key) => {
     return url.searchParams.get(key)
   }
+
   next()
-  if (typeof res === 'string') {
-    res = new Response(res, {
+
+  if (typeof c.res === 'string') {
+    c.res = new Response(c.res, {
       status: 200,
       headers: {
         'Content-Type': 'text/plain',
