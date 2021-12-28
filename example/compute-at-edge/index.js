5330be712a68c94f08a0651d1a5d3e155e371518
Yusuke Wada <yusuke@kamawada.com>
2021-12-28 15:13:37 +0900

    Update Compute@Edge script

--- example/compute-at-edge/index.js
+++ example/compute-at-edge/index.js
@@ -1,8 +1,17 @@
-const Hono = require('../../src/hono')
+const Hono = require('hono')
 const app = Hono()
+
+app.use('*', (c, next) => {
+  console.log(`[${c.req.method}] ${c.req.url}`)
+  next()
+})
 
 app.get('/', () => {
   return new Response('Hono!! Compute@Edge!!')
+})
+
+app.get('/hello/:name', (c) => {
+  return new Response(`Hello ${c.req.params('name')}!!!!!`)
 })
 
 app.fire()
--- example/compute-at-edge/webpack.config.js
+++ example/compute-at-edge/webpack.config.js
@@ -11,4 +11,5 @@ module.exports = {
     path: path.resolve(__dirname, 'bin'),
     libraryTarget: 'this',
   },
+  mode: 'production',
 }
