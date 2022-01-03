bb0cc7f62c12b1162a0773ff735cc05d119755cb
Yusuke Wada <yusuke@kamawada.com>
2022-01-04 07:02:31 +0900

    Use edge-mock instead of node-fetch (#18)

--- src/middleware/logger.test.js
+++ src/middleware/logger.test.js
@@ -1,5 +1,7 @@
-const fetch = require('node-fetch')
+const { makeEdgeEnv } = require('edge-mock')
 const { Hono, Middleware } = require('../hono')
+
+makeEdgeEnv()
 
 describe('Logger by Middleware', () => {
   const app = new Hono()
@@ -10,11 +12,11 @@ describe('Logger by Middleware', () => {
   }
 
   app.use('*', Middleware.logger(logFn))
-  app.get('/', () => new fetch.Response('root'))
+  app.get('/', () => new Response('root'))
 
   it('Log status 200', async () => {
-    const req = new fetch.Request('https://example.com/')
-    const res = await app.dispatch(req, new fetch.Response())
+    const req = new Request('/')
+    const res = await app.dispatch(req)
     expect(res).not.toBeNull()
     expect(res.status).toBe(200)
     expect(log.startsWith('  --> GET / \x1b[32m200\x1b[0m')).toBe(true)
@@ -22,11 +24,11 @@ describe('Logger by Middleware', () => {
 
   it('Log status 404', async () => {
     app.notFound = () => {
-      return new fetch.Response('Default 404 Nout Found', { status: 404 })
+      return new Response('Default 404 Nout Found', { status: 404 })
     }
 
-    const req = new fetch.Request('https://example.com/notfound')
-    const res = await app.dispatch(req, new fetch.Response())
+    const req = new Request('/notfound')
+    const res = await app.dispatch(req)
     expect(res).not.toBeNull()
     expect(res.status).toBe(404)
     expect(log.startsWith('  --> GET /notfound \x1b[33m404\x1b[0m')).toBe(true)
