23ba6f5c67f30c091322060a0291351a152e5fb9
Minghe <h.minghe@gmail.com>
2022-01-04 05:06:49 +0800

    Logger middleware (#17)

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,11 @@
 const defaultFilter = require('./middleware/defaultFilter')
 const poweredBy = require('./middleware/poweredBy')
+const logger = require('./middleware/logger')
 
 function Middleware() {}
 
 Middleware.defaultFilter = defaultFilter
 Middleware.poweredBy = poweredBy
+Middleware.logger = logger
 
 module.exports = Middleware
