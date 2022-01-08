0bfb398ecfa6adf2f466c0d4031f356b9a3894e7
Yusuke Wada <yusuke@kamawada.com>
2022-01-08 13:46:49 +0900

    Feature/html method (#31)
    * Add c.html() method
    
    * Use c.html() method

--- test/context.test.ts
+++ test/context.test.ts
@@ -11,14 +11,23 @@ describe('Context', () => {
   it('c.text', async () => {
     const res = c.text('text in c')
     expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Type')).toBe('text/plain')
     expect(await res.text()).toBe('text in c')
   })
 
   it('c.json', async () => {
     const res = c.json({ message: 'Hello' })
     expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Type')).toMatch('application/json')
     const text = await res.text()
     const object = JSON.parse(text)
     expect(object.message).toBe('Hello')
+  })
+
+  it('c.html', async () => {
+    const res = c.html('<h1>Hello! Hono!</h1>')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Type')).toMatch('text/html')
+    expect(await res.text()).toBe('<h1>Hello! Hono!</h1>')
   })
 })
