94413e98ecad5f46b753fdab027e50aa9f4c00e9
Yusuke Wada <yusuke@kamawada.com>
2022-01-10 18:12:43 +0900

    Add blog CRUD example

---
+++ example/blog/README.md
@@ -0,0 +1,13 @@
+# hono-example-blog
+
+- CRUD
+- Using TypeScript
+- Test with Jest `miniflare environment`
+
+## Endpoints
+
+- `GET /posts`
+- `POST /posts`
+- `GET /posts/:id`
+- `PUT /posts/:id`
+- `DELETE /posts/:id`
---
+++ example/blog/src/model.ts
@@ -0,0 +1,54 @@
+declare global {
+  interface Crypto {
+    randomUUID(): string
+  }
+}
+
+export interface Post {
+  id: string
+  title: string
+  body: string
+}
+
+export type Param = {
+  title: string
+  body: string
+}
+
+const posts: { [key: string]: Post } = {}
+
+export const getPosts = (): Post[] => {
+  return Object.values(posts)
+}
+
+export const getPost = (id: string): Post | undefined => {
+  const post = posts[id]
+  if (post) return post
+}
+
+export const createPost = (param: Param): Post | undefined => {
+  if (!(param.title && param.body)) return
+  const id = crypto.randomUUID()
+  const newPost: Post = { id: id, title: param.title, body: param.body }
+  posts[id] = newPost
+  return newPost
+}
+
+export const updatePost = (id: string, param: Param): boolean => {
+  if (!(param.title && param.body)) return false
+  const post = posts[id]
+  if (post) {
+    post.title = param.title
+    post.body = param.body
+    return true
+  }
+  return false
+}
+
+export const deletePost = (id: string): boolean => {
+  if (posts[id]) {
+    delete posts[id]
+    return true
+  }
+  return false
+}
