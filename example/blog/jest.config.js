511c0ebd7e63706d748a2950e016c1e542c8103c
Yusuke Wada <yusuke@kamawada.com>
2022-01-10 23:30:19 +0900

    Feature/example blog (#39)
    * Add blog CRUD example
    
    * Fixed some
    
    * Exclude example test

--- example/blog/jest.config.js
+++ example/blog/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
+  testEnvironment: 'miniflare', // ✨
   testMatch: ['**/test/**/*.+(ts|tsx|js)', '**/src/**/(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
-  testPathIgnorePatterns: ['./example'],
 }
---
+++ example/blog/src/controller.ts
@@ -0,0 +1,51 @@
+import type { Handler } from '../../../dist'
+import * as Model from './model'
+
+export const root: Handler = (c) => {
+  return c.json({ message: 'Hello' })
+}
+
+export const list: Handler = (c) => {
+  const posts = Model.getPosts()
+  return c.json({ posts: posts, ok: true })
+}
+
+export const create: Handler = async (c) => {
+  const param = (await c.req.json()) as Model.Param
+  const newPost = Model.createPost(param)
+  if (!newPost) {
+    // Is 200 suitable?
+    return c.json({ error: 'Can not create new post', ok: false }, 200)
+  }
+  return c.json({ post: newPost, ok: true }, 201)
+}
+
+export const show: Handler = async (c) => {
+  const id = c.req.params('id')
+  const post = Model.getPost(id)
+  if (!post) {
+    return c.json({ error: 'Not Found', ok: false }, 404)
+  }
+  return c.json({ post: post, ok: true })
+}
+
+export const update: Handler = async (c) => {
+  const id = c.req.params('id')
+  if (!Model.getPost(id)) {
+    // 204 No Content
+    return c.json({ ok: false }, 204)
+  }
+  const param = (await c.req.json()) as Model.Param
+  const success = Model.updatePost(id, param)
+  return c.json({ ok: success })
+}
+
+export const destroy: Handler = async (c) => {
+  const id = c.req.params('id')
+  if (!Model.getPost(id)) {
+    // 204 No Content
+    return c.json({ ok: false }, 204)
+  }
+  const success = Model.deletePost(id)
+  return c.json({ ok: success })
+}
