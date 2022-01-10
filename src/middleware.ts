fa078aa7a73e3f1afcc6d75bb5fb2131e8a5b6b0
Minghe <h.minghe@gmail.com>
2022-01-10 10:17:52 +0800

    feat(body-parse): a body parse middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,12 @@ import { defaultMiddleware } from './middleware/default'
 import { poweredBy } from './middleware/powered-by/powered-by'
 import { logger } from './middleware/logger/logger'
 import { basicAuth } from './middleware/basic-auth/basic-auth'
+import { bodyParse } from './middleware/body-parse/body-parse'
 
 export class Middleware {
   static default = defaultMiddleware
   static poweredBy = poweredBy
   static logger = logger
   static basicAuth = basicAuth
+  static bodyParse = bodyParse
 }
