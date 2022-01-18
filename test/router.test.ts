b6c3eef686b17f585ba6a7035d3f3e5d57ba0292
Yusuke Wada <yusuke@kamawada.com>
2022-01-19 01:44:21 +0900

    feat: default route with wildcard (#52)
    * Default wildcard route
    
    * Update example
    
    * Add test

--- test/router.test.ts
+++ test/router.test.ts
@@ -41,6 +41,17 @@ describe('Complex', () => {
     expect(res.handler).toBe('get wildcard')
   })
 
+  it('Default', async () => {
+    router.add('GET', '/api/abc', 'get api')
+    router.add('GET', '/api/*', 'fallback')
+    let res = router.match('GET', '/api/abc')
+    expect(res).not.toBeNull()
+    expect(res.handler).toBe('get api')
+    res = router.match('GET', '/api/def')
+    expect(res).not.toBeNull()
+    expect(res.handler).toBe('fallback')
+  })
+
   it('Regexp', async () => {
     router.add('GET', '/post/:date{[0-9]+}/:title{[a-z]+}', 'get post')
     let res = router.match('GET', '/post/20210101/hello')
