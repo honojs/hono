f1c6ef07402532b17bffe62771ce84c6944cf5d4
Yusuke Wada <yusuke@kamawada.com>
2022-01-19 09:33:27 +0900

    feat: CORS  middleware (#53)
    * Update blog example
    
    * Implemented
    
    * Fixed blog example

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { poweredBy } from './middleware/powered-by/powered-by'
 import { logger } from './middleware/logger/logger'
 import { basicAuth } from './middleware/basic-auth/basic-auth'
 import { bodyParse } from './middleware/body-parse/body-parse'
+import { cors } from './middleware/cors/cors'
 
 export class Middleware {
   static default = defaultMiddleware
@@ -10,4 +11,5 @@ export class Middleware {
   static logger = logger
   static basicAuth = basicAuth
   static bodyParse = bodyParse
+  static cors = cors
 }
---
+++ src/middleware/cors/cors.ts
@@ -0,0 +1,77 @@
+import type { Context } from '../../context'
+
+type CORSOptions = {
+  origin: string
+  allowMethods?: string[]
+  allowHeaders?: string[]
+  maxAge?: number
+  credentials?: boolean
+  exposeHeaders?: string[]
+}
+
+export const cors = (options?: CORSOptions) => {
+  const defaults: CORSOptions = {
+    origin: '*',
+    allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH'],
+    allowHeaders: [],
+    exposeHeaders: [],
+  }
+  const opts = {
+    ...defaults,
+    ...options,
+  }
+
+  return async (c: Context, next: Function) => {
+    await next()
+
+    function set(key: string, value: string) {
+      c.res.headers.append(key, value)
+    }
+
+    set('Access-Control-Allow-Origin', opts.origin)
+
+    // Suppose the server sends a response with an Access-Control-Allow-Origin value with an explicit origin (rather than the "*" wildcard).
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+    if (opts.origin !== '*') {
+      set('Vary', 'Origin')
+    }
+
+    if (opts.credentials) {
+      set('Access-Control-Allow-Credentials', 'true')
+    }
+
+    if (opts.exposeHeaders.length) {
+      set('Access-Control-Expose-Headers', opts.exposeHeaders.join(','))
+    }
+
+    if (c.req.method === 'OPTIONS') {
+      // Preflight
+
+      if (opts.maxAge != null) {
+        set('Access-Control-Max-Age', opts.maxAge.toString())
+      }
+
+      if (opts.allowMethods.length) {
+        set('Access-Control-Allow-Methods', opts.allowMethods.join(','))
+      }
+
+      let headers = opts.allowHeaders
+      if (!headers.length) {
+        const requestHeaders = c.req.headers.get('Access-Control-Request-Headers')
+        if (requestHeaders) {
+          headers = requestHeaders.split(/\s*,\s*/)
+        }
+      }
+      if (headers.length) {
+        set('Access-Control-Allow-Headers', headers.join(','))
+        set('Vary', 'Access-Control-Request-Headers')
+      }
+
+      c.res = new Response(null, {
+        headers: c.res.headers,
+        status: 204,
+        statusText: c.res.statusText,
+      })
+    }
+  }
+}
