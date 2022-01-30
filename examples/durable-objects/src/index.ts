1ffafeb328bb63f1ca5272da4f334eef9bb20edf
Yusuke Wada <yusuke@kamawada.com>
2022-01-30 11:07:04 +0900

    Update durable object example

--- examples/durable-objects/src/index.ts
+++ examples/durable-objects/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from '../../../src/index'
-import type  { Env } from '../../../src/index'
 
 export { Counter } from './counter'
 
@@ -11,12 +10,12 @@ declare module '../../../src/index' {
 
 const app = new Hono()
 
-app.get('*', async c => {
+app.get('*', async (c) => {
   const id = c.env.COUNTER.idFromName('A')
   const obj = c.env.COUNTER.get(id)
   const resp = await obj.fetch(c.req.url)
 
-  if(resp.status === 404) {
+  if (resp.status === 404) {
     return c.text('404 Not Found', 404)
   }
 
@@ -24,8 +23,4 @@ app.get('*', async c => {
   return c.text(`Count is ${count}`)
 })
 
-export default {
-  fetch(request: Request, env: Env, event: FetchEvent) {
-    return app.fetch(request, env, event)
-  },
-}
+export default app
