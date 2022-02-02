8f48292166653c8320e80478f260ad1f9ebff4da
Yusuke Wada <yusuke@kamawada.com>
2022-02-03 08:23:41 +0900

    Add negative tests

--- src/utils/buffer.test.ts
+++ src/utils/buffer.test.ts
@@ -12,11 +12,15 @@ describe('buffer', () => {
   it('decodeBaes64', async () => {
     expect(await decodeBase64('aG9vb29vb29vbw==')).toBe('hooooooooo')
     expect(await decodeBase64('54KO')).toBe('炎')
+
+    expect(await decodeBase64('abcdedf')).not.toBe('abcdef')
   })
 
   it('sha256', async () => {
     expect(await sha256('hono')).toBe('8b3dc17add91b7e8f0b5109a389927d66001139cd9b03fa7b95f83126e1b2b23')
     expect(await sha256('炎')).toBe('1fddc5a562ee1fbeb4fc6def7d4be4911fcdae4273b02ae3a507b170ba0ea169')
+
+    expect(await sha256('abcdedf')).not.toBe('abcdef')
   })
 
   it('positive', async () => {
