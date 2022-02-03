e1e8130c11578d5b80a21106c9a45035138ece52
Yusuke Wada <yusuke@kamawada.com>
2022-02-04 08:01:12 +0900

    feat: add root option on mustache middleware (#89)

--- src/middleware/mustache/mustache.test.ts
+++ src/middleware/mustache/mustache.test.ts
@@ -6,11 +6,13 @@ const store: { [key: string]: string } = {
   'index.abcdef.mustache': '{{> header}}Title: {{ title }}{{> footer}}',
   'header.abcdef.mustache': '<html><body>',
   'footer.abcdef.mustache': '</body></html>',
+  'view/index.abcdef.mustache': '<h1>With Root</h1>',
 }
 const manifest = JSON.stringify({
   'index.mustache': 'index.abcdef.mustache',
   'header.mustache': 'header.abcdef.mustache',
   'footer.mustache': 'footer.abcdef.mustache',
+  'view/index.mustache': 'view/index.abcdef.mustache',
 })
 
 Object.assign(global, { __STATIC_CONTENT_MANIFEST: manifest })
@@ -25,8 +27,8 @@ Object.assign(global, {
 describe('Mustache by Middleware', () => {
   const app = new Hono()
 
-  app.use('*', mustache())
-  app.get('/', (c) => {
+  app.use('/foo', mustache())
+  app.get('/foo', (c) => {
     return c.render(
       'index',
       { title: 'Hono!' },
@@ -37,10 +39,22 @@ describe('Mustache by Middleware', () => {
     )
   })
 
+  app.use('/bar', mustache({ root: 'view' }))
+  app.get('/bar', (c) => {
+    return c.render('index')
+  })
+
   it('Mustache template redering', async () => {
-    const req = new Request('http://localhost/')
+    const req = new Request('http://localhost/foo')
     const res = await app.dispatch(req)
     expect(res.status).toBe(200)
     expect(await res.text()).toBe('<html><body>Title: Hono!</body></html>')
+  })
+
+  it('Mustache template redering with root', async () => {
+    const req = new Request('http://localhost/bar')
+    const res = await app.dispatch(req)
+    expect(res.status).toBe(200)
+    expect(await res.text()).toBe('<h1>With Root</h1>')
   })
 })
--- src/utils/cloudflare.ts
+++ src/utils/cloudflare.ts
@@ -1,3 +1,5 @@
+import { getMimeType } from './mime'
+
 declare const __STATIC_CONTENT: KVNamespace, __STATIC_CONTENT_MANIFEST: string
 
 export const getContentFromKVAsset = async (path: string): Promise<ArrayBuffer> => {
@@ -21,4 +23,36 @@ export const getContentFromKVAsset = async (path: string): Promise<ArrayBuffer> 
     content = content as ArrayBuffer
   }
   return content
+}
+
+type Options = {
+  filename: string
+  root?: string
+  defaultDocument?: string
+}
+
+export const getKVFilePath = (opt: Options): string => {
+  let filename = opt.filename
+  let root = opt.root || ''
+  const defaultDocument = opt.defaultDocument || 'index.html'
+
+  if (filename.endsWith('/')) {
+    // /top/ => /top/index.html
+    filename = filename.concat(defaultDocument)
+  } else if (!filename.match(/\.[a-zA-Z0-9]+$/)) {
+    // /top => /top/index.html
+    filename = filename.concat('/' + defaultDocument)
+  }
+
+  // /foo.html => foo.html
+  filename = filename.replace(/^\//, '')
+
+  // assets/ => assets
+  root = root.replace(/\/$/, '')
+
+  // ./assets/foo.html => assets/foo.html
+  let path = root ? root + '/' + filename : filename
+  path = path.replace(/^\.?\//, '')
+
+  return path
 }
