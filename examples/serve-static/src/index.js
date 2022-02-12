8eab634662a2911fa47282138613d647159291e8
Yusuke Wada <yusuke@kamawada.com>
2022-02-12 14:18:42 +0900

    chore: fixed serve-static example

--- examples/serve-static/src/index.js
+++ examples/serve-static/src/index.js
@@ -1,9 +1,9 @@
-import { Hono } from '../../../dist'
-import { serveStatic } from '../../../src/middleware/serve-static/serve-static'
+import { Hono } from 'hono'
+import { serveStatic } from 'hono/serve-static'
 
 const hono = new Hono()
 
-hono.use('/static/*', serveStatic({ root: './assets' }))
+hono.use('/static/*', serveStatic({ root: './' }))
 hono.get('/', (c) => c.text('This is Home! You can access: /static/hello.txt'))
 
 hono.fire()
