7a60c6da7d7efbd81f34874dac6282b661a346e7
Yusuke Wada <yusuke@kamawada.com>
2022-02-26 22:26:15 +0900

    feat: etag middleware (#119)

--- src/middleware/body-parse/body-parse.ts
+++ src/middleware/body-parse/body-parse.ts
@@ -1,24 +1,9 @@
+import { parseBody } from '../../utils/body'
 import type { Context } from '../../context'
 
 export const bodyParse = () => {
   return async (ctx: Context, next: Function) => {
-    const contentType = ctx.req.headers.get('Content-Type') || ''
-
-    if (contentType.includes('application/json')) {
-      ctx.req.parsedBody = await ctx.req.json()
-    } else if (contentType.includes('application/text')) {
-      ctx.req.parsedBody = await ctx.req.text()
-    } else if (contentType.includes('text/html')) {
-      ctx.req.parsedBody = await ctx.req.text()
-    } else if (contentType.includes('form')) {
-      const form: Record<string, string | File> = {}
-      const data = [...(await ctx.req.formData())].reduce((acc, cur) => {
-        acc[cur[0]] = cur[1]
-        return acc
-      }, form)
-      ctx.req.parsedBody = data
-    }
-
+    ctx.req.parsedBody = await parseBody(ctx.req)
     await next()
   }
 }
