b206ab71c1bf78eef86db202faa149c0650c5ded
Yusuke Wada <yusuke@kamawada.com>
2022-03-09 13:43:07 +0900

    chore: update example for fastly compute@edge

---
+++ examples/compute-at-edge/src/index.ts
@@ -0,0 +1,22 @@
+import { Hono } from 'hono'
+import { basicAuth } from 'hono/basic-auth'
+import { SHA256 } from 'crypto-js'
+
+const app = new Hono()
+
+app.use(
+  '/auth/*',
+  basicAuth({
+    username: 'compute',
+    password: 'edge',
+    hashFunction: (m: string) => SHA256(m).toString(),
+  })
+)
+
+app.get('/', (c) => c.text('Hello! Compute@Edge!'))
+app.get('/auth/*', (c) => c.text('You are authorized!'))
+app.get('/hello/:name', (c) => {
+  return c.text(`Hello ${c.req.param('name')}!!!!!`)
+})
+
+app.fire()
