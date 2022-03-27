a46d3597f150664c9a6bd53ae0aacc2587ffefe7
Yusuke Wada <yusuke@kamawada.com>
2022-03-27 20:27:52 +0900

    feat: add GraphQL server middleware (#142)

---
+++ src/middleware/graphql-server/graphql-server.ts
@@ -0,0 +1,210 @@
+// Based on the code in the `express-graphql` package.
+// https://github.com/graphql/express-graphql/blob/main/src/index.ts
+
+import { parseBody } from './parse-body'
+import type { Context } from '../../context'
+
+import {
+  Source,
+  parse,
+  execute,
+  validateSchema,
+  validate,
+  specifiedRules,
+  getOperationAST,
+} from 'graphql'
+
+import type { GraphQLSchema, DocumentNode, ValidationRule, FormattedExecutionResult } from 'graphql'
+
+type Options = {
+  schema: GraphQLSchema
+  rootValue?: unknown
+  pretty?: boolean
+  validationRules?: ReadonlyArray<ValidationRule>
+  // graphiql?: boolean
+}
+
+export const graphqlServer = (options: Options) => {
+  const schema = options.schema
+  const rootValue = options.rootValue
+  const pretty = options.pretty ?? false
+  const validationRules = options.validationRules ?? []
+  // const showGraphiQL = options.graphiql ?? false
+
+  return async (c: Context, next: Function) => {
+    await next()
+
+    // GraphQL HTTP only supports GET and POST methods.
+    if (c.req.method !== 'GET' && c.req.method !== 'POST') {
+      c.res = c.json(errorMessages(['GraphQL only supports GET and POST requests.']), 405, {
+        Allow: 'GET, POST',
+      })
+      return
+    }
+
+    let params: GraphQLParams
+    try {
+      params = await getGraphQLParams(c.req)
+    } catch (e) {
+      if (e instanceof Error) {
+        c.res = c.json(errorMessages([e.message]), 400)
+      }
+      return
+    }
+
+    const { query, variables, operationName } = params
+
+    if (query == null) {
+      c.res = c.json(errorMessages(['Must provide query string.']), 400)
+      return
+    }
+
+    const schemaValidationErrors = validateSchema(schema)
+    if (schemaValidationErrors.length > 0) {
+      // Return 500: Internal Server Error if invalid schema.
+      c.res = c.json(errorMessages(['GraphQL schema validation error.']), 500)
+      return
+    }
+
+    let documentAST: DocumentNode
+    try {
+      documentAST = parse(new Source(query, 'GraphQL request'))
+    } catch (syntaxError: unknown) {
+      // Return 400: Bad Request if any syntax errors errors exist.
+      c.res = c.json(errorMessages(['GraphQL syntax error.']), 400)
+      return
+    }
+
+    // Validate AST, reporting any errors.
+    const validationErrors = validate(schema, documentAST, [...specifiedRules, ...validationRules])
+
+    if (validationErrors.length > 0) {
+      // Return 400: Bad Request if any validation errors exist.
+      c.res = c.json(errorMessages(['GraphQL validation error.']), 400)
+      return
+    }
+
+    if (c.req.method === 'GET') {
+      // Determine if this GET request will perform a non-query.
+      const operationAST = getOperationAST(documentAST, operationName)
+      if (operationAST && operationAST.operation !== 'query') {
+        /*
+        Now , does not support GraphiQL
+        if (showGraphiQL) {
+          //return respondWithGraphiQL(response, graphiqlOptions, params)
+        }
+        */
+
+        // Otherwise, report a 405: Method Not Allowed error.
+        c.res = c.json(
+          errorMessages([
+            `Can only perform a ${operationAST.operation} operation from a POST request.`,
+          ]),
+          405,
+          { Allow: 'POST' }
+        )
+        return
+      }
+    }
+
+    let result: FormattedExecutionResult
+
+    try {
+      result = await execute({
+        schema,
+        document: documentAST,
+        rootValue,
+        variableValues: variables,
+        operationName: operationName,
+      })
+    } catch (error: unknown) {
+      // Return 400: Bad Request if any execution context errors exist.
+      c.res = c.json(errorMessages(['GraphQL execution context error.']), 400)
+      return
+    }
+
+    if (result.data == null) {
+      c.res = c.json(errorMessages([result.errors.toString()]), 500)
+      return
+    }
+
+    /*
+    Now, does not support GraphiQL
+    if (showGraphiQL) {
+    }
+    */
+
+    if (pretty) {
+      const payload = JSON.stringify(result, null, pretty ? 2 : 0)
+      c.res = c.text(payload, 200, {
+        'Content-Type': 'application/json',
+      })
+    } else {
+      c.res = c.json(result)
+    }
+    return
+  }
+}
+
+export interface GraphQLParams {
+  query: string | null
+  variables: { readonly [name: string]: unknown } | null
+  operationName: string | null
+  raw: boolean
+}
+
+export const getGraphQLParams = async (request: Request): Promise<GraphQLParams> => {
+  const urlData = new URLSearchParams(request.url.split('?')[1])
+  const bodyData = await parseBody(request)
+
+  // GraphQL Query string.
+  let query = urlData.get('query') ?? (bodyData.query as string | null)
+
+  if (typeof query !== 'string') {
+    query = null
+  }
+
+  // Parse the variables if needed.
+  let variables = (urlData.get('variables') ?? bodyData.variables) as {
+    readonly [name: string]: unknown
+  } | null
+  if (typeof variables === 'string') {
+    try {
+      variables = JSON.parse(variables)
+    } catch {
+      throw Error('Variables are invalid JSON.')
+    }
+  } else if (typeof variables !== 'object') {
+    variables = null
+  }
+
+  // Name of GraphQL operation to execute.
+  let operationName = urlData.get('operationName') ?? (bodyData.operationName as string | null)
+  if (typeof operationName !== 'string') {
+    operationName = null
+  }
+
+  const raw = urlData.get('raw') != null || bodyData.raw !== undefined
+
+  const params: GraphQLParams = {
+    query: query,
+    variables: variables,
+    operationName: operationName,
+    raw: raw,
+  }
+
+  return params
+}
+
+export const errorMessages = (messages: string[]) => {
+  const errors = messages.map((message) => {
+    return {
+      message: message,
+    }
+  })
+  return {
+    errors: errors,
+  }
+}
+
+export const graphiQLResponse = () => {}
---
+++ src/middleware/graphql-server/parse-body.ts
@@ -0,0 +1,26 @@
+export async function parseBody(req: Request): Promise<{ [param: string]: unknown }> {
+  const contentType = req.headers.get('content-type')
+
+  switch (contentType) {
+    case 'application/graphql':
+      return { query: await req.text() }
+    case 'application/json':
+      try {
+        return await req.json()
+      } catch (e) {
+        throw Error(`POST body sent invalid JSON: ${e}`)
+      }
+    case 'application/x-www-form-urlencoded':
+      return parseFormURL(req)
+  }
+
+  return {}
+}
+
+const parseFormURL = async (req: Request) => {
+  const text = await req.text()
+  const searchParams = new URLSearchParams(text)
+  const res: { [params: string]: string } = {}
+  searchParams.forEach((v, k) => (res[k] = v))
+  return res
+}
