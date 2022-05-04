c476b95b2f293dbf4bfa158a5663321db6bb2642
Minghe <h.minghe@gmail.com>
2022-05-04 20:53:13 +0800

    feat(compose): compose both the ordinary middlewares and response (#195)
    middlewares

--- src/compose.ts
+++ src/compose.ts
@@ -3,18 +3,18 @@ import type { ErrorHandler } from '@/hono'
 
 // Based on the code in the MIT licensed `koa-compose` package.
 export const compose = <C>(middleware: Function[], onError?: ErrorHandler) => {
-  return function (context: C) {
+  return function (context: C, next?: Function) {
     let index = -1
     return dispatch(0)
     async function dispatch(i: number): Promise<C> {
-      if (i === middleware.length) {
-        return context
-      }
       if (i <= index) {
         return Promise.reject(new Error('next() called multiple times'))
       }
-      const handler = middleware[i]
+      let handler = middleware[i]
       index = i
+      if (i === middleware.length) handler = next
+      if (!handler) return Promise.resolve(context)
+
       return Promise.resolve(handler(context, dispatch.bind(null, i + 1)))
         .then(() => {
           return context
