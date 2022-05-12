4dfd18026d389902da52b228ea4651638eae5893
Taku Amano <taku@taaas.jp>
2022-05-12 15:09:33 +0900

    feat: multiple matching routes for RegExpRouter (#220)
    * feat: multi match router for RegExpRouter.
    
    * Separate routers into primary and secondary.
    
    * Add RegExpRouter to the build target, again.
    
    Because RegExpRouter has caught up with develop.
    
    * Match a fixed string.
    
    * refactor: Rename member name from "paramAlias" to "paramAliasMap"

---
+++ src/router/reg-exp-router/router.test.ts
@@ -0,0 +1,153 @@
+import { METHOD_NAME_ALL } from '@/router'
+import { RegExpRouter } from '@/router/reg-exp-router/router'
+
+describe('Basic Usage', () => {
+  const router = new RegExpRouter<string>()
+
+  router.add('GET', '/hello', 'get hello')
+  router.add('POST', '/hello', 'post hello')
+
+  it('get, post hello', async () => {
+    let res = router.match('GET', '/hello')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['get hello'])
+
+    res = router.match('POST', '/hello')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['post hello'])
+
+    res = router.match('PUT', '/hello')
+    expect(res).toBeNull()
+
+    res = router.match('GET', '/')
+    expect(res).toBeNull()
+  })
+})
+
+describe('Complex', () => {
+  let router: RegExpRouter<string>
+  beforeEach(() => {
+    router = new RegExpRouter<string>()
+  })
+
+  it('Named Param', async () => {
+    router.add('GET', '/entry/:id', 'get entry')
+    const res = router.match('GET', '/entry/123')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['get entry'])
+    expect(res.params['id']).toBe('123')
+  })
+
+  it('Wildcard', async () => {
+    router.add('GET', '/wild/*/card', 'get wildcard')
+    const res = router.match('GET', '/wild/xxx/card')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['get wildcard'])
+  })
+
+  it('Default', async () => {
+    router.add('GET', '/api/abc', 'get api')
+    router.add('GET', '/api/*', 'fallback')
+    let res = router.match('GET', '/api/abc')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['fallback', 'get api'])
+    res = router.match('GET', '/api/def')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['fallback'])
+  })
+
+  it('Regexp', async () => {
+    router.add('GET', '/post/:date{[0-9]+}/:title{[a-z]+}', 'get post')
+    let res = router.match('GET', '/post/20210101/hello')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['get post'])
+    expect(res.params['date']).toBe('20210101')
+    expect(res.params['title']).toBe('hello')
+    res = router.match('GET', '/post/onetwothree')
+    expect(res).toBeNull()
+    res = router.match('GET', '/post/123/123')
+    expect(res).toBeNull()
+  })
+})
+
+describe('Optimization for METHOD_NAME_OF_ALL', () => {
+  let router: RegExpRouter<string>
+  beforeEach(() => {
+    router = new RegExpRouter<string>()
+  })
+
+  it('Apply to all requests', async () => {
+    router.add(METHOD_NAME_ALL, '*', 'OK')
+    const res = router.match('GET', '/entry/123')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['OK'])
+    expect(res.params).toMatchObject({})
+  })
+
+  it('Apply to all requests under a specific path', async () => {
+    router.add(METHOD_NAME_ALL, '/path/to/*', 'OK')
+    let res = router.match('GET', '/entry/123')
+    expect(res).toBeNull()
+
+    res = router.match('GET', '/path/to/entry/123')
+    expect(res).not.toBeNull()
+    expect(res.handlers).toEqual(['OK'])
+    expect(res.params).toMatchObject({})
+  })
+})
+
+describe('Multi match', () => {
+  describe('Blog', () => {
+    const router = new RegExpRouter<string>()
+
+    router.add('ALL', '*', 'middleware a')
+    router.add('GET', '*', 'middleware b')
+    router.add('GET', '/entry', 'get entries')
+    router.add('POST', '/entry/*', 'middleware c')
+    router.add('POST', '/entry', 'post entry')
+    router.add('GET', '/entry/:id', 'get entry')
+    router.add('GET', '/entry/:id/comment/:comment_id', 'get comment')
+    it('GET /', async () => {
+      const res = router.match('GET', '/')
+      expect(res).not.toBeNull()
+      expect(res.handlers).toEqual(['middleware a', 'middleware b'])
+    })
+    it('GET /entry/123', async () => {
+      const res = router.match('GET', '/entry/123')
+      expect(res).not.toBeNull()
+      expect(res.handlers).toEqual(['middleware a', 'middleware b', 'get entry'])
+      expect(res.params['id']).toBe('123')
+    })
+    it('GET /entry/123/comment/456', async () => {
+      const res = router.match('GET', '/entry/123/comment/456')
+      expect(res).not.toBeNull()
+      expect(res.handlers).toEqual(['middleware a', 'middleware b', 'get comment'])
+      expect(res.params['id']).toBe('123')
+      expect(res.params['comment_id']).toBe('456')
+    })
+    it('POST /entry', async () => {
+      const res = router.match('POST', '/entry')
+      expect(res).not.toBeNull()
+      expect(res.handlers).toEqual(['middleware a', 'middleware c', 'post entry'])
+    })
+    it('DELETE /entry', async () => {
+      const res = router.match('DELETE', '/entry')
+      expect(res).not.toBeNull()
+      expect(res.handlers).toEqual(['middleware a'])
+    })
+  })
+
+  describe('Ambiguous', () => {
+    const router = new RegExpRouter<string>()
+
+    router.add('GET', '/:user/entries', 'get user entries')
+    router.add('GET', '/entry/:name', 'get entry')
+    it('GET /entry/entry', async () => {
+      const res = router.match('GET', '/entry/entries')
+      expect(res).not.toBeNull()
+      expect(res.handlers).toEqual(['get user entries', 'get entry'])
+      expect(res.params['user']).toBe('entry')
+      expect(res.params['name']).toBe('entries')
+    })
+  })
+})
