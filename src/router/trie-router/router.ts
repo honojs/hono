5ac53e6f16b89d44252c93393bbac0eba9a60318
Taku Amano <taku@taaas.jp>
2022-05-26 07:39:43 +0900

    refactor(router): Make Router an interface, not a class (#268)
    * Allow initialization with router objects instead of routerClass.
    
    * Make Router an interface, not a class.
    
    * Make Result an interface, not a class.
    
    * Apply getSortedHandlers for result.
    
    Co-authored-by: Yusuke Wada <yusuke@kamawada.com>

--- src/router/trie-router/router.ts
+++ src/router/trie-router/router.ts
@@ -1,12 +1,10 @@
-import { Router } from '../../router'
-import type { Result } from '../../router'
+import type { Result, Router } from '../../router'
 import { Node } from './node'
 
-export class TrieRouter<T> extends Router<T> {
+export class TrieRouter<T> implements Router<T> {
   node: Node<T>
 
   constructor() {
-    super()
     this.node = new Node()
   }
 
