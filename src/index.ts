1209364c8a1b4871a94a97fedac6cd7fb4d0ea00
Taku Amano <taku@taaas.jp>
2022-06-04 17:50:39 +0900

    fix: Import "declare global" for the Request interface explicitly.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./request.ts" /> Import "declare global" for the Request interface.
+
 export { Hono } from './hono'
 export type { Handler, Next } from './hono'
 export { Context } from './context'
