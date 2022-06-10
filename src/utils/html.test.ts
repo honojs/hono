38c53524ddc089d3188672731d6d8e4f1d8ae24b
Taku Amano <taku@taaas.jp>
2022-06-10 22:04:07 +0900

    feat(utils/html): Introduce `html` template literals tag.

--- src/utils/html.test.ts
+++ src/utils/html.test.ts
@@ -1,8 +1,15 @@
-import { escape } from './html'
+import { escape, html } from './html'
 
 describe('HTML escape', () => {
   it('Should escape special characters', () => {
     expect(escape('I <b>think</b> this is good.')).toBe('I &lt;b&gt;think&lt;/b&gt; this is good.')
     expect(escape('John "Johnny" Smith')).toBe('John &quot;Johnny&quot; Smith')
+  })
+})
+
+describe('Tagged Template Literals', () => {
+  it('Should escape special characters', () => {
+    const name = 'John "Johnny" Smith'
+    expect(html`<p>I'm ${name}.</p>`.toString()).toBe('<p>I\'m John &quot;Johnny&quot; Smith.</p>')
   })
 })
--- src/utils/html.ts
+++ src/utils/html.ts
@@ -11,3 +11,26 @@ const replaceFn = (m: string) => entityMap[m]
 export const escape = (str: string): string => {
   return str.replace(escapeRe, replaceFn)
 }
+
+export const html = (strings: TemplateStringsArray, ...values: any[]): HtmlEscapedString => {
+  let result = ''
+
+  for (let i = 0, len = strings.length - 1; i < len; i++) {
+    result += strings[i]
+
+    const value = values[i]
+    if (typeof value === 'boolean' || value === null || value === undefined) {
+      continue
+    } else if (typeof value === 'object' && (value as any).isEscaped) {
+      result += value
+    } else {
+      result += escape(value.toString())
+    }
+  }
+  result += strings[strings.length - 1]
+
+  const escapedString = new String(result) as HtmlEscapedString
+  escapedString.isEscaped = true
+
+  return escapedString
+}
