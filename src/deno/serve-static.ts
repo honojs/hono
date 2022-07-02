b1bdfb43ecaade36a82c7c843353199e2de6cbf1
Yusuke Wada <yusuke@kamawada.com>
2022-07-02 23:01:46 +0900

    feat: add `serve-static` middlware for deno

---
+++ src/deno/serve-static.ts
@@ -0,0 +1,73 @@
+import type { Context } from '../context'
+import type { Next } from '../hono'
+import { getMimeType } from '../utils/mime'
+
+export type ServeStaticOptions = {
+  root?: string
+  path?: string
+}
+
+const DEFAULT_DOCUMENT = 'index.html'
+
+export const serveStatic = (options: ServeStaticOptions = { root: '' }) => {
+  return async (c: Context, next: Next): Promise<Response | undefined> => {
+    // Do nothing if Response is already set
+    if (c.res && c.finalized) {
+      await next()
+    }
+
+    const url = new URL(c.req.url)
+
+    let path = getFilePath({
+      filename: options.path ?? url.pathname,
+      root: options.root,
+      defaultDocument: DEFAULT_DOCUMENT,
+    })
+
+    path = `./${path}`
+    const content = await Deno.readFile(path)
+    if (content) {
+      const mimeType = getMimeType(path)
+      if (mimeType) {
+        c.header('Content-Type', mimeType)
+      }
+      // Return Response object
+      return c.body(content)
+    } else {
+      console.warn(`Static file: ${path} is not found`)
+      await next()
+    }
+    return
+  }
+}
+
+type FilePathOptions = {
+  filename: string
+  root?: string
+  defaultDocument?: string
+}
+
+export const getFilePath = (options: FilePathOptions): string => {
+  let filename = options.filename
+  let root = options.root || ''
+  const defaultDocument = options.defaultDocument || 'index.html'
+
+  if (filename.endsWith('/')) {
+    // /top/ => /top/index.html
+    filename = filename.concat(defaultDocument)
+  } else if (!filename.match(/\.[a-zA-Z0-9]+$/)) {
+    // /top => /top/index.html
+    filename = filename.concat('/' + defaultDocument)
+  }
+
+  // /foo.html => foo.html
+  filename = filename.replace(/^\.?\//, '')
+
+  // assets/ => assets
+  root = root.replace(/\/$/, '')
+
+  let path = root ? root + '/' + filename : filename
+  path = path.replace(/^\.?\//, '')
+
+  return path
+}
