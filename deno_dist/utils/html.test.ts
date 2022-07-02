a438d76a0ae8da336d51d4c37c786b91067bc83c
Yusuke Wada <yusuke@kamawada.com>
2022-07-02 22:31:10 +0900

    chore: rm `*.ts.test` from deno_dist (#338)

--- deno_dist/utils/html.test.ts
+++
@@ -1,8 +0,0 @@
-import { escape } from './html.ts'
-
-describe('HTML escape', () => {
-  it('Should escape special characters', () => {
-    expect(escape('I <b>think</b> this is good.')).toBe('I &lt;b&gt;think&lt;/b&gt; this is good.')
-    expect(escape('John "Johnny" Smith')).toBe('John &quot;Johnny&quot; Smith')
-  })
-})
