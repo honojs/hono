52b4fd32d88d5b65da1aa1832a8bee92b69c0a83
Yusuke Wada <yusuke@kamawada.com>
2022-07-13 09:50:31 +0900

    feat(bun): add "serve-static" middleware (#370)
    Close ##369 #368

---
+++ src/middleware/serve-static/bun.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import type { Context } from '../../context'
+import type { Next } from '../../hono'
+import { getFilePath } from '../../utils/filepath'
+import { getMimeType } from '../../utils/mime'
+
+// @ts-ignore
+const { file } = Bun
+
+export type ServeStaticOptions = {
+  root?: string
+  path?: string
+}
+
+const DEFAULT_DOCUMENT = 'index.html'
+
+export const serveStatic = (options: ServeStaticOptions = { root: '' }) => {
+  return async (c: Context, next: Next): Promise<Response | undefined> => {
+    // Do nothing if Response is already set
+    if (c.res && c.finalized) {
+      await next()
+    }
+
+    const url = new URL(c.req.url)
+
+    let path = getFilePath({
+      filename: options.path ?? url.pathname,
+      root: options.root,
+      defaultDocument: DEFAULT_DOCUMENT,
+    })
+
+    path = `./${path}`
+    const content = file(path)
+    if (content) {
+      const mimeType = getMimeType(path)
+      if (mimeType) {
+        c.header('Content-Type', mimeType)
+      }
+      // Return Response object
+      return c.body(content)
+    } else {
+      console.warn(`Static file: ${path} is not found`)
+      await next()
+    }
+    return
+  }
+}
