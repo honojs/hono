e50d25afa2a8310fa3fe743e1f4cb82a215ae6a2
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-07-15 23:20:04 +0000

    Update order of executions

--- src/middleware/compress/index.ts
+++ src/middleware/compress/index.ts
@@ -1,30 +1,22 @@
 import type { Context } from '../../context'
 import type { Next } from '../../hono'
+// Since Node.js v17
+// import { CompressionStream } from 'node:stream/web'
 
 interface CompressionOptions {
     encoding?: 'gzip' | 'deflate'
 }
 
-export const compress = (options: CompressionOptions) => {
+export const compress = (options?: CompressionOptions) => {
   return async (ctx: Context, next: Next) => {
+    await next()
+
     const accepted = ctx.req.headers.get('Accept-Encoding')
-    if (!accepted) {
-        await next()
+    const pattern = options?.encoding ?? /gzip|deflate/
+    const match = accepted?.match(pattern)
+    if (!accepted || !match || !ctx.res.body) {
         return
     }
-
-    const pattern = options.encoding ?? /gzip|deflate/
-    const match = accepted.match(pattern)
-    if (!match) {
-        await next()
-        return
-    }
-    
-    if (!ctx.res.body) {
-        await next()
-        return
-    }
-
     const encoding = match[0]
     const stream = new CompressionStream(encoding)
     ctx.res = new Response(ctx.res.body.pipeThrough(stream), ctx.res.clone())
