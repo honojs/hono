89fa4be07bb21c6d5785af0b12f266c2d319a1cf
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-07-16 09:38:43 +0900

    feat: Add compress middleware (#386)
    * Add compress middleware
    
    * Format code
    
    * Update order of executions
    
    * Add test
    
    * Remove version check
    
    * Remove node:stream/web
    
    * Update dependency
    
    * Remove version check

---
+++ src/middleware/compress/index.test.ts
@@ -0,0 +1,55 @@
+import { Hono } from '../../hono'
+import { compress } from '.'
+
+describe('Parse Compress Middleware', () => {
+
+  const app = new Hono()
+
+  app.use('*', compress())
+  app.get('/hello', async (ctx) => {
+    return ctx.text('hello')
+  })
+
+  it('gzip', async () => {
+    const req = new Request('http://localhost/hello', {
+      method: 'GET',
+      headers: new Headers({ 'Accept-Encoding': 'gzip' }),
+    })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Encoding')).toEqual('gzip')
+  })
+  
+  it('deflate', async () => {
+    const req = new Request('http://localhost/hello', {
+      method: 'GET',
+      headers: new Headers({ 'Accept-Encoding': 'deflate' }),
+    })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Encoding')).toEqual('deflate')
+  })
+  
+  it('gzip or deflate', async () => {
+    const req = new Request('http://localhost/hello', {
+      method: 'GET',
+      headers: new Headers({ 'Accept-Encoding': 'gzip, deflate' }),
+    })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Encoding')).toEqual('gzip')
+  })
+
+  it('raw', async () => {
+    const req = new Request('http://localhost/hello', {
+      method: 'GET',
+    })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('Content-Encoding')).toBeNull()
+  })
+})
