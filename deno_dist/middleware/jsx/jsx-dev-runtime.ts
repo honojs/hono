a9de0f3e6fe006051b9616f21597d1d295da5558
Yusuke Wada <yusuke@kamawada.com>
2022-07-24 15:20:05 +0900

    fix export path

--- deno_dist/middleware/jsx/jsx-dev-runtime.ts
+++ deno_dist/middleware/jsx/jsx-dev-runtime.ts
@@ -1,6 +1,7 @@
+import type { HtmlEscapedString } from '../html/index.ts'
 import { jsx } from './index.ts'
 
-export function jsxDEV(tag: string | Function, props: Record<string, any>) {
+export function jsxDEV(tag: string | Function, props: Record<string, any>): HtmlEscapedString {
   const children = props.children ?? []
   delete props['children']
   return jsx(tag, props, ...children)
