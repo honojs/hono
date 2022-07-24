2c8a03ca1a02afaa88296a7c5f7f44ce863db98d
Yusuke Wada <yusuke@kamawada.com>
2022-07-24 20:35:09 +0900

    fix(jsx/bun): do not escape children
    For JSX middleware on Bun.
    Fix bug that JSX `children` will force escaped.

--- deno_dist/middleware/jsx/jsx-dev-runtime.ts
+++ deno_dist/middleware/jsx/jsx-dev-runtime.ts
@@ -4,5 +4,5 @@ import { jsx } from './index.ts'
 export function jsxDEV(tag: string | Function, props: Record<string, any>): HtmlEscapedString {
   const children = props.children ?? []
   delete props['children']
-  return jsx(tag, props, ...children)
+  return jsx(tag, props, children)
 }
--- src/middleware/jsx/jsx-dev-runtime.ts
+++ src/middleware/jsx/jsx-dev-runtime.ts
@@ -4,5 +4,5 @@ import { jsx } from '.'
 export function jsxDEV(tag: string | Function, props: Record<string, any>): HtmlEscapedString {
   const children = props.children ?? []
   delete props['children']
-  return jsx(tag, props, ...children)
+  return jsx(tag, props, children)
 }
