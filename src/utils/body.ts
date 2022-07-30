4a2de0c1d17d47e282ccc2af41f036b2c84f8bbf
Yusuke Wada <yusuke@kamawada.com>
2022-07-30 21:21:57 +0900

    fix(`parseBody`): return blank object when JSON body is nothing (#433)
    Fix #428

--- src/utils/body.ts
+++ src/utils/body.ts
@@ -4,7 +4,11 @@ export const parseBody = async (
   const contentType = r.headers.get('Content-Type') || ''
 
   if (contentType.includes('application/json')) {
-    return await r.json()
+    let body = {}
+    try {
+      body = await r.json()
+    } catch {} // Do nothing
+    return body
   } else if (contentType.includes('application/text')) {
     return await r.text()
   } else if (contentType.startsWith('text')) {
