bc35351129d36cba34bf5270b00cd65bdbdcfc59
Taku Amano <taku@taaas.jp>
2022-07-31 17:17:24 +0900

    perf(jsx): Compare with React, Nano and Preact.

--- benchmarks/jsx/index.tsx
+++ benchmarks/jsx/index.tsx
@@ -1,44 +1,67 @@
 import Benchmark from 'benchmark'
-import { jsx, Fragment } from '../../src/middleware/jsx'
 
-const Content = () => (
-  <>
-    <p id='a' class='class-name'>
-      1<br />a
-    </p>
-    <p id='b' class='class-name'>
-      2<br />b
-    </p>
-    <div dangerouslySetInnerHTML={{ __html: '<p id="c" class="class-name">3<br/>c</p>' }} />
-    {null}
-    {undefined}
-  </>
-)
+import Nano, { Fragment as NanoFragment } from 'nano-jsx'
+import * as Preact from 'preact'
+import { render as renderByPreact } from 'preact-render-to-string'
+import React from 'react'
+import { renderToString as renderByReact } from 'react-dom/server'
 
-const Form = () => (
-  <form>
-    <input type='text' value='a' readonly tabindex={1} />
-    <input type='checkbox' value='b' checked={true} tabindex={2} />
-    <input type='checkbox' value='c' checked={true} tabindex={3} />
-    <input type='checkbox' value='d' checked={false} tabindex={4} />
-    <input type='checkbox' value='e' checked={false} tabindex={5} />
-  </form>
-)
+import * as HonoJSX from '../../src/middleware/jsx'
 
-const Page = () => (
-  <html>
-    <body>
-      <Content />
-      <Form />
-    </body>
-  </html>
-)
+const buildPage = ({ jsx, Fragment }: { jsx: any; Fragment: any }) => {
+  const Content = () => (
+    <>
+      <p id='a' class='class-name'>
+        1<br />a
+      </p>
+      <p id='b' class='class-name'>
+        2<br />b
+      </p>
+      <div dangerouslySetInnerHTML={{ __html: '<p id="c" class="class-name">3<br/>c</p>' }} />
+      {null}
+      {undefined}
+    </>
+  )
+
+  const Form = () => (
+    <form>
+      <input type='text' value='a' readonly tabindex={1} />
+      <input type='checkbox' value='b' checked={true} tabindex={2} />
+      <input type='checkbox' value='c' checked={true} tabindex={3} />
+      <input type='checkbox' value='d' checked={false} tabindex={4} />
+      <input type='checkbox' value='e' checked={false} tabindex={5} />
+    </form>
+  )
+
+  return () => (
+    <html>
+      <body>
+        <Content />
+        <Form />
+      </body>
+    </html>
+  )
+}
+
+const PageByHonoJSX = buildPage(HonoJSX)
+const PageByReact = buildPage({ jsx: React.createElement, Fragment: React.Fragment })
+const PageByNano = buildPage({ jsx: Nano.h, Fragment: NanoFragment })
+const PageByPreact = buildPage({ jsx: Preact.h, Fragment: Preact.Fragment })
 
 const suite = new Benchmark.Suite()
 
 suite
-  .add('render', () => {
-    Page().toString()
+  .add('Hono', () => {
+    PageByHonoJSX().toString()
+  })
+  .add('React', () => {
+    renderByReact(PageByReact())
+  })
+  .add('Preact', () => {
+    renderByPreact(PageByPreact())
+  })
+  .add('Nano', () => {
+    Nano.renderSSR(PageByNano)
   })
   .on('cycle', (ev: any) => {
     console.log(String(ev.target))
