fd1092d32acf8ac147124821ffb919d88689cbc1
Taku Amano <taku@taaas.jp>
2022-08-02 05:09:26 +0900

    perf(utils/html): Improve `escape` performance.
    Improved `escape` performance with reference to the react-dom implementation.

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -1,13 +1,56 @@
 export type HtmlEscapedString = string & { isEscaped: true }
+export type Buffer = [string]
 
-const entityMap: Record<string, string> = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-}
-const escapeRe = new RegExp(`[${Object.keys(entityMap).join('')}]`, 'g')
-const replaceFn = (m: string) => entityMap[m]
+// The `escape` and `escapeToBuffer` implementations are based on code from the MIT licensed `react-dom` package.
+// https://github.com/facebook/react/blob/main/packages/react-dom/src/server/escapeTextForBrowser.js
+
+const escapeRe = /[&<>"]/
+
 export const escape = (str: string): string => {
-  return str.replace(escapeRe, replaceFn)
+  // This function is an alias for `escapeToBuffer`,
+  // but returns immediately if `str` does not contain the character to be escaped.
+  const match = str.search(escapeRe)
+  if (match === -1) {
+    return str
+  }
+
+  const buffer: Buffer = ['']
+  escapeToBuffer(str, buffer)
+  return buffer[0]
+}
+
+export const escapeToBuffer = (str: string, buffer: Buffer): void => {
+  const match = str.search(escapeRe)
+  if (match === -1) {
+    buffer[0] += str
+    return
+  }
+
+  let escape
+  let index
+  let lastIndex = 0
+
+  for (index = match; index < str.length; index++) {
+    switch (str.charCodeAt(index)) {
+      case 34: // "
+        escape = '&quot;'
+        break
+      case 38: // &
+        escape = '&amp;'
+        break
+      case 60: // <
+        escape = '&lt;'
+        break
+      case 62: // >
+        escape = '&gt;'
+        break
+      default:
+        continue
+    }
+
+    buffer[0] += str.substring(lastIndex, index) + escape
+    lastIndex = index + 1
+  }
+
+  buffer[0] += str.substring(lastIndex, index)
 }
