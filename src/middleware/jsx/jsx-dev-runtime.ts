e73ce55bdcf3045376d9703ab556c6c487085c86
Taku Amano <taku@taaas.jp>
2022-08-02 06:33:10 +0900

    refactor(jsx): A function `jsx` returns `JSXNode` now.

--- src/middleware/jsx/jsx-dev-runtime.ts
+++ src/middleware/jsx/jsx-dev-runtime.ts
@@ -1,7 +1,7 @@
-import type { HtmlEscapedString } from '../html'
 import { jsx } from '.'
+import type { JSXNode } from '.'
 
-export function jsxDEV(tag: string | Function, props: Record<string, any>): HtmlEscapedString {
+export function jsxDEV(tag: string | Function, props: Record<string, any>): JSXNode {
   const children = props.children ?? []
   delete props['children']
   return jsx(tag, props, children)
