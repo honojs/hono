128a59aec24d97b1f7e3b7b6e6071a5d26d1bfea
Yusuke Wada <yusuke@kamawada.com>
2022-08-27 11:38:48 +0900

    fix: make that `c.req.parseBody` parses only `FormData` (#487)

--- src/utils/body.ts
+++ src/utils/body.ts
@@ -1,28 +1,16 @@
-export async function parseBody<T = any>(r: Request | Response): Promise<T> {
-  let body: any
-
-  const contentType = r.headers.get('Content-Type') || ''
-
-  if (contentType.includes('application/json')) {
-    let jsonBody = {}
-    try {
-      jsonBody = await r.json()
-    } catch {} // Do nothing
-    body = jsonBody
-  } else if (contentType.includes('application/text')) {
-    body = await r.text()
-  } else if (contentType.startsWith('text')) {
-    body = await r.text()
-  } else if (contentType.includes('form')) {
+export async function parseBody(r: Request | Response): Promise<Record<string, string | File>> {
+  let body: Record<string, string | File> = {}
+  const contentType = r.headers.get('Content-Type')
+  if (
+    contentType &&
+    (contentType.startsWith('multipart/form-data') ||
+      contentType === 'application/x-www-form-urlencoded')
+  ) {
     const form: Record<string, string | File> = {}
-    const data = [...(await r.formData())].reduce((acc, cur) => {
+    body = [...(await r.formData())].reduce((acc, cur) => {
       acc[cur[0]] = cur[1]
       return acc
     }, form)
-    body = data
-  } else {
-    body = await r.arrayBuffer()
   }
-
-  return body as T
+  return body
 }
