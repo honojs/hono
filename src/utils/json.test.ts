2c6f9d3383e3fac6a29512bcd973c82ea1cd218f
Derrick Farris <mr.dcfarris@gmail.com>
2022-09-27 21:01:38 -0500

    feat(JSON-Path): add support for array JSONPath-Plus syntax

--- src/utils/json.test.ts
+++ src/utils/json.test.ts
@@ -1,3 +1,4 @@
+import type { JSONArray } from './json'
 import { JSONPath } from './json'
 
 describe('JSONPath', () => {
@@ -10,26 +11,74 @@ describe('JSONPath', () => {
       },
       tags: ['foo', 'bar', { framework: 'Hono' }],
     },
+    relatedPosts: [
+      {
+        title: 'HelloWorld',
+        author: {
+          name: 'The Flash',
+          age: 32,
+        },
+        tags: ['flash', 'marvel'],
+      },
+      {
+        title: 'Hello World',
+        author: {
+          name: 'Spiderman',
+          age: 26,
+        },
+        tags: ['spider', 'man'],
+      },
+      {
+        title: 'World. Hello!',
+        author: {
+          name: 'Batman',
+          age: 45,
+        },
+        tags: ['bat', 'man'],
+      },
+    ],
   }
 
   it('Should return the string or number pointed by JSON Path', () => {
     expect(JSONPath(data, 'post.title')).toBe('Hello')
     expect(JSONPath(data, 'post.author.name')).toBe('Superman')
     expect(JSONPath(data, 'post.author.age')).toBe(20)
-    expect(JSONPath(data, 'post.tags.0')).toBe('foo')
-    expect(JSONPath(data, 'post.tags.1')).toBe('bar')
-    expect(JSONPath(data, 'post.tags.2.framework')).toBe('Hono')
+    expect(JSONPath(data, 'post.tags[0]')).toBe('foo')
+    expect(JSONPath(data, 'post.tags[1]')).toBe('bar')
+    expect(JSONPath(data, 'post.tags[2].framework')).toBe('Hono')
   })
 
   it('Should return undefined', () => {
     expect(JSONPath(data, 'post.foo')).toBe(undefined)
     expect(JSONPath(data, 'post.author.foo')).toBe(undefined)
-    expect(JSONPath(data, 'post.tags.3')).toBe(undefined)
+    expect(JSONPath(data, 'post.tags[3]')).toBe(undefined)
   })
 
   it('Should return objects pointed by JSON Path', () => {
     expect(typeof JSONPath(data, 'post')).toBe('object')
     expect(JSONPath(data, 'post.author')).toEqual({ name: 'Superman', age: 20 })
-    expect(JSONPath(data, 'post.tags').length).toBe(3)
+    expect((JSONPath(data, 'post.tags') as JSONArray).length).toBe(3)
+  })
+
+  it('Should return an array when targeting fields in arrays of objects', () => {
+    expect(Array.isArray(JSONPath(data, 'relatedPosts[*].title'))).toBe(true)
+    expect(JSONPath(data, 'relatedPosts[*].title')).toEqual([
+      'HelloWorld',
+      'Hello World',
+      'World. Hello!',
+    ])
+  })
+
+  it('Should return all deeply nested JSON path results', () => {
+    expect(Array.isArray(JSONPath(data, 'relatedPosts[*].tags[*]'))).toBe(true)
+    expect(JSONPath(data, 'relatedPosts[*].tags[*]')).toEqual([
+      'flash',
+      'marvel',
+      'spider',
+      'man',
+      'bat',
+      'man',
+    ])
+    expect(JSONPath(data, 'relatedPosts[*].tags[1]')).toEqual(['marvel', 'man', 'man'])
   })
 })
--- src/utils/json.ts
+++ src/utils/json.ts
@@ -1,20 +1,78 @@
-export type JSONPrimative = string | boolean | number | null
-export type JSONObject = { [key: string]: JSONPrimative }
-export type JSONValue = JSONObject | JSONPrimative
+const ARRAY_NOTATION_REGEX = /\[([0-9\*])\]/
+const ANY_INDEX = '*'
 
-export const JSONPath = (data: JSONObject, path: string) => {
+export type JSONPrimative = string | boolean | number | null | undefined
+export type JSONArray = (JSONPrimative | JSONObject | JSONArray)[]
+export type JSONObject = { [key: string]: JSONPrimative | JSONArray | JSONObject }
+export type JSONValue = JSONObject | JSONArray | JSONPrimative
+
+export type AnyIndex = typeof ANY_INDEX
+
+export const isArraySubpath = (subpath: string): boolean => {
+  const match = subpath.match(ARRAY_NOTATION_REGEX)
+  return Boolean(match ? match.length : false)
+}
+export const getArrSubpathIndex = (subpath: string): number | AnyIndex | undefined => {
+  const match = subpath.match(ARRAY_NOTATION_REGEX)
+  if (!match?.length) return undefined
+  // This works because only * and 0-9 should pass regex
+  if (match[1] === '*') {
+    return match[1]
+  } else {
+    const asInt = parseInt(match[1])
+    if (isNaN(asInt)) return undefined
+    else return asInt
+  }
+}
+
+const fromPath = (data: JSONObject, parts: string[]): JSONValue | undefined => {
   let val: JSONValue | undefined = data
-  const parts = path.split('.')
   const length = parts.length
-  for (let i = 0; i < length && val !== undefined && val !== null; i++) {
+  for (let i = 0; i < length && val !== undefined; i++) {
     const p = parts[i]
     if (p !== '') {
-      if (typeof val === 'object') {
-        val = val[p]
+      if (isArraySubpath(p)) {
+        const arrName = p.replace(ARRAY_NOTATION_REGEX, '')
+        const arr = (val as JSONObject)[arrName] as JSONObject[] | JSONPrimative[]
+        if (Array.isArray(arr)) {
+          const subIdx = getArrSubpathIndex(p)
+          if (typeof subIdx === 'number') {
+            val = arr[subIdx]
+          } else if (subIdx === '*') {
+            // Get rest of path
+            if (Array.isArray(arr[0])) {
+              throw new Error('Directly nested arrays not supported!')
+            }
+            val = arr.flatMap((curr) =>
+              curr && typeof curr === 'object' ? fromPath(curr, parts.slice(i + 1)) : curr
+            )
+            break
+          } else {
+            val = undefined
+          }
+        } else {
+          // This is an error, return undefined
+          val = undefined
+        }
+      } else if (Array.isArray(val)) {
+        // Val is an array, but we weren't marked as an array subpath
+        // Also, we know we shouldn't be here if this array was the last subpath part
+        // So this is invalid
+        val = undefined
+      } else if (typeof val === 'object') {
+        // Type coercion because type validator thinks val can be null past the if check
+        val = (val as JSONObject)[p]
       } else {
         val = undefined
       }
     }
   }
+  return val
+}
+
+export const JSONPath = (data: JSONObject, path: string) => {
+  let val: JSONValue | undefined = data
+  const parts = path.split('.')
+  val = fromPath(data, parts)
   return val
 }
