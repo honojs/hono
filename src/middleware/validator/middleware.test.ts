8c3655560e8d23e2115773799422bec0562cf370
Yusuke Wada <yusuke@kamawada.com>
2022-10-20 15:40:50 +0900

    fix(validator): show "type" error messages if does not have "value" errors

--- src/middleware/validator/middleware.test.ts
+++ src/middleware/validator/middleware.test.ts
@@ -707,3 +707,26 @@ describe('Nested structured data', () => {
     expect(res.status).toBe(400)
   })
 })
+
+describe('Special case', () => {
+  const app = new Hono()
+  app.get(
+    '/search',
+    validator((v) => ({
+      q: v.query('q').isRequired(),
+      page: v.query('page').asNumber(),
+    })),
+    (c) => {
+      return c.text('Valid')
+    }
+  )
+  it('Should show the type message error if it does not have "value" errors', async () => {
+    const res = await app.request('http://localhost/search?q=foo')
+    expect(res.status).toBe(400)
+    expect(await res.text()).toBe(
+      [
+        'Invalid Value [undefined]: the query parameter "page" is invalid - should be "number"',
+      ].join('\n')
+    )
+  })
+})
