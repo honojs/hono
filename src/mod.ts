4526b077974d639715209e1d764352ae291e0d28
Yusuke Wada <yusuke@kamawada.com>
2022-10-24 23:59:46 +0900

    feat(validator): export `Validator` from 'hono' (#627)

--- src/mod.ts
+++ src/mod.ts
@@ -25,3 +25,6 @@ export { RegExpRouter } from './router/reg-exp-router'
 export { TrieRouter } from './router/trie-router'
 export { StaticRouter } from './router/static-router'
 export { SmartRouter } from './router/smart-router'
+
+// Validator
+export type { Validator } from './validator/validator'
