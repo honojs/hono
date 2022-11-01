1de54b5b01016d710dc9664818342b69c35e5286
Yusuke Wada <yusuke@kamawada.com>
2022-11-01 21:36:30 +0900

    fix(jsx): export fragment

--- deno_dist/middleware/jsx/jsx-dev-runtime.ts
+++ deno_dist/middleware/jsx/jsx-dev-runtime.ts
@@ -1,5 +1,6 @@
 import { jsx } from './index.ts'
 import type { JSXNode } from './index.ts'
+export { Fragment } from './index.ts'
 
 export function jsxDEV(tag: string | Function, props: Record<string, any>): JSXNode {
   const children = props.children ?? []
