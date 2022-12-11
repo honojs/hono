abde7d1d775ce3c5c36f02d1490ab10419884acb
Yusuke Wada <yusuke@kamawada.com>
2022-12-11 11:05:30 +0900

    fix(type): infer types correctly (#715)

--- src/validator/schema.ts
+++ src/validator/schema.ts
@@ -23,32 +23,36 @@ export type Schema = {
     | VArray<Schema>
 }
 
-export type SchemaToProp<T> = {
-  [K in keyof T]: T[K] extends VNumberArray
-    ? number[]
-    : T[K] extends VBooleanArray
-    ? boolean[]
-    : T[K] extends VStringArray
-    ? string[]
-    : T[K] extends VNumber
-    ? number
-    : T[K] extends VBoolean
-    ? boolean
-    : T[K] extends VString
-    ? string
-    : T[K] extends VObjectBase<Schema>
-    ? T[K]['container'] extends VNumber
-      ? number
-      : T[K]['container'] extends VString
-      ? string
-      : T[K]['container'] extends VBoolean
-      ? boolean
-      : T[K] extends VArray<Schema>
-      ? SchemaToProp<ReadonlyArray<T[K]['container']>>
-      : T[K] extends VObject<Schema>
-      ? SchemaToProp<T[K]['container']>
-      : T[K] extends Schema
-      ? SchemaToProp<T[K]>
-      : never
-    : SchemaToProp<T[K]>
-}
+export type SchemaToProp<T> = T extends VArray<infer R>
+  ? SchemaToProp<R>[]
+  : T extends VObject<infer R>
+  ? SchemaToProp<R>
+  : {
+      [K in keyof T]: T[K] extends VNumberArray
+        ? number[]
+        : T[K] extends VBooleanArray
+        ? boolean[]
+        : T[K] extends VStringArray
+        ? string[]
+        : T[K] extends VString
+        ? string
+        : T[K] extends VNumber
+        ? number
+        : T[K] extends VBoolean
+        ? boolean
+        : T[K] extends VObjectBase<Schema>
+        ? T[K]['container'] extends VNumber
+          ? number
+          : T[K]['container'] extends VString
+          ? string
+          : T[K]['container'] extends VBoolean
+          ? boolean
+          : T[K] extends VArray<infer R>
+          ? SchemaToProp<R>[]
+          : T[K] extends VObject<infer R>
+          ? SchemaToProp<R>
+          : T[K] extends Schema
+          ? SchemaToProp<T[K]>
+          : never
+        : SchemaToProp<T[K]>
+    }
