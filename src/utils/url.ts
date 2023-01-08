3b588e9fdc281befbaa496699fbb666fa65ef26b
Yusuke Wada <yusuke@kamawada.com>
2023-01-08 17:22:44 +0900

    feat(trie-router): `regexp` support path including slashes (#768)
    * feat(trie-router): `regexp` support path including slashes
    
    * refactor(trie-router): Use `splitRoutingPath`. (#781)
    
    * refactor(utils): splitRoutingPath, allow `@` to be safely used in parameter names. (#790)
    
    Co-authored-by: Taku Amano <taku@taaas.jp>

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -8,6 +8,39 @@ export const splitPath = (path: string): string[] => {
   return paths
 }
 
+export const splitRoutingPath = (path: string): string[] => {
+  const groups: [string, string][] = [] // [mark, original string]
+  for (let i = 0; ; ) {
+    let replaced = false
+    path = path.replace(/\{[^}]+\}/g, (m) => {
+      const mark = `@\\${i}`
+      groups[i] = [mark, m]
+      i++
+      replaced = true
+      return mark
+    })
+    if (!replaced) {
+      break
+    }
+  }
+
+  const paths = path.split(/\//) // faster than path.split('/')
+  if (paths[0] === '') {
+    paths.shift()
+  }
+  for (let i = groups.length - 1; i >= 0; i--) {
+    const [mark] = groups[i]
+    for (let j = paths.length - 1; j >= 0; j--) {
+      if (paths[j].indexOf(mark) !== -1) {
+        paths[j] = paths[j].replace(mark, groups[i][1])
+        break
+      }
+    }
+  }
+
+  return paths
+}
+
 const patternCache: { [key: string]: Pattern } = {}
 export const getPattern = (label: string): Pattern | null => {
   // *            => wildcard
