307ab334e20c47527721c4bbf898ddabddff82ad
Yusuke Wada <yusuke@kamawada.com>
2023-01-17 08:03:22 +0900

    added Next.js adapter

---
+++ src/adapter/nextjs/handler.ts
@@ -0,0 +1,8 @@
+import type { Hono } from '../../hono'
+import type { Env } from '../../types'
+
+export const handle = <E extends Env, R extends Request>(app: Hono<E>) => {
+  return async (req: R) => {
+    return app.fetch(req)
+  }
+}
