4f9d7f2d7eeb1ad00e2b29650b25b2e1688e2d32
Yusuke Wada <yusuke@kamawada.com>
2023-01-24 08:53:31 +0900

    fix(context): fixed bug raw `Response` content-type is overwritten (#836)

--- deno_dist/context.ts
+++ deno_dist/context.ts
@@ -66,6 +66,7 @@ export class Context<
 
   set res(_res: Response) {
     if (this._res) {
+      this._res.headers.delete('content-type')
       this._res.headers.forEach((v, k) => {
         _res.headers.set(k, v)
       })
