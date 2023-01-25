45a43157f1f1d03787dda316df871039c19cfe2b
Yusuke Wada <yusuke@kamawada.com>
2023-01-26 00:30:55 +0900

    do not include `passThroughOnException: () => void` in `EventContext`

--- src/adapter/cloudflare-pages/handler.ts
+++ src/adapter/cloudflare-pages/handler.ts
@@ -1,11 +1,16 @@
 // @denoify-ignore
-/// <reference types="@cloudflare/workers-types" />
 import type { Hono } from '../../hono'
 import type { Env } from '../../types'
 
+type EventContext = {
+  request: Request
+  waitUntil: (promise: Promise<unknown>) => void
+  env: {} & { ASSETS: { fetch: typeof fetch } }
+}
+
 export const handle = <E extends Env>(app: Hono<E>) => {
-  return (eventContext: EventContext<{}, string, {}>): Response | Promise<Response> => {
-    const { request, env, waitUntil, passThroughOnException } = eventContext
-    return app.fetch(request, env, { waitUntil, passThroughOnException })
+  return (eventContext: EventContext): Response | Promise<Response> => {
+    const { request, env, waitUntil } = eventContext
+    return app.fetch(request, env, { waitUntil, passThroughOnException: () => {} })
   }
 }
