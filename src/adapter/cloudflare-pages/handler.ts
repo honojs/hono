c9df9fee6925d7cc7e83cd1d9258d46c6e0667bb
Yusuke Wada <yusuke@kamawada.com>
2023-01-28 01:34:15 +0900

    fix(cloudflare-pages): fix type incompatibility (#837)
    * fix(cloudflare-pages): fix type incompatibility
    
    * use triple-slash
    
    * do not include `passThroughOnException: () => void` in `EventContext`

--- src/adapter/cloudflare-pages/handler.ts
+++ src/adapter/cloudflare-pages/handler.ts
@@ -1,13 +1,16 @@
 // @denoify-ignore
-import type { EventContext } from '@cloudflare/workers-types'
 import type { Hono } from '../../hono'
 import type { Env } from '../../types'
 
+type EventContext = {
+  request: Request
+  waitUntil: (promise: Promise<unknown>) => void
+  env: {} & { ASSETS: { fetch: typeof fetch } }
+}
+
 export const handle = <E extends Env>(app: Hono<E>) => {
-  return (eventContext: EventContext<{}, string, {}>) => {
-    const { request, env, waitUntil, passThroughOnException } = eventContext
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return app.fetch(request, env, { waitUntil, passThroughOnException })
+  return (eventContext: EventContext): Response | Promise<Response> => {
+    const { request, env, waitUntil } = eventContext
+    return app.fetch(request, env, { waitUntil, passThroughOnException: () => {} })
   }
 }
