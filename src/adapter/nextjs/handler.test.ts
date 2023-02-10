724fbae4bf932b6528811512a96b536d2372384d
Yusuke Wada <yusuke@kamawada.com>
2023-02-11 00:58:21 +0900

    feat(adapter): `handle(path, app)` for CF Pages & Next.js (#878)

---
+++ src/adapter/nextjs/handler.test.ts
@@ -0,0 +1,28 @@
+import { Hono } from '../../hono'
+import { handle } from './handler'
+
+describe('Adapter for Next.js', () => {
+  it('Should return 200 response', async () => {
+    const app = new Hono()
+    app.get('/api/foo', (c) => {
+      return c.text('/api/foo')
+    })
+    const handler = handle(app)
+    const req = new Request('http://localhost/api/foo')
+    const res = await handler(req)
+    expect(res.status).toBe(200)
+    expect(await res.text()).toBe('/api/foo')
+  })
+
+  it('Should return 200 response with path', async () => {
+    const app = new Hono()
+    app.get('/foo', (c) => {
+      return c.text('/api/foo')
+    })
+    const handler = handle('/api', app)
+    const req = new Request('http://localhost/api/foo')
+    const res = await handler(req)
+    expect(res.status).toBe(200)
+    expect(await res.text()).toBe('/api/foo')
+  })
+})
