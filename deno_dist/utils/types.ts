5e582ab54a2a925659ad3110adac488ca5aa0ee9
Yusuke Wada <yusuke@kamawada.com>
2023-02-28 18:47:17 +0900

    create `TrueAndFalseToBoolean` and add a comment

--- deno_dist/utils/types.ts
+++ deno_dist/utils/types.ts
@@ -15,12 +15,14 @@ export type JSONPrimitive = string | boolean | number | null | undefined
 export type JSONArray = (JSONPrimitive | JSONObject | JSONArray)[]
 export type JSONObject = { [key: string]: JSONPrimitive | JSONArray | JSONObject }
 export type JSONValue = JSONObject | JSONArray | JSONPrimitive
-export type PrettyJSON<T> = T extends true
-  ? boolean
-  : T extends false
-  ? boolean
-  : T extends JSONPrimitive
-  ? T
+
+// `boolean` will be `true` or `false` because it's an alias of them.
+// See: https://github.com/microsoft/TypeScript/issues/22596
+// This type converts `true | false` to `boolean` that we expect.
+type TrueAndFalseToBoolean<T> = T extends true ? boolean : T extends false ? boolean : T
+
+export type PrettyJSON<T> = T extends JSONPrimitive
+  ? TrueAndFalseToBoolean<T>
   : T extends JSONArray
   ? PrettyJSON<T[number]>
   : T extends JSONObject
