165d925231dafff5074977dd6a834a05bda44118
Yusuke Wada <yusuke@kamawada.com>
2023-03-02 21:44:11 +0900

    fix(pages/nextjs): don't use `route()` if path is not passed (#943)

--- src/adapter/nextjs/handler.ts
+++ src/adapter/nextjs/handler.ts
@@ -3,10 +3,12 @@ import { Hono } from '../../hono'
 import type { Env } from '../../types'
 
 interface HandleInterface {
-  <E extends Env>(subApp: Hono<E>, path?: string): (req: Request) => Promise<Response>
+  <E extends Env>(subApp: Hono<E>, path?: string): (req: Request) => Response | Promise<Response>
 }
 
 export const handle: HandleInterface =
-  <E extends Env>(subApp: Hono<E>, path: string = '/') =>
-  async (req) =>
-    new Hono<E>().route(path, subApp).fetch(req)
+  <E extends Env>(subApp: Hono<E>, path?: string) =>
+  (req) => {
+    const app = path ? new Hono<E>().route(path, subApp) : subApp
+    return app.fetch(req)
+  }
