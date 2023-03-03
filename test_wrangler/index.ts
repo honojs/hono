bd2a1ca3538d3599d02086a2a23052470b3e855f
Yusuke Wada <yusuke@kamawada.com>
2023-03-03 15:59:10 +0900

    typo

--- test_wrangler/index.ts
+++ test_wrangler/index.ts
@@ -6,7 +6,7 @@ const app = new Hono()
 app.get('/', (c) => c.text('Hello Wrangler!'))
 
 app.get('/env', (c) => {
-  const { NAME } = env<{ NAME: 'Cloudflare' }>(c)
+  const { NAME } = env<{ NAME: string }>(c)
   return c.text(NAME)
 })
 
