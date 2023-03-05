b0579a185c33088c1369486620788de76f022be9
Yusuke Wada <yusuke@kamawada.com>
2023-03-05 23:39:04 +0900

    fix(type): remove unnecessary distribution (#951)

--- src/context.ts
+++ src/context.ts
@@ -4,7 +4,7 @@ import type { Env, NotFoundHandler, Input } from './types'
 import type { CookieOptions } from './utils/cookie'
 import { serialize } from './utils/cookie'
 import type { StatusCode } from './utils/http-status'
-import type { PrettyJSON, JSONValue } from './utils/types'
+import type { JSONValue } from './utils/types'
 
 type Runtime = 'node' | 'deno' | 'bun' | 'workerd' | 'fastly' | 'edge-light' | 'lagon' | 'other'
 type HeaderRecord = Record<string, string | string[]>
@@ -242,7 +242,7 @@ export class Context<
     object: T extends JSONValue ? T : JSONValue,
     status: StatusCode = this._status,
     headers?: HeaderRecord
-  ): TypedResponse<T extends JSONValue ? (JSONValue extends T ? never : PrettyJSON<T>) : never> => {
+  ): TypedResponse<T extends JSONValue ? (JSONValue extends T ? never : T) : never> => {
     return {
       response: this.json(object, status, headers),
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
