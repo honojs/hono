a62132712404da343add869c8567f6f6ea57f5c9
Stefan Schonert <sh@schonert.dk>
2023-03-23 10:20:04 +0100

    Added aws-handler support for APIGatewayProxyEventV2

--- src/adapter/aws-lambda/handler.ts
+++ src/adapter/aws-lambda/handler.ts
@@ -6,59 +6,115 @@ import type { Hono } from '../../hono'
 // @ts-ignore
 globalThis.crypto = crypto
 
-interface APIGatewayEvent {
-  httpMethod: string
-  headers: Record<string, string | undefined>
-  path: string
-  body: string | null
-  isBase64Encoded: boolean
+// When calling Lambda directly through function urls
+interface APIGatewayProxyEventV2 {
+  httpMethod: string;
+  headers: Record<string, string | undefined>;
+  rawPath: string;
+  rawQueryString: string;
+  body: string | null;
+  isBase64Encoded: boolean;
   requestContext: {
-    domainName: string
-  }
+    domainName: string;
+  };
+}
+
+// When calling Lambda through API Gateway
+interface APIGatewayProxyEvent {
+  httpMethod: string;
+  headers: Record<string, string | undefined>;
+  path: string;
+  body: string | null;
+  isBase64Encoded: boolean;
+  requestContext: {
+    domainName: string;
+  };
 }
 
 interface APIGatewayProxyResult {
-  statusCode: number
-  body: string
-  headers: Record<string, string>
-  isBase64Encoded: boolean
+  statusCode: number;
+  body: string;
+  headers: Record<string, string>;
+  isBase64Encoded: boolean;
 }
 
+/**
+ * Accepts events from API Gateway(`APIGatewayProxyEvent`) and directly through Function Url(`APIGatewayProxyEventV2`)
+ */
 export const handle = (app: Hono) => {
-  return async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-    const req = createRequest(event)
-    const res = await app.fetch(req)
-    const arrayBuffer = await res.arrayBuffer()
-    const result: APIGatewayProxyResult = {
-      statusCode: res.status,
-      body: String.fromCharCode(...new Uint8Array(arrayBuffer)),
-      headers: {},
-      isBase64Encoded: false,
-    }
+  return async (
+    event: APIGatewayProxyEvent | APIGatewayProxyEventV2
+  ): Promise<APIGatewayProxyResult> => {
+    const req = createRequest(event);
+    const res = await app.fetch(req);
 
-    res.headers.forEach((value, key) => {
-      result.headers[key] = value
-    })
+    return createResult(res);
+  };
+};
 
-    return result
-  }
+async function createResult(res: Response): Promise<APIGatewayProxyResult> {
+  const isBase64Encoded = res.body instanceof ReadableStream;
+  const body = isBase64Encoded
+    ? await fromReadableToString(res)
+    : await fromBufferTostring(res);
+
+  const result: APIGatewayProxyResult = {
+    body,
+    headers: {},
+    statusCode: res.status,
+    isBase64Encoded,
+  };
+
+  res.headers.forEach((value, key) => {
+    result.headers[key] = value;
+  });
+
+  return result;
 }
 
-const createRequest = (event: APIGatewayEvent) => {
-  const url = `https://${event.requestContext.domainName}${event.path}`
-  const headers = new Headers()
+function createRequest(event: APIGatewayProxyEvent | APIGatewayProxyEventV2) {
+  const url = isProxyEventV2(event)
+    ? `https://${event.requestContext.domainName}${event.rawPath}?${event.rawQueryString}`
+    : `https://${event.requestContext.domainName}${event.path}`;
+
+  const headers = new Headers();
   for (const [k, v] of Object.entries(event.headers)) {
-    if (v) headers.set(k, v)
+    if (v) headers.set(k, v);
   }
-  const method = event.httpMethod
 
   const requestInit: RequestInit = {
-    headers: headers,
-    method: method,
-  }
+    headers,
+    method: event.httpMethod,
+  };
 
   if (event.body) {
-    requestInit.body = event.isBase64Encoded ? atob(event.body) : event.body
+    requestInit.body = event.isBase64Encoded
+      ? Buffer.from(event.body, "base64")
+      : event.body;
   }
-  return new Request(url, requestInit)
+
+  return new Request(url, requestInit);
+}
+
+function isProxyEventV2(
+  event: APIGatewayProxyEvent | APIGatewayProxyEventV2
+): event is APIGatewayProxyEventV2 {
+  return event.hasOwnProperty("rawPath");
+}
+
+async function fromBufferTostring(res: Response) {
+  const arrayBuffer = await res.arrayBuffer();
+  return String.fromCharCode(...new Uint8Array(arrayBuffer));
+}
+
+async function fromReadableToString(res: Response) {
+  const chunks = [];
+  const stream = res.body || new ReadableStream();
+
+  // @ts-ignore: asking for asyncIterator
+  for await (let chunk of stream) {
+    chunks.push(chunk);
+  }
+
+  return Buffer.concat(chunks).toString("base64");
 }
