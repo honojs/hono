3c7f28e4750ebf09d84448f4a4c8a7ebf7978584
Yusuke Wada <yusuke@kamawada.com>
2023-05-04 00:40:51 +0900

    chore: denoify

--- deno_dist/middleware.ts
+++ deno_dist/middleware.ts
@@ -2,6 +2,7 @@
 export * from './middleware/basic-auth/index.ts'
 export * from './middleware/bearer-auth/index.ts'
 export * from './middleware/cache/index.ts'
+export * from './middleware/cookie/index.ts'
 export * from './middleware/compress/index.ts'
 export * from './middleware/cors/index.ts'
 export * from './middleware/etag/index.ts'
