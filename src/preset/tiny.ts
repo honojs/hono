be965a89ddb176c0da6235ba1698716ecb0e2f9d
Yusuke Wada <yusuke@kamawada.com>
2023-05-09 21:56:55 +0900

    feat(preset): add `hono/quick` (#1074)

--- src/preset/tiny.ts
+++ src/preset/tiny.ts
@@ -1,6 +1,6 @@
-import { HonoBase } from './hono-base'
-import { PatternRouter } from './router/pattern-router'
-import type { Env } from './types'
+import { HonoBase } from '../hono-base'
+import { PatternRouter } from '../router/pattern-router'
+import type { Env } from '../types'
 
 export class Hono<E extends Env = Env, S = {}, BasePath extends string = ''> extends HonoBase<
   E,
