efebf6456993308adcb9562e6837c25b371d99c8
Yusuke Wada <yusuke@kamawada.com>
2023-05-09 21:56:55 +0900

    feat(preset): add `hono/quick` (#1074)

---
+++ src/preset/quick.ts
@@ -0,0 +1,14 @@
+import { HonoBase } from '../hono-base'
+import { LinearRouter } from '../router/linear-router'
+import type { Env } from '../types'
+
+export class Hono<E extends Env = Env, S = {}, BasePath extends string = ''> extends HonoBase<
+  E,
+  S,
+  BasePath
+> {
+  constructor() {
+    super()
+    this.router = new LinearRouter()
+  }
+}
