187cb7ed5bbdd2341822801aee4ca796fff70754
Yusuke Wada <yusuke@kamawada.com>
2023-05-09 22:15:10 +0900

    chore: denoify

---
+++ deno_dist/preset/quick.ts
@@ -0,0 +1,14 @@
+import { HonoBase } from '../hono-base.ts'
+import { LinearRouter } from '../router/linear-router/index.ts'
+import type { Env } from '../types.ts'
+
+export class Hono<E extends Env = Env, S = {}, BasePath extends string = ''> extends HonoBase<
+  E,
+  S,
+  BasePath
+> {
+  constructor() {
+    super()
+    this.router = new LinearRouter()
+  }
+}
