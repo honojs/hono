44e0e8185c482460cd3487c189e1870233a0cf04
Yusuke Wada <yusuke@kamawada.com>
2023-05-15 09:12:51 +0900

    feat: `mount()`

---
+++ src/mounter.test.ts
@@ -0,0 +1,110 @@
+import { Hono } from './hono'
+import { getBaseURLAndPath, normalizePath, getNewRequestURL, mount } from './mounter'
+import { getPath } from './utils/url'
+
+describe('getBaseURLAndPath', () => {
+  const baseURL = 'http://localhost'
+  it('Should return the base URL', () => {
+    expect(getBaseURLAndPath('http://localhost/abc')).toEqual([baseURL, '/abc'])
+    expect(getBaseURLAndPath('http://localhost/abc/def')).toEqual([baseURL, '/abc/def'])
+    expect(getBaseURLAndPath('http://localhost/')).toEqual([baseURL, '/'])
+    expect(getBaseURLAndPath('http://localhost')).toEqual([baseURL, '/'])
+    expect(getBaseURLAndPath('hhttp')).toEqual(['hhttp', '/'])
+  })
+})
+
+describe('normalizePath', () => {
+  it('Should normalize a path', () => {
+    expect(normalizePath('/abc')).toBe('/abc')
+    expect(normalizePath('/abc/')).toBe('/abc/')
+    expect(normalizePath('/abc/*')).toBe('/abc')
+    expect(normalizePath('/abc*')).toBe('/abc*')
+    expect(normalizePath('/*')).toBe('/')
+    expect(normalizePath('/')).toBe('/')
+  })
+})
+
+describe('getNewRequestURL', () => {
+  it('Should return a correct URL', () => {
+    expect(getNewRequestURL('http://localhost/abc', '/abc')).toBe('http://localhost/')
+    expect(getNewRequestURL('http://localhost/abc/def', '/abc')).toBe('http://localhost/def')
+    expect(getNewRequestURL('http://localhost/abc/def/ghi', '/abc')).toBe(
+      'http://localhost/def/ghi'
+    )
+    expect(getNewRequestURL('http://localhost/abc', '/')).toBe('http://localhost/abc')
+    expect(getNewRequestURL('http://localhost/abc', 'abc')).toBe('http://localhost/abc')
+    expect(getNewRequestURL('http://localhost/abc/def', '/def')).toBe('http://localhost/abc/def')
+    expect(getNewRequestURL('http://localhost/abc', '/*')).toBe('http://localhost/abc')
+    expect(getNewRequestURL('http://localhost/abc/def', '/abc/*')).toBe('http://localhost/def')
+    expect(getNewRequestURL('http://localhost/abc/def', '/abc*')).toBe('http://localhost/def')
+  })
+})
+
+describe('mount', () => {
+  const anotherApp = (req: Request, params: unknown) => {
+    const path = getPath(req)
+    if (path === '/') {
+      return new Response('AnotherApp')
+    }
+    if (path === '/hello') {
+      return new Response('Hello from AnotherApp')
+    }
+    if (path == '/with-params') {
+      return new Response(
+        JSON.stringify({
+          params,
+        }),
+        {
+          headers: {
+            'Content-Type': 'application.json',
+          },
+        }
+      )
+    }
+    return new Response('Not Found from AnotherApp', {
+      status: 404,
+    })
+  }
+
+  const app = new Hono()
+  app.use('*', async (c, next) => {
+    await next()
+    c.header('x-message', 'Foo')
+  })
+  app.get('/', (c) => c.text('Hono'))
+  app.get(
+    ...mount('/another-app', anotherApp, () => {
+      return 'params'
+    })
+  )
+
+  it('Should return responses from Hono app', async () => {
+    const res = await app.request('/')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('x-message')).toBe('Foo')
+    expect(await res.text()).toBe('Hono')
+  })
+
+  it('Should return responses from AnotherApp', async () => {
+    let res = await app.request('/another-app')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('x-message')).toBe('Foo')
+    expect(await res.text()).toBe('AnotherApp')
+
+    res = await app.request('/another-app/hello')
+    expect(res.status).toBe(200)
+    expect(res.headers.get('x-message')).toBe('Foo')
+    expect(await res.text()).toBe('Hello from AnotherApp')
+
+    res = await app.request('/another-app/not-found')
+    expect(res.status).toBe(404)
+    expect(res.headers.get('x-message')).toBe('Foo')
+    expect(await res.text()).toBe('Not Found from AnotherApp')
+
+    res = await app.request('/another-app/with-params')
+    expect(res.status).toBe(200)
+    expect(await res.json()).toEqual({
+      params: 'params',
+    })
+  })
+})
---
+++ src/mounter.ts
@@ -0,0 +1,58 @@
+import type { Context } from './context'
+import type { MiddlewareHandler } from './types'
+import { mergePath } from './utils/url'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ApplicationHandler = (request: Request, args?: any) => Response | Promise<Response>
+type OptionHandler = (c: Context) => unknown
+
+/**
+ * @experimental
+ * `mount()` is an experimental feature.
+ * The API might be changed.
+ */
+export const mount = (
+  path: string,
+  applicationHandler: ApplicationHandler,
+  optionHandler?: OptionHandler
+): [string, MiddlewareHandler] => {
+  path = normalizePath(path)
+  const handler: MiddlewareHandler = async (c, next) => {
+    let executionContext: ExecutionContext | undefined = undefined
+    try {
+      executionContext = c.executionCtx
+    } catch {} // Do nothing
+    // Default options are "Env" and "ExecutionContext"
+    const options = optionHandler ? optionHandler(c) : [c.env, executionContext]
+    const optionsArray = Array.isArray(options) ? options : [options]
+
+    const res = await applicationHandler(
+      new Request(getNewRequestURL(c.req.url, path), c.req.raw),
+      ...optionsArray
+    )
+
+    if (res) return res
+
+    await next()
+  }
+  return [mergePath(path, '*'), handler]
+}
+
+export const getBaseURLAndPath = (url: string) => {
+  const match = url.match(/(^https?:\/\/[^\/]+)\/(.+)?/)
+  const baseURL = match ? match[1] : url
+  const path = match && match[2] ? `/${match[2]}` : '/'
+  return [baseURL, path]
+}
+
+export const normalizePath = (path: string) => {
+  return path.replace(/\/\*$/, '') || '/' // Normalize for `/abc/*`
+}
+
+export const getNewRequestURL = (urlString: string, path: string) => {
+  const [baseURL, oldPath] = getBaseURLAndPath(urlString)
+  const regexp = new RegExp(`^${path}`)
+  path = oldPath.replace(regexp, '')
+  if (!/^\//.test(path)) path = '/' + path
+  return baseURL + normalizePath(path)
+}
