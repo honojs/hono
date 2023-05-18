4fde03e22f5a29920b1432f2627c6a8e000bc3be
Yusuke Wada <yusuke@kamawada.com>
2023-05-18 14:18:55 +0900

    fix(preset): fix constructor args (#1113)
    * fix(preset): fix constructor args
    
    * chore: denoify

--- deno_dist/preset/tiny.ts
+++ deno_dist/preset/tiny.ts
@@ -7,8 +7,8 @@ export class Hono<E extends Env = Env, S = {}, BasePath extends string = '/'> ex
   S,
   BasePath
 > {
-  constructor() {
-    super()
+  constructor(init: Partial<Pick<Hono, 'getPath'> & { strict: boolean }> = {}) {
+    super(init)
     this.router = new PatternRouter()
   }
 }
