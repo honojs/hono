b2d486f1d27467797eb3f48ee00801009f035b93
Taku Amano <taku@taaas.jp>
2023-06-02 22:16:02 +0900

    refactor(pattern-router): Use `Object.assign` for shortening the code.

--- src/router/pattern-router/router.ts
+++ src/router/pattern-router/router.ts
@@ -1,7 +1,7 @@
 import type { Result, Router } from '../../router'
 import { METHOD_NAME_ALL } from '../../router'
 
-type Route<T> = [RegExp, string, T, string] // [pattern, method, handler, path]
+type Route<T> = [RegExp, string, T] // [pattern, method, handler, path]
 
 export class PatternRouter<T> implements Router<T> {
   name: string = 'PatternRouter'
@@ -43,32 +43,22 @@ export class PatternRouter<T> implements Router<T> {
       new RegExp(`^${parts.join('')}${endsWithWildcard ? '' : '/?$'}`),
       method,
       handler,
-      path,
     ])
   }
 
   match(method: string, path: string): Result<T> | null {
     const handlers: T[] = []
-    let params: Record<string, string> | undefined = undefined
-    const matchedRoutes: Route<T>[] = []
+    const params: Record<string, string> = {}
 
-    for (const route of this.routes) {
-      const [pattern, routeMethod, handler] = route
+    for (const [pattern, routeMethod, handler] of this.routes) {
       const isRegExp = pattern.source.charCodeAt(pattern.source.length - 1) === 36
       if (routeMethod === METHOD_NAME_ALL || routeMethod === method) {
         const match = pattern.exec(path)
         if (match) {
           handlers.push(handler)
-          for (const r of matchedRoutes) {
-            const m = pattern.exec(r[3])
-            if (m && isRegExp) {
-              params = Object.assign({}, m.groups)
-            }
-          }
           if (isRegExp) {
-            params ??= match.groups || {}
+            Object.assign(params, match.groups)
           }
-          matchedRoutes.push(route)
         }
       }
     }
@@ -76,7 +66,7 @@ export class PatternRouter<T> implements Router<T> {
     return handlers.length
       ? {
           handlers,
-          params: params || {},
+          params,
         }
       : null
   }
