acb9a13c3284b1aaf7beb8ff99ef42204d022957
Yusuke Wada <yusuke@kamawada.com>
2023-07-08 18:25:50 +0900

    fix(mount): include query params (#1224)
    * fix(mount): include query params
    
    * denoify

--- deno_dist/utils/url.ts
+++ deno_dist/utils/url.ts
@@ -74,6 +74,11 @@ export const getPath = (request: Request): string => {
   return url.slice(url.indexOf('/', 8), queryIndex === -1 ? undefined : queryIndex)
 }
 
+export const getQueryStrings = (url: string): string => {
+  const queryIndex = url.indexOf('?', 8)
+  return queryIndex === -1 ? '' : '?' + url.slice(queryIndex + 1)
+}
+
 export const getPathNoStrict = (request: Request): string => {
   const result = getPath(request)
 
