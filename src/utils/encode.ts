01ea10b32fdafd9533e3bf58d241c43a4e93c01e
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-15 06:27:27 +0000

    lint

--- src/utils/encode.ts
+++ src/utils/encode.ts
@@ -6,11 +6,11 @@ export const encodeBase64Url = (buf: ArrayBufferLike): string =>
   encodeBase64(buf).replace(/\/|\+/g, (m) => ({ '/': '_', '+': '-' }[m] ?? m))
 
 export const encodeBase64 = (buf: ArrayBufferLike): string => {
-  const buffer = Buffer.from(buf);
-  return buffer.toString('base64');
+  const buffer = Buffer.from(buf)
+  return buffer.toString('base64')
 }
 
 export const decodeBase64 = (str: string): Uint8Array => {
-  const buffer = Buffer.from(str, 'base64');
-  return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength);
+  const buffer = Buffer.from(str, 'base64')
+  return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength)
 }
