a84ca4aec2b9eabede2b70129fbd6891cc475b9c
Yusuke Wada <yusuke@kamawada.com>
2023-07-20 17:43:35 +0900

    fix(hono/quick): use SmartRouter (#1250)
    * fix(hono/quick): use SmartRouter
    
    * denoify

--- deno_dist/preset/quick.ts
+++ deno_dist/preset/quick.ts
@@ -1,5 +1,7 @@
 import { HonoBase } from '../hono-base.ts'
 import { LinearRouter } from '../router/linear-router/index.ts'
+import { SmartRouter } from '../router/smart-router/index.ts'
+import { TrieRouter } from '../router/trie-router/index.ts'
 import type { Env } from '../types.ts'
 
 export class Hono<E extends Env = Env, S = {}, BasePath extends string = '/'> extends HonoBase<
@@ -9,6 +11,8 @@ export class Hono<E extends Env = Env, S = {}, BasePath extends string = '/'> ex
 > {
   constructor(init: Partial<Pick<Hono, 'getPath'> & { strict: boolean }> = {}) {
     super(init)
-    this.router = new LinearRouter()
+    this.router = new SmartRouter({
+      routers: [new LinearRouter(), new TrieRouter()],
+    })
   }
 }
