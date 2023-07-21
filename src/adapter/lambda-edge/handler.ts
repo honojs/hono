8d65333ac546c6a9d4203c2ba20faa639378b146
watany <76135106+watany-dev@users.noreply.github.com>
2023-07-21 16:59:32 +0900

    feat(adaptor): Lambda@Edge supports response callbacks (#1251)
    * change header
    
    * fixed
    
    * simple test
    
    * minify
    
    * refactor
    
    * fix
    
    * binding response
    
    * fix
    
    * fix
    
    * remove fix
    
    * remove lowercase

--- src/adapter/lambda-edge/handler.ts
+++ src/adapter/lambda-edge/handler.ts
@@ -45,6 +45,12 @@ export interface CloudFrontRequest {
   }
 }
 
+export interface CloudFrontResponse {
+  headers: CloudFrontHeaders
+  status: string
+  statusDescription?: string
+}
+
 interface CloudFrontConfig {
   distributionDomainName: string
   distributionId: string
@@ -56,6 +62,7 @@ interface CloudFrontEvent {
   cf: {
     config: CloudFrontConfig
     request: CloudFrontRequest
+    response?: CloudFrontResponse
   }
 }
 
@@ -87,6 +94,13 @@ interface CloudFrontResult {
  * Accepts events from 'Lambda@Edge' event
  * https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/lambda-event-structure.html
  */
+const convertHeaders = (headers: Headers): CloudFrontHeaders => {
+  const cfHeaders: CloudFrontHeaders = {}
+  headers.forEach((value, key) => {
+    cfHeaders[key.toLowerCase()] = [{ key: key.toLowerCase(), value }]
+  })
+  return cfHeaders
+}
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const handle = (app: Hono<any>) => {
@@ -95,12 +109,14 @@ export const handle = (app: Hono<any>) => {
     context?: CloudFrontContext,
     callback?: Callback
   ): Promise<CloudFrontResult> => {
-    const req = createRequest(event)
-    const res = await app.fetch(req, {
+    const res = await app.fetch(createRequest(event), {
       event,
       context,
-      callback,
+      callback: (err: Error | null, result?: CloudFrontResult | CloudFrontRequest) => {
+        callback?.(err, result)
+      },
       request: event.Records[0].cf.request,
+      response: event.Records[0].cf.response,
     })
     return createResult(res)
   }
@@ -108,47 +124,35 @@ export const handle = (app: Hono<any>) => {
 
 const createResult = async (res: Response): Promise<CloudFrontResult> => {
   const isBase64Encoded = isContentTypeBinary(res.headers.get('content-type') || '')
-
   const body = isBase64Encoded ? encodeBase64(await res.arrayBuffer()) : await res.text()
-
-  const headers: { [header: string]: { key: string; value: string }[] } = {}
-
-  res.headers.forEach((value, key) => {
-    headers[key.toLowerCase()] = [{ key: key.toLowerCase(), value }]
-  })
 
   return {
     status: res.status.toString(),
-    headers,
+    headers: convertHeaders(res.headers),
     body,
   }
 }
 
 const createRequest = (event: CloudFrontEdgeEvent) => {
-  const queryString = extractQueryString(event)
+  const queryString = event.Records[0].cf.request.querystring
   const urlPath = `https://${event.Records[0].cf.config.distributionDomainName}${event.Records[0].cf.request.uri}`
   const url = queryString ? `${urlPath}?${queryString}` : urlPath
 
   const headers = new Headers()
-  for (const [k, v] of Object.entries(event.Records[0].cf.request.headers)) {
+  Object.entries(event.Records[0].cf.request.headers).forEach(([k, v]) => {
     v.forEach((header) => headers.set(k, header.value))
-  }
-  const method = event.Records[0].cf.request.method
-  const requestInit: RequestInit = {
-    headers,
-    method,
-  }
+  })
 
   const requestBody = event.Records[0].cf.request.body
-  requestInit.body =
-    requestBody?.encoding === 'base64' && requestBody?.data
+  const body = requestBody?.encoding === 'base64' && requestBody?.data
       ? Buffer.from(requestBody.data, 'base64')
       : requestBody?.data
-  return new Request(url, requestInit)
-}
 
-const extractQueryString = (event: CloudFrontEdgeEvent) => {
-  return event.Records[0].cf.request.querystring
+  return new Request(url, {
+    headers,
+    method: event.Records[0].cf.request.method,
+    body,
+  })
 }
 
 export const isContentTypeBinary = (contentType: string) => {
