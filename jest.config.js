d5c1201e6988143d8593b5d28413a675a4d89c85
Yusuke Wada <yusuke@kamawada.com>
2023-07-28 16:26:04 +0900

    chore: tweak `jest.config.js` (#1274)
    * chore: tweak `jest.config.js`
    
    * fix for lagon test

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,8 @@
 export default {
-  testMatch: ['**/test/**/*.+(ts|tsx|js)', '**/src/**/(*.)+(spec|test).+(ts|tsx|js)'],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/src/**/(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   testEnvironment: 'miniflare',
-  testEnvironmentOptions: {
-    /*
-    bindings: {
-      __STATIC_CONTENT: {
-        get: (key) => {
-          const table = { 'index.abcdef.index': 'This is index' }
-          return table[key]
-        },
-      },
-      __STATIC_CONTENT_MANIFEST: JSON.stringify({
-        'index.index': 'index.abcdef.index',
-      }),
-    },
-    kvNamespaces: ['TEST_NAMESPACE'],
-    */
-  },
 }
