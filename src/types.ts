246d4e3f372de9e2df92037a7c9aab6d265302b9
Yusuke Wada <yusuke@cloudflare.com>
2023-08-03 22:57:09 +0900

    fix(types): add a missing handler type

--- src/types.ts
+++ src/types.ts
@@ -126,8 +126,14 @@ export interface HandlerInterface<
 
   ////  app.get(path, ...handlers[])
 
-  // app.get(path, handler, handler)
+  // app.get(path, handler)
+  <P extends string, O = {}, I extends Input = {}>(path: P, handler: H<E, P, I, O>): Hono<
+    E,
+    RemoveBlankRecord<S | Schema<M, MergePath<BasePath, P>, I['in'], O>>,
+    BasePath
+  >
 
+  // app.get(path, handler, handler)
   <P extends string, O = {}, I extends Input = {}>(
     path: P,
     ...handlers: [H<E, P, I, O>, H<E, P, I, O>]
