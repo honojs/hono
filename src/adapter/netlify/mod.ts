cca6fedb17dfb24d99925dfd105132b27d2a6b28
Yusuke Wada <yusuke@kamawada.com>
2023-08-08 09:35:05 +0900

    fix(netlify): fix import paths (#1304)

--- src/adapter/netlify/mod.ts
+++ src/adapter/netlify/mod.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-export { handle } from './handler.ts'
-export type { Env } from './handler.ts'
+export { handle } from './handler'
+export type { Env } from './handler'
