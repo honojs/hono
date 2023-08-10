3587c4630b7938eab1595a15f09dad0465d83cfb
watany <76135106+watany-dev@users.noreply.github.com>
2023-08-10 15:44:35 +0000

    v0.1

---
+++ src/middleware/secure-header/index.test.ts
@@ -0,0 +1,41 @@
+import { Hono } from '../../hono'
+import { secureHeader } from '.'
+
+describe('Secure Header Middleware', () => {
+
+  it('all headers enabled', async () => {
+    const app = new Hono()
+    app.use('*', secureHeader())
+    app.get('/test', async (ctx) => {
+      return ctx.text('test')
+    })
+
+    const req = new Request('http://localhost/test', { method: 'GET' })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('X-Frame-Options')).toEqual('DENY')
+    expect(res.headers.get('Strict-Transport-Security')).toEqual('max-age=15552000; includeSubDomains')
+    expect(res.headers.get('X-Download-Options')).toEqual('noopen')
+    expect(res.headers.get('X-XSS-Protection')).toEqual('0')
+    expect(res.headers.get('X-Powered-By')).toBeNull()
+  })
+
+  it('specific headers disabled', async () => {
+    const app = new Hono()
+    app.use('*', secureHeader({ xFrameOptions: false, xXssProtection: false }))
+    app.get('/test', async (ctx) => {
+      return ctx.text('test')
+    })
+
+    const req = new Request('http://localhost/test', { method: 'GET' })
+    const res = await app.request(req)
+    expect(res).not.toBeNull()
+    expect(res.status).toBe(200)
+    expect(res.headers.get('X-Frame-Options')).toBeNull()
+    expect(res.headers.get('Strict-Transport-Security')).toEqual('max-age=15552000; includeSubDomains')
+    expect(res.headers.get('X-Download-Options')).toEqual('noopen')
+    expect(res.headers.get('X-XSS-Protection')).toBeNull()
+    expect(res.headers.get('X-Powered-By')).toBeNull()
+  })
+})
---
+++ src/middleware/secure-header/index.ts
@@ -0,0 +1,32 @@
+import type { MiddlewareHandler } from '../../types'
+
+interface SecureHeaderOptions {
+  xFrameOptions?: boolean
+  strictTransportSecurity?: boolean
+  xDownloadOptions?: boolean
+  xXssProtection?: boolean
+}
+
+export const secureHeader = (options?: SecureHeaderOptions): MiddlewareHandler => {
+  return async (ctx, next) => {
+    if (options?.xFrameOptions !== false) {
+      ctx.res.headers.set('X-Frame-Options', 'DENY')
+    }
+    
+    if (options?.strictTransportSecurity !== false) {
+      ctx.res.headers.set('Strict-Transport-Security', 'max-age=15552000; includeSubDomains')
+    }
+    
+    if (options?.xDownloadOptions !== false) {
+      ctx.res.headers.set('X-Download-Options', 'noopen')
+    }
+
+    if (options?.xXssProtection !== false) {
+      ctx.res.headers.set('X-XSS-Protection', '0')
+    }
+
+    ctx.res.headers.delete('X-Powered-By')
+
+    await next()
+  }
+}
