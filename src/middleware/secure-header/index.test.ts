214b668ce52589da474ae9ecec4be8f5863f7925
watany <76135106+watany-dev@users.noreply.github.com>
2023-08-11 11:38:20 +0000

    v0.4

--- src/middleware/secure-header/index.test.ts
+++ src/middleware/secure-header/index.test.ts
@@ -2,7 +2,6 @@ import { Hono } from '../../hono'
 import { secureHeader } from '.'
 
 describe('Secure Header Middleware', () => {
-
   it('all headers enabled', async () => {
     const app = new Hono()
     app.use('*', secureHeader())
@@ -15,12 +14,14 @@ describe('Secure Header Middleware', () => {
     expect(res).not.toBeNull()
     expect(res.status).toBe(200)
     expect(res.headers.get('X-Frame-Options')).toEqual('SAMEORIGIN')
-    expect(res.headers.get('Strict-Transport-Security')).toEqual('max-age=15552000; includeSubDomains')
+    expect(res.headers.get('Strict-Transport-Security')).toEqual(
+      'max-age=15552000; includeSubDomains'
+    )
     expect(res.headers.get('X-Download-Options')).toEqual('noopen')
     expect(res.headers.get('X-XSS-Protection')).toEqual('0')
     expect(res.headers.get('X-Powered-By')).toBeNull()
     expect(res.headers.get('X-DNS-Prefetch-Control')).toEqual('off')
-    expect(res.headers.get('X-Content-Type-Options')).toEqual('nosniff')  
+    expect(res.headers.get('X-Content-Type-Options')).toEqual('nosniff')
   })
 
   it('specific headers disabled', async () => {
@@ -35,10 +36,11 @@ describe('Secure Header Middleware', () => {
     expect(res).not.toBeNull()
     expect(res.status).toBe(200)
     expect(res.headers.get('X-Frame-Options')).toBeNull()
-    expect(res.headers.get('Strict-Transport-Security')).toEqual('max-age=15552000; includeSubDomains')
+    expect(res.headers.get('Strict-Transport-Security')).toEqual(
+      'max-age=15552000; includeSubDomains'
+    )
     expect(res.headers.get('X-Download-Options')).toEqual('noopen')
     expect(res.headers.get('X-XSS-Protection')).toBeNull()
     expect(res.headers.get('X-Powered-By')).toBeNull()
   })
-
 })
--- src/middleware/secure-header/index.ts
+++ src/middleware/secure-header/index.ts
@@ -1,39 +1,49 @@
 import type { MiddlewareHandler } from '../../types'
 
 interface SecureHeaderOptions {
-  xContentTypeOptions?: boolean;
-  xDnsPrefetchControl?: boolean;
-  xFrameOptions?: boolean;
-  strictTransportSecurity?: boolean;
-  xDownloadOptions?: boolean;
-  xXssProtection?: boolean;
+  crossOriginResourcePolicy?: boolean
+  crossOriginOpenerPolicy?: boolean
+  referrerPolicy?: boolean
+  strictTransportSecurity?: boolean
+  xContentTypeOptions?: boolean
+  xDnsPrefetchControl?: boolean
+  xDownloadOptions?: boolean
+  xFrameOptions?: boolean
+  xPermittedCrossDomainPolicies?: boolean
+  xXssProtection?: boolean
 }
 
 const HEADER_MAP = {
+  crossOriginResourcePolicy: ['Cross-Origin-Resource-Policy', 'same-origin'],
+  crossOriginOpenerPolicy: ['Cross-Origin-Opener-Policy', 'same-origin'],
   referrerPolicy: ['Referrer-Policy', 'no-referrer'],
-  strictTransportSecurity: ['Strict-Transport-Security', 'max-age=15552000; includeSubDomains'],
+  strictTransportSecurity: ['Strict-Transport-Security', 'max-age=31536000; includeSubDomains'],
   xContentTypeOptions: ['X-Content-Type-Options', 'nosniff'],
   xDnsPrefetchControl: ['X-DNS-Prefetch-Control', 'off'],
   xDownloadOptions: ['X-Download-Options', 'noopen'],
   xFrameOptions: ['X-Frame-Options', 'SAMEORIGIN'],
   xPermittedCrossDomainPolicies: ['X-Permitted-Cross-Domain-Policies', 'none'],
-  xXssProtection: ['X-XSS-Protection', '0']
+  xXssProtection: ['X-XSS-Protection', '0'],
 }
 
 const DEFAULT_OPTIONS = {
+  crossOriginResourcePolicy: true,
+  crossOriginOpenerPolicy: true,
+  referrerPolicy: true,
+  strictTransportSecurity: true,
   xContentTypeOptions: true,
   xDnsPrefetchControl: true,
-  xFrameOptions: true,
-  strictTransportSecurity: true,
   xDownloadOptions: true,
-  xXssProtection: true
+  xFrameOptions: true,
+  xPermittedCrossDomainPolicies: true,
+  xXssProtection: true,
 }
 
 export const secureHeader = (customOptions?: Partial<SecureHeaderOptions>): MiddlewareHandler => {
   const options = { ...DEFAULT_OPTIONS, ...customOptions }
   const headersToSet = Object.entries(HEADER_MAP)
-  .filter(([key]) => options[key as keyof SecureHeaderOptions])
-  .map(([key, value]) => value)
+    .filter(([key]) => options[key as keyof SecureHeaderOptions])
+    .map(([key, value]) => value)
 
   return async (ctx, next) => {
     headersToSet.forEach(([header, value]) => {
