27b2d5b29314b6f9b43e288c98a6d904923a0003
Yusuke Wada <yusuke@cloudflare.com>
2023-08-19 18:29:44 +0900

    mv jsx

--- src/jsx/index.test.tsx
+++ src/jsx/index.test.tsx
@@ -1,5 +1,5 @@
-import { Hono } from '../../hono'
-import { html } from '../html/index'
+import { Hono } from '../hono'
+import { html } from '../middleware/html'
 import { jsx, memo, Fragment } from './index'
 
 interface SiteData {
