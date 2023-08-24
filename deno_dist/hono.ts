6034dd7f7f846fc71d8d1ae398db1305dcd5392f
Yusuke Wada <yusuke@kamawada.com>
2023-08-24 15:36:49 +0900

    fix(types): remove type-errors for routes (#1371)
    * fix(types): remove type-errors for routes
    
    * denoify
    
    * fixed the client and the presets
    
    * rename

--- deno_dist/hono.ts
+++ deno_dist/hono.ts
@@ -2,13 +2,13 @@ import { HonoBase } from './hono-base.ts'
 import { RegExpRouter } from './router/reg-exp-router/index.ts'
 import { SmartRouter } from './router/smart-router/index.ts'
 import { TrieRouter } from './router/trie-router/index.ts'
-import type { Env } from './types.ts'
+import type { Env, Schema } from './types.ts'
 
-export class Hono<E extends Env = Env, S = {}, BasePath extends string = '/'> extends HonoBase<
-  E,
-  S,
-  BasePath
-> {
+export class Hono<
+  E extends Env = Env,
+  S extends Schema = {},
+  BasePath extends string = '/'
+> extends HonoBase<E, S, BasePath> {
   constructor(init: Partial<Pick<Hono, 'router' | 'getPath'> & { strict: boolean }> = {}) {
     super(init)
     this.router =
--- src/hono.ts
+++ src/hono.ts
@@ -2,13 +2,13 @@ import { HonoBase } from './hono-base'
 import { RegExpRouter } from './router/reg-exp-router'
 import { SmartRouter } from './router/smart-router'
 import { TrieRouter } from './router/trie-router'
-import type { Env } from './types'
+import type { Env, Schema } from './types'
 
-export class Hono<E extends Env = Env, S = {}, BasePath extends string = '/'> extends HonoBase<
-  E,
-  S,
-  BasePath
-> {
+export class Hono<
+  E extends Env = Env,
+  S extends Schema = {},
+  BasePath extends string = '/'
+> extends HonoBase<E, S, BasePath> {
   constructor(init: Partial<Pick<Hono, 'router' | 'getPath'> & { strict: boolean }> = {}) {
     super(init)
     this.router =
