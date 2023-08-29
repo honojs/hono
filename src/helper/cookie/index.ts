f1129ce045fb48dd304190a4050e21306602d980
uaichat <129906704+uaichat@users.noreply.github.com>
2023-08-29 19:34:39 -0300

    fix: error to retrieve cookie
    TypeError: Cannot read properties of undefined (reading 'split')

--- src/helper/cookie/index.ts
+++ src/helper/cookie/index.ts
@@ -16,11 +16,11 @@ export const getCookie: GetCookie = (c, key?) => {
   const cookie = c.req.raw.headers.get('Cookie')
   if (typeof key === 'string') {
     if (!cookie) return undefined
-    const obj = parse(cookie)
+    const obj = parse(cookie, key)
     return obj[key]
   }
   if (!cookie) return {}
-  const obj = parse(cookie)
+  const obj = parse(cookie, key)
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return obj as any
 }
