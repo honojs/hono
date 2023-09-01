7b68ff61a4f441f415035e892135bd4bdaf17644
renzor <117486829+renzor-fist@users.noreply.github.com>
2023-09-01 16:15:59 -0600

    feat: Add $url method to hono client to extract an endpoint's url (#1384)

--- deno_dist/client/types.ts
+++ deno_dist/client/types.ts
@@ -17,6 +17,8 @@ type ClientRequest<S extends Schema> = {
           options?: ClientRequestOptions
         ) => Promise<ClientResponse<O>>
     : never
+} & {
+  $url: () => Promise<URL>
 }
 
 type BlankRecordToNever<T> = T extends Record<infer R, unknown>
