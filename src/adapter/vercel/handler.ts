24386ab26f0192f2205edb3fc006d8bd63c880fa
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2023-09-05 19:18:09 -0700

    vercel pass RequestContext

--- src/adapter/vercel/handler.ts
+++ src/adapter/vercel/handler.ts
@@ -2,6 +2,6 @@
 import type { Hono } from '../../hono'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const handle = (app: Hono<any, any, any>) => (req: Request) => {
-  return app.fetch(req)
+export const handle = (app: Hono<any, any, any>) => (req: Request, event: any) => {
+  return app.fetch(req, {}, event)
 }
