eb5ce8dd96f65bc31e34a7519b33980d8cf32cba
Yusuke Wada <yusuke@cloudflare.com>
2023-09-07 11:54:13 +0900

    denoify-ignore

--- src/adapter/vercel/handler.ts
+++ src/adapter/vercel/handler.ts
@@ -1,5 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 // @denoify-ignore
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Hono } from '../../hono'
 
 export const handle =
