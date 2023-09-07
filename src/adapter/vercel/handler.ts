98fe07ee21c60ae4666e18aa760ec971afcc4bac
Yusuke Wada <yusuke@cloudflare.com>
2023-09-07 11:47:35 +0900

    add an appropriate type and make it throws error is `passThroughOnException` is called

--- src/adapter/vercel/handler.ts
+++ src/adapter/vercel/handler.ts
@@ -1,7 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // @denoify-ignore
 import type { Hono } from '../../hono'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const handle = (app: Hono<any, any, any>) => (req: Request, event: any) => {
-  return app.fetch(req, {}, event)
-}
+export const handle =
+  (app: Hono<any, any, any>) =>
+  (req: Request, requestContext: Omit<ExecutionContext, 'passThroughOnException'>) => {
+    return app.fetch(
+      req,
+      {},
+      {
+        waitUntil: requestContext?.waitUntil,
+        passThroughOnException: () => {
+          throw new Error('`passThroughOnException` is not implemented in the Vercel')
+        },
+      }
+    )
+  }
