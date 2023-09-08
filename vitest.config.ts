b7161090ea42420767c3e51460032d621e651088
Yusuke Wada <yusuke@kamawada.com>
2023-09-08 18:10:12 +0900

    test: ignore `sandbox` dir (#1424)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     globals: true,
     include: ['**/src/**/(*.)+(spec|test).+(ts|tsx|js)'],
+    exclude: [...configDefaults.exclude, '**/sandbox/**'],
     environment: 'miniflare',
     coverage: {
       provider: 'v8',
