a77c6d871299bdae97641a389f5c3c36df40268b
watany <76135106+watany-dev@users.noreply.github.com>
2023-09-13 14:32:19 +0000

    refactor

--- src/helper/adapter/index.ts
+++ src/helper/adapter/index.ts
@@ -5,61 +5,39 @@ export const env = <T extends Record<string, string>, C extends Context = Contex
 ): T & C['env'] => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const global = globalThis as any
+  const globalEnv = global?.process?.env as T;
 
-  if (
-    c.runtime === 'bun' ||
-    c.runtime === 'node' ||
-    c.runtime === 'edge-light' ||
-    c.runtime === 'lagon'
-  ) {
-    return global?.process?.env as T
+  const runtime = getRuntimeKey()
+
+  const runtimeEnvHandlers: Record<string, () => T> = {
+    'bun': () => globalEnv,
+    'node': () => globalEnv,
+    'edge-light': () => globalEnv,
+    'lagon': () => globalEnv,
+    'deno': () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      return Deno.env.toObject()
+    },
+    'workerd': () => c.env,
+    'fastly': () => ({} as T),
+    'other': () => ({} as T),
   }
-  if (c.runtime === 'deno') {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    return Deno.env.toObject()
-  }
-  if (c.runtime === 'workerd') {
-    return c.env
-  }
-  if (c.runtime === 'fastly') {
-    // On Fastly Compute@Edge, you can use the ConfigStore to manage user-defined data.
-    return {} as T
-  }
-  return {} as T
+
+  return runtimeEnvHandlers[runtime]()
 }
 
 export const getRuntimeKey = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const global = globalThis as any
 
-  if (global?.Deno !== undefined) {
-    return 'deno'
-  }
-
-  if (global?.Bun !== undefined) {
-    return 'bun'
-  }
-
-  if (typeof global?.WebSocketPair === 'function') {
-    return 'workerd'
-  }
-
-  if (typeof global?.EdgeRuntime === 'string') {
-    return 'edge-light'
-  }
-
-  if (global?.fastly !== undefined) {
-    return 'fastly'
-  }
-
-  if (global?.__lagon__ !== undefined) {
-    return 'lagon'
-  }
-
-  if (global?.process?.release?.name === 'node') {
-    return 'node'
-  }
-
+  if (global?.Deno !== undefined) return 'deno'
+  if (global?.Bun !== undefined) return 'bun'
+  if (typeof global?.WebSocketPair === 'function') return 'workerd'
+  if (typeof global?.EdgeRuntime === 'string') return 'edge-light'
+  if (global?.fastly !== undefined) return 'fastly'
+  if (global?.__lagon__ !== undefined) return 'lagon'
+  if (global?.process?.release?.name === 'node') return 'node'
+  
   return 'other'
 }
