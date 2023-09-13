073a443d370c88bf26ac26b848e2dca30a19d36c
hagishi <fujiyama@kakumei.jp>
2023-09-13 17:26:39 +0900

    feat: add test client helper

---
+++ src/helper/testing/index.ts
@@ -0,0 +1,18 @@
+import { hc as HonoClient } from '../../client'
+import type { Hono } from '../../hono'
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ExtractEnv<T> = T extends Hono<infer E, any, any> ? E : never
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const hc = <T extends Hono<any, any, any>>(
+  app: T,
+  Env?: ExtractEnv<T>['Bindings'] | {},
+  executionCtx?: ExecutionContext
+) => {
+  const customFetch = (input: RequestInfo | URL, init?: RequestInit) => {
+    return app.request(input, init, Env, executionCtx)
+  }
+
+  return HonoClient<typeof app>('', { fetch: customFetch })
+}
