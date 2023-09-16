6e14daeaf002df7e9ed751b0955fbdf24b194ae9
sor4chi <sorakawa0218@gmail.com>
2023-09-16 14:42:33 +0900

    test: use `stream.sleep` for waiting

--- src/context.test.ts
+++ src/context.test.ts
@@ -264,7 +264,7 @@ describe('Pass a ResponseInit to respond methods', () => {
     const res = c.streamText(async (stream) => {
       for (let i = 0; i < 3; i++) {
         await stream.write(`${i}`)
-        await new Promise((res) => setTimeout(res, 1000))
+        await stream.sleep(1000)
       }
     })
     if (!res.body) {
@@ -282,7 +282,7 @@ describe('Pass a ResponseInit to respond methods', () => {
     const res = c.stream(async (stream) => {
       for (let i = 0; i < 3; i++) {
         await stream.write(new Uint8Array([i]))
-        await new Promise((res) => setTimeout(res, 1000))
+        await stream.sleep(1000)
       }
     })
     if (!res.body) {
