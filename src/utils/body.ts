01cc8da9a2701bb5ad019012d38517eafa8cf42f
sor4chi <sorakawa0218@gmail.com>
2023-09-30 23:01:14 +0900

    feat: add "all" option to parseBody

--- src/utils/body.ts
+++ src/utils/body.ts
@@ -1,9 +1,31 @@
 import type { HonoRequest } from '../request'
 
-export type BodyData = Record<string, string | string[] | File>
+export type BodyData = Record<string, string | File | (string | File)[]>
+export type ParseBodyOptions = {
+  /**
+   * Parse all fields with multiple values should be parsed as an array.
+   * @default false
+   * @example
+   * ```ts
+   * const data = new FormData()
+   * data.append('file', 'aaa')
+   * data.append('file', 'bbb')
+   * ```
+   *
+   * If `all` is `false`:
+   * parseBody should return `{ file: 'bbb' }`
+   *
+   * If `all` is `true`:
+   * parseBody should return `{ file: ['aaa', 'bbb'] }`
+   */
+  all?: boolean
+}
 
 export const parseBody = async <T extends BodyData = BodyData>(
-  request: HonoRequest | Request
+  request: HonoRequest | Request,
+  options: ParseBodyOptions = {
+    all: false,
+  }
 ): Promise<T> => {
   let body: BodyData = {}
   const contentType = request.headers.get('Content-Type')
@@ -17,16 +39,17 @@ export const parseBody = async <T extends BodyData = BodyData>(
     if (formData) {
       const form: BodyData = {}
       formData.forEach((value, key) => {
-        if (key.slice(-2) === '[]') {
-          if (!form[key]) {
-            form[key] = [value.toString()]
-          } else {
-            if (Array.isArray(form[key])) {
-              ;(form[key] as string[]).push(value.toString())
-            }
-          }
-        } else {
+        const isParseAll = options.all || key.slice(-2) === '[]'
+
+        if (!isParseAll) {
           form[key] = value
+          return
+        }
+
+        form[key] ??= []
+        if (Array.isArray(form[key])) {
+          ;(form[key] as (string | File)[]).push(value)
+          return
         }
       })
       body = form
