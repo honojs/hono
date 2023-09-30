43749a0efdc8722745cc882940885365047d5fcf
watany <76135106+watany-dev@users.noreply.github.com>
2023-09-30 21:55:35 +0900

    feat(adaptor): Accessable Lambda "Context" (#1523)
    * adding lambda-context
    
    * format
    
    * type any
    
    * ignore denoify
    
    * export type

--- src/adapter/aws-lambda/index.ts
+++ src/adapter/aws-lambda/index.ts
@@ -1,3 +1,4 @@
 // @denoify-ignore
 export { handle } from './handler'
 export type { ApiGatewayRequestContext, LambdaFunctionUrlRequestContext } from './custom-context'
+export type { LambdaContext } from './types'
---
+++ src/adapter/aws-lambda/types.ts
@@ -0,0 +1,47 @@
+// @denoify-ignore
+export interface CognitoIdentity {
+  cognitoIdentityId: string
+  cognitoIdentityPoolId: string
+}
+
+export interface ClientContext {
+  client: ClientContextClient
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Custom?: any
+  env: ClientContextEnv
+}
+
+export interface ClientContextClient {
+  installationId: string
+  appTitle: string
+  appVersionName: string
+  appVersionCode: string
+  appPackageName: string
+}
+
+export interface ClientContextEnv {
+  platformVersion: string
+  platform: string
+  make: string
+  model: string
+  locale: string
+}
+
+/**
+ * {@link Handler} context parameter.
+ * See {@link https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html AWS documentation}.
+ */
+export interface LambdaContext {
+  callbackWaitsForEmptyEventLoop: boolean
+  functionName: string
+  functionVersion: string
+  invokedFunctionArn: string
+  memoryLimitInMB: string
+  awsRequestId: string
+  logGroupName: string
+  logStreamName: string
+  identity?: CognitoIdentity | undefined
+  clientContext?: ClientContext | undefined
+
+  getRemainingTimeInMillis(): number
+}
