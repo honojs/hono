6b1c0a616d5c33344af3a61b3c98240f58bbc55f
Yusuke Wada <yusuke@kamawada.com>
2023-10-05 00:50:15 +0900

    fix(vercel): pass request context

--- src/adapter/vercel/handler.ts
+++ src/adapter/vercel/handler.ts
@@ -2,17 +2,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Hono } from '../../hono'
 
-export const handle =
-  (app: Hono<any, any, any>) =>
-  (req: Request, requestContext: Omit<ExecutionContext, 'passThroughOnException'>) => {
-    return app.fetch(
-      req,
-      {},
-      {
-        waitUntil: requestContext?.waitUntil,
-        passThroughOnException: () => {
-          throw new Error('`passThroughOnException` is not implemented in the Vercel')
-        },
-      }
-    )
-  }
+export const handle = (app: Hono<any, any, any>) => (req: Request, requestContext: FetchEvent) => {
+  return app.fetch(req, {}, requestContext as any)
+}
