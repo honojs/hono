13aef49dc00892a005ef18b4d1b04f502a17773d
Arunava Basu <arunavabasuDev@gmail.com>
2023-10-04 14:34:44 +0000

    feat:yarn denoify

--- deno_dist/helper/factory/index.ts
+++ deno_dist/helper/factory/index.ts
@@ -6,6 +6,6 @@ import type { Env, Input, MiddlewareHandler } from '../../types.ts'
  * The API might be changed.
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const middleware = <E extends Env = any, P extends string = any, I extends Input = {}>(
+export const createMiddleware = <E extends Env = any, P extends string = any, I extends Input = {}>(
   middleware: MiddlewareHandler<E, P, I>
 ) => middleware
