e3cc268ed09c395619a0e1c6739c4b1abe5ad31f
Yusuke Wada <yusuke@kamawada.com>
2023-10-15 22:21:14 +0900

    refactor: remove a not used type

--- src/router/trie-router/node.ts
+++ src/router/trie-router/node.ts
@@ -1,4 +1,4 @@
-import type { Result, Params } from '../../router'
+import type { Params } from '../../router'
 import { METHOD_NAME_ALL } from '../../router'
 import type { Pattern } from '../../utils/url'
 import { splitPath, splitRoutingPath, getPattern } from '../../utils/url'
