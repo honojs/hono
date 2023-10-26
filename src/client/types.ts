a654842d56f4123b19de712a0d078d8402467869
Andrew <slider7259@gmail.com>
2023-10-26 11:07:25 +0200

    fix(client/types): make args param optional if the input has no required keys (#1618)
    * feat(types.ts): add RequiredKeysOf and HasRequiredKeys utility types
    
    * fix(client/types.ts): make args param optional if input has no required keys
    
    * chore(types.ts): move RequiredKeysOf and HasRequiredKeys types to utils/types.ts
    
    * chore(types.ts): rewrite ClientRequest type
    
    * chore(types.ts): fix formatting
    
    * chore(types.ts): denoify

--- src/client/types.ts
+++ src/client/types.ts
@@ -1,6 +1,6 @@
 import type { Hono } from '../hono'
 import type { Schema } from '../types'
-import type { RemoveBlankRecord } from '../utils/types'
+import type { HasRequiredKeys } from '../utils/types'
 
 type HonoRequest = typeof Hono.prototype['request']
 
@@ -16,9 +16,11 @@ export type ClientRequestOptions<T = unknown> = keyof T extends never
 
 type ClientRequest<S extends Schema> = {
   [M in keyof S]: S[M] extends { input: infer R; output: infer O }
-    ? RemoveBlankRecord<Required<R>> extends never
-      ? (args?: {}, options?: ClientRequestOptions) => Promise<ClientResponse<O>>
-      : (args: R, options?: ClientRequestOptions) => Promise<ClientResponse<O>>
+    ? R extends object
+      ? HasRequiredKeys<R> extends true
+        ? (args: R, options?: ClientRequestOptions) => Promise<ClientResponse<O>>
+        : (args?: R, options?: ClientRequestOptions) => Promise<ClientResponse<O>>
+      : never
     : never
 } & {
   $url: () => URL
