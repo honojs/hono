12e5f5c356b9ce049a250a0cd072af0b8081a44d
Taku Amano <taku@taaas.jp>
2023-11-09 00:22:23 +0900

    feat(jsx-renderer): Support "stream" option in jsxRenderer.

--- src/middleware/jsx-renderer/index.ts
+++ src/middleware/jsx-renderer/index.ts
@@ -1,7 +1,9 @@
 import type { Context, Renderer } from '../../context'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { html, raw } from '../../helper/html'
 import { jsx, createContext, useContext } from '../../jsx'
 import type { FC, JSXNode } from '../../jsx'
+import { renderToReadableStream } from '../../jsx/streaming'
 import type { Env, Input, MiddlewareHandler } from '../../types'
 
 export const RequestContext = createContext<Context | null>(null)
@@ -12,6 +14,7 @@ type PropsForRenderer = [...Required<Parameters<Renderer>>] extends [unknown, in
 
 type RendererOptions = {
   docType?: boolean | string
+  stream?: boolean | Record<string, string>
 }
 
 const createRenderer =
@@ -23,16 +26,25 @@ const createRenderer =
         : options?.docType === true
         ? '<!DOCTYPE html>'
         : ''
+    const body = html`${raw(docType)}${jsx(
+      RequestContext.Provider,
+      { value: c },
+      (component ? component({ children, ...(props || {}) }) : children) as any
+    )}`
 
-    return c.html(
-      (docType +
-        /* eslint-disable @typescript-eslint/no-explicit-any */
-        jsx(
-          RequestContext.Provider,
-          { value: c },
-          (component ? component({ children, ...(props || {}) }) : children) as any
-        )) as any
-    )
+    if (options?.stream) {
+      return c.body(renderToReadableStream(body), {
+        headers:
+          options.stream === true
+            ? {
+                'Transfer-Encoding': 'chunked',
+                'Content-Type': 'text/html; charset=UTF-8',
+              }
+            : options.stream,
+      })
+    } else {
+      return c.html(body)
+    }
   }
 
 export const jsxRenderer =
