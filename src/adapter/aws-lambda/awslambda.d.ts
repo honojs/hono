882383272ceeec23ab806743c3b0a4a43d998515
watany <76135106+watany-dev@users.noreply.github.com>
2023-11-11 00:44:20 +0900

    fix(Adaptor): Lambda Response Streaming With Content Type  (#1673)
    * lambda 0.1
    
    * ok stream setted content-type
    
    * fix d.ts

--- src/adapter/aws-lambda/awslambda.d.ts
+++ src/adapter/aws-lambda/awslambda.d.ts
@@ -5,6 +5,14 @@ import type { LambdaContext, Handler } from './types'
 
 declare global {
   namespace awslambda {
+    // Note: Anticipated logic for AWS
+    // https://github.com/aws/aws-lambda-nodejs-runtime-interface-client/blob/main/src/HttpResponseStream.js
+    export class HttpResponseStream {
+      static from(
+        underlyingStream: NodeJS.WritableStream,
+        prelude: Record<string, unknown>
+      ): NodeJS.WritableStream
+    }
     function streamifyResponse(
       f: (
         event: any,
