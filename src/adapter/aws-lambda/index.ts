1fe5a3692f65ee1555a4f91f81d8784d6e35b7f6
juho <hietala@gmail.com>
2023-11-16 13:33:40 +0100

    Export APIGatewayProxyResult from aws-lambda
    Adds an export for `APIGatewayProxyResult` so that it can be referenced from a handler:
    
    ```ts
    export const handler: APIGatewayProxyResult = handle(app);
    ```
    
    Without it exposed, TS complains:
    
    ```
    Exported variable 'handler' has or is using name 'APIGatewayProxyResult' from external module "node_modules/.pnpm/hono@3.10.1/node_modules/hono/dist/types/adapter/aws-lambda/handler" but cannot be named.
    ```

--- src/adapter/aws-lambda/index.ts
+++ src/adapter/aws-lambda/index.ts
@@ -1,4 +1,4 @@
 // @denoify-ignore
-export { handle, streamHandle } from './handler'
+export { handle, streamHandle, type APIGatewayProxyResult } from './handler'
 export type { ApiGatewayRequestContext, LambdaFunctionUrlRequestContext } from './custom-context'
 export type { LambdaContext } from './types'
