f450ab00d259763711f9234215f753639307ce96
Yusuke Wada <yusuke@kamawada.com>
2023-11-28 21:40:39 +0900

    create `Factory` class

--- src/helper/factory/index.test.ts
+++ src/helper/factory/index.test.ts
@@ -1,6 +1,9 @@
+import { expectTypeOf } from 'vitest'
 import { hc } from '../../client'
 import { Hono } from '../../index'
-import { createMiddleware } from './index'
+import type { ExtractSchema } from '../../types'
+import { validator } from '../../validator'
+import { createMiddleware, createFactory } from './index'
 
 describe('createMiddleware', () => {
   type Env = { Variables: { foo: string } }
@@ -28,5 +31,154 @@ describe('createMiddleware', () => {
     const client = hc<typeof route>('http://localhost')
     const url = client.message.$url()
     expect(url.pathname).toBe('/message')
+  })
+})
+
+// A fake function for testing types.
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function extractSchema<T = unknown>(_: T): ExtractSchema<T> {
+  return 0 as ExtractSchema<T>
+}
+
+describe('createHandler', () => {
+  const mw = (message: string) =>
+    createMiddleware(async (c, next) => {
+      await next()
+      c.header('x-message', message)
+    })
+
+  describe('Basic', () => {
+    const factory = createFactory()
+    const app = new Hono()
+
+    const handlersA = factory.createHandlers((c) => {
+      return c.text('A')
+    })
+    app.get('/a', ...handlersA)
+
+    const handlersB = factory.createHandlers(mw('B'), (c) => {
+      return c.text('B')
+    })
+    app.get('/b', ...handlersB)
+
+    it('Should return 200 response - GET /a', async () => {
+      const res = await app.request('/a')
+      expect(res.status).toBe(200)
+      expect(await res.text()).toBe('A')
+    })
+
+    it('Should return 200 response with a custom header - GET /b', async () => {
+      const res = await app.request('/b')
+      expect(res.status).toBe(200)
+      expect(res.headers.get('x-message')).toBe('B')
+      expect(await res.text()).toBe('B')
+    })
+  })
+
+  describe('Types', () => {
+    type Env = { Variables: { foo: string } }
+
+    const factory = createFactory<Env>()
+    const app = new Hono<Env>()
+
+    const handlersA = factory.createHandlers(
+      validator('query', () => {
+        return {
+          page: '1',
+        }
+      }),
+      (c) => {
+        const foo = c.var.foo
+        const { page } = c.req.valid('query')
+        return c.jsonT({ page, foo })
+      }
+    )
+    const routesA = app.get('/posts', ...handlersA)
+
+    type ExpectedA = {
+      '/posts': {
+        $get: {
+          input: {
+            query: {
+              page: string
+            }
+          }
+          output: {
+            foo: string
+            page: string
+          }
+        }
+      }
+    }
+
+    it('Should return correct types', () => {
+      expectTypeOf(extractSchema(routesA)).toEqualTypeOf<ExpectedA>()
+    })
+  })
+
+  // It's difficult to cover all possible patterns,
+  // so these tests will only cover the minimal cases.
+
+  describe('Types - Complex', () => {
+    type Env = { Variables: { foo: string } }
+
+    const factory = createFactory<Env>()
+    const app = new Hono<Env>()
+
+    const handlersA = factory.createHandlers(
+      validator('header', () => {
+        return {
+          auth: 'token',
+        }
+      }),
+      validator('query', () => {
+        return {
+          page: '1',
+        }
+      }),
+      validator('json', () => {
+        return {
+          id: 123,
+        }
+      }),
+      (c) => {
+        const foo = c.var.foo
+        const { auth } = c.req.valid('header')
+        const { page } = c.req.valid('query')
+        const { id } = c.req.valid('json')
+        return c.jsonT({ auth, page, foo, id })
+      }
+    )
+    const routesA = app.get('/posts', ...handlersA)
+
+    type ExpectedA = {
+      '/posts': {
+        $get: {
+          input: {
+            header: {
+              auth: string
+            }
+          } & {
+            query: {
+              page: string
+            }
+          } & {
+            json: {
+              id: number
+            }
+          }
+          output: {
+            auth: string
+            page: string
+            foo: string
+            id: number
+          }
+        }
+      }
+    }
+
+    it('Should return correct types', () => {
+      expectTypeOf(extractSchema(routesA)).toEqualTypeOf<ExpectedA>()
+    })
   })
 })
--- src/helper/factory/index.ts
+++ src/helper/factory/index.ts
@@ -1,87 +1,73 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Env, Input, MiddlewareHandler, H, HandlerResponse } from '../../types'
 
-export const createMiddleware = <E extends Env = any, P extends string = any, I extends Input = {}>(
-  middleware: MiddlewareHandler<E, P, I>
-) => middleware
+export class Factory<E extends Env = any, P extends string = any> {
+  createMiddleware = <I extends Input = {}>(middleware: MiddlewareHandler<E, P, I>) => middleware
 
-export function createHandlers<E extends Env = any, P extends string = any, I extends Input = {}>(
-  handler1: H<E, P, I>
-): [H<E, P, I>]
+  createHandlers<I extends Input = {}>(handler1: H<E, P, I>): [H<E, P, I>]
+  createHandlers<I extends Input = {}, I2 extends Input = I, R extends HandlerResponse<any> = any>(
+    handler1: H<E, P, I, R>,
+    handler2: H<E, P, I2, R>
+  ): [H<E, P, I, R>, H<E, P, I2, R>]
 
-export function createHandlers<
-  E extends Env = any,
-  P extends string = any,
-  I extends Input = {},
-  I2 extends Input = I
->(handler1: H<E, P, I>, handler2: H<E, P, I2>): [H<E, P, I>, H<E, P, I2>]
+  createHandlers<
+    I extends Input = {},
+    I2 extends Input = I,
+    I3 extends Input = I & I2,
+    R extends HandlerResponse<any> = any
+  >(
+    handler1: H<E, P, I, R>,
+    handler2: H<E, P, I2, R>,
+    handler3: H<E, P, I3, R>
+  ): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>]
 
-export function createHandlers<
-  E extends Env = any,
-  P extends string = any,
-  I extends Input = {},
-  I2 extends Input = I,
-  I3 extends Input = I & I2,
-  R extends HandlerResponse<any> = any
->(
-  handler1: H<E, P, I, R>,
-  handler2: H<E, P, I2, R>,
-  handler3: H<E, P, I3, R>
-): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>]
+  createHandlers<
+    I extends Input = {},
+    I2 extends Input = I,
+    I3 extends Input = I & I2,
+    I4 extends Input = I & I2 & I3,
+    R extends HandlerResponse<any> = any
+  >(
+    handler1: H<E, P, I, R>,
+    handler2: H<E, P, I2, R>,
+    handler3: H<E, P, I3, R>,
+    handler4: H<E, P, I4, R>
+  ): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>, H<E, P, I4, R>]
 
-export function createHandlers<
-  E extends Env = any,
-  P extends string = any,
-  I extends Input = {},
-  I2 extends Input = I,
-  I3 extends Input = I & I2,
-  I4 extends Input = I & I2 & I3,
-  R extends HandlerResponse<any> = any
->(
-  handler1: H<E, P, I, R>,
-  handler2: H<E, P, I2, R>,
-  handler3: H<E, P, I3, R>,
-  handler4: H<E, P, I4, R>
-): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>, H<E, P, I4, R>]
+  createHandlers<
+    I extends Input = {},
+    I2 extends Input = I,
+    I3 extends Input = I & I2,
+    I4 extends Input = I & I2 & I3,
+    R extends HandlerResponse<any> = any
+  >(
+    handler1: H<E, P, I, R>,
+    handler2: H<E, P, I2, R>,
+    handler3: H<E, P, I3, R>,
+    handler4: H<E, P, I4, R>
+  ): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>, H<E, P, I4, R>]
 
-export function createHandlers<
-  E extends Env = any,
-  P extends string = any,
-  I extends Input = {},
-  I2 extends Input = I,
-  I3 extends Input = I & I2,
-  I4 extends Input = I & I2 & I3,
-  R extends HandlerResponse<any> = any
->(
-  handler1: H<E, P, I, R>,
-  handler2: H<E, P, I2, R>,
-  handler3: H<E, P, I3, R>,
-  handler4: H<E, P, I4, R>
-): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>, H<E, P, I4, R>]
+  createHandlers<
+    I extends Input = {},
+    I2 extends Input = I,
+    I3 extends Input = I & I2,
+    I4 extends Input = I & I2 & I3,
+    I5 extends Input = I & I2 & I3 & I4,
+    R extends HandlerResponse<any> = any
+  >(
+    handler1: H<E, P, I, R>,
+    handler2: H<E, P, I2, R>,
+    handler3: H<E, P, I3, R>,
+    handler4: H<E, P, I4, R>,
+    handler5: H<E, P, I5, R>
+  ): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>, H<E, P, I4, R>, H<E, P, I5, R>]
 
-export function createHandlers<
-  E extends Env = any,
-  P extends string = any,
-  I extends Input = {},
-  I2 extends Input = I,
-  I3 extends Input = I & I2,
-  I4 extends Input = I & I2 & I3,
-  I5 extends Input = I & I2 & I3 & I4,
-  R extends HandlerResponse<any> = any
->(
-  handler1: H<E, P, I, R>,
-  handler2: H<E, P, I2, R>,
-  handler3: H<E, P, I3, R>,
-  handler4: H<E, P, I4, R>,
-  handler5: H<E, P, I5, R>
-): [H<E, P, I, R>, H<E, P, I2, R>, H<E, P, I3, R>, H<E, P, I4, R>, H<E, P, I5, R>]
-
-export function createHandlers(
-  handler1: H,
-  handler2?: H,
-  handler3?: H,
-  handler4?: H,
-  handler5?: H
-) {
-  return [handler1, handler2, handler3, handler4, handler5]
+  createHandlers(handler1: H, handler2?: H, handler3?: H, handler4?: H, handler5?: H) {
+    return [handler1, handler2, handler3, handler4, handler5]
+  }
 }
+
+export const createFactory = <E extends Env = any, P extends string = any>() => new Factory<E, P>()
+export const createMiddleware = <E extends Env = any, P extends string = any, I extends Input = {}>(
+  middleware: MiddlewareHandler
+) => createFactory<E, P>().createMiddleware<I>(middleware)
