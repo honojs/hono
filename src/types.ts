7749a5dca03ad6af464f1a5ccfeaa13e97c85178
Taku Amano <taku@taaas.jp>
2023-11-29 19:18:09 +0900

    refactor: "status" is not needed in TypedResponse

--- src/types.ts
+++ src/types.ts
@@ -521,7 +521,6 @@ export type MergePath<A extends string, B extends string> = A extends ''
 export type TypedResponse<T = unknown> = {
   data: T
   format: 'json' // Currently, support only `json` with `c.jsonT()`
-  status: StatusCode
 }
 
 type ExtractResponseData<T> = T extends Promise<infer T2>
