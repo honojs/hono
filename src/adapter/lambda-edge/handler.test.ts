af3447dfc738f0c281a40d8e8a59d168edf5011a
Elias Brange <elias.brange@gmail.com>
2023-12-06 23:44:25 +0100

    fix: add missing tail to svg regex (#1788)

--- src/adapter/lambda-edge/handler.test.ts
+++ src/adapter/lambda-edge/handler.test.ts
@@ -4,6 +4,8 @@ describe('isContentTypeBinary', () => {
   it('Should determine whether it is binary', () => {
     expect(isContentTypeBinary('image/png')).toBe(true)
     expect(isContentTypeBinary('font/woff2')).toBe(true)
+    expect(isContentTypeBinary('image/svg+xml')).toBe(false)
+    expect(isContentTypeBinary('image/svg+xml; charset=UTF-8')).toBe(false)
     expect(isContentTypeBinary('text/plain')).toBe(false)
     expect(isContentTypeBinary('text/plain; charset=UTF-8')).toBe(false)
     expect(isContentTypeBinary('text/css')).toBe(false)
