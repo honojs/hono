5f0d5ee2649d8fa5fa4b2476d7bedb9953a5cef7
ysugimoto <sugimoto@wnotes.net>
2023-12-17 16:54:59 +0900

    reflect reviews and solve problem

--- runtime_tests/fastly/vitest.config.ts
+++ runtime_tests/fastly/vitest.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
-import fastlyCompute from "vite-plugin-fastly-js-compute";
+import fastlyCompute from 'vite-plugin-fastly-js-compute'
 
 export default defineConfig({
   plugins: [fastlyCompute()],
