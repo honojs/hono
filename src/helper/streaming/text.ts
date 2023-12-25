1b28081aa341241058b352d922e62051bbb209d9
sor4chi <sorakawa0218@gmail.com>
2023-12-25 02:47:47 +0000

    fix: match the header notation with that of `streamSSE

--- src/helper/streaming/text.ts
+++ src/helper/streaming/text.ts
@@ -4,8 +4,8 @@ import type { StreamingApi } from '../../utils/stream'
 import { stream } from '.'
 
 export const streamText = (c: Context, cb: (stream: StreamingApi) => Promise<void>): Response => {
-  c.header('content-type', TEXT_PLAIN)
-  c.header('x-content-type-options', 'nosniff')
-  c.header('transfer-encoding', 'chunked')
+  c.header('Content-Type', TEXT_PLAIN)
+  c.header('X-Content-Type-Options', 'nosniff')
+  c.header('Transfer-Encoding', 'chunked')
   return stream(c, cb)
 }
