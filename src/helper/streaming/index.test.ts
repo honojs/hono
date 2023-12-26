7768865e7ab9d388e1c5c2a45879d18adb43f354
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-12-27 06:55:24 +0900

    fix: move `c.stream*` to helper (#1846)
    * refactor: move streaming helper to `streaming/sse.ts`
    
    * feat: add streamSSE's export in streaming handler
    
    * feat: move `stream` and `streamText` to streaming helper
    
    * chore: add deprecated expression for `c.stream` and `c.streamText`
    
    * fix: use `stream` helper in streamSSE
    
    * refactor: move `streamText` to `text.ts`
    
    * test: add some case to `stream` and `streamText`
    
    * test: refactor `streamSSE` case for uniformity
    
    * chore: denoify
    
    * fix: update jsdoc's deprecated description, simplify `c.stream` and `c.streamText`
    
    * fix: match the header notation with that of `streamSSE
    
    * chore: denoify
    
    * refactor: remove unnecesary export

--- src/helper/streaming/index.test.ts
+++ src/helper/streaming/index.test.ts
@@ -1,45 +1,28 @@
-import { Hono } from '../../hono'
-import { streamSSE } from '.'
+import { Context } from '../../context'
+import { HonoRequest } from '../../request'
+import { stream } from '.'
 
-describe('SSE Streaming headers', () => {
+describe('Basic Streaming Helper', () => {
+  const req = new HonoRequest(new Request('http://localhost/'))
+  let c: Context
+  beforeEach(() => {
+    c = new Context(req)
+  })
+
   it('Check SSE Response', async () => {
-    const app = new Hono()
-    app.get('/sse', async (c) => {
-      return streamSSE(c, async (stream) => {
-        let id = 0
-        const maxIterations = 5
-
-        while (id < maxIterations) {
-          const message = `Message\nIt is ${id}`
-          await stream.writeSSE({ data: message, event: 'time-update', id: String(id++) })
-          await stream.sleep(100)
-        }
-      })
+    const res = stream(c, async (stream) => {
+      for (let i = 0; i < 3; i++) {
+        await stream.write(new Uint8Array([i]))
+        await stream.sleep(1)
+      }
     })
-
-    const res = await app.request('/sse')
-    expect(res).not.toBeNull()
-    expect(res.status).toBe(200)
-    expect(res.headers.get('Transfer-Encoding')).toEqual('chunked')
-    expect(res.headers.get('Content-Type')).toEqual('text/event-stream')
-    expect(res.headers.get('Cache-Control')).toEqual('no-cache')
-    expect(res.headers.get('Connection')).toEqual('keep-alive')
-
     if (!res.body) {
       throw new Error('Body is null')
     }
     const reader = res.body.getReader()
-    const decoder = new TextDecoder()
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
       const { value } = await reader.read()
-      const decodedValue = decoder.decode(value)
-
-      // Check the structure and content of the SSE message
-      let expectedValue = 'event: time-update\n'
-      expectedValue += 'data: Message\n'
-      expectedValue += `data: It is ${i}\n`
-      expectedValue += `id: ${i}\n\n`
-      expect(decodedValue).toBe(expectedValue)
+      expect(value).toEqual(new Uint8Array([i]))
     }
   })
 })
