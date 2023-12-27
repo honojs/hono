556138e283d6b289dcf07322b9a7cf4010a94f2b
watany <76135106+watany-dev@users.noreply.github.com>
2023-12-27 01:22:40 +0000

    check & denoify

--- deno_dist/helper/streaming/stream.ts
+++ deno_dist/helper/streaming/stream.ts
@@ -1,5 +1,5 @@
-import type { Context } from '../../context'
-import { StreamingApi } from '../../utils/stream'
+import type { Context } from '../../context.ts'
+import { StreamingApi } from '../../utils/stream.ts'
 
 export const stream = (c: Context, cb: (stream: StreamingApi) => Promise<void>): Response => {
   const { readable, writable } = new TransformStream()
