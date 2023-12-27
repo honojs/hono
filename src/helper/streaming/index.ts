db4413534caa23002aa8627dbb2f951485de6996
watany <76135106+watany-dev@users.noreply.github.com>
2023-12-27 01:13:18 +0000

    refactor

--- src/helper/streaming/index.ts
+++ src/helper/streaming/index.ts
@@ -1,12 +1,3 @@
-import type { Context } from '../../context'
-import { StreamingApi } from '../../utils/stream'
-
-export const stream = (c: Context, cb: (stream: StreamingApi) => Promise<void>): Response => {
-  const { readable, writable } = new TransformStream()
-  const stream = new StreamingApi(writable)
-  cb(stream).finally(() => stream.close())
-  return c.newResponse(readable)
-}
-
+export { stream } from './stream'
 export { streamSSE } from './sse'
 export { streamText } from './text'
