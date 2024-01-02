538c93cf1df8dd39a5b73282f6713ac6109df6a5
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-02 23:18:04 +0900

    feat(streaming): implement `stream.onAbort` (#1871)
    * feat: extend `StreamAPI` for response interception and adapt stream handler for it
    
    * chore: denoify

--- src/utils/stream.test.ts
+++ src/utils/stream.test.ts
@@ -3,8 +3,8 @@ import { StreamingApi } from './stream'
 describe('StreamingApi', () => {
   it('write(string)', async () => {
     const { readable, writable } = new TransformStream()
-    const api = new StreamingApi(writable)
-    const reader = readable.getReader()
+    const api = new StreamingApi(writable, readable)
+    const reader = api.responseReadable.getReader()
     api.write('foo')
     expect((await reader.read()).value).toEqual(new TextEncoder().encode('foo'))
     api.write('bar')
@@ -13,8 +13,8 @@ describe('StreamingApi', () => {
 
   it('write(Uint8Array)', async () => {
     const { readable, writable } = new TransformStream()
-    const api = new StreamingApi(writable)
-    const reader = readable.getReader()
+    const api = new StreamingApi(writable, readable)
+    const reader = api.responseReadable.getReader()
     api.write(new Uint8Array([1, 2, 3]))
     expect((await reader.read()).value).toEqual(new Uint8Array([1, 2, 3]))
     api.write(new Uint8Array([4, 5, 6]))
@@ -23,8 +23,8 @@ describe('StreamingApi', () => {
 
   it('writeln(string)', async () => {
     const { readable, writable } = new TransformStream()
-    const api = new StreamingApi(writable)
-    const reader = readable.getReader()
+    const api = new StreamingApi(writable, readable)
+    const reader = api.responseReadable.getReader()
     api.writeln('foo')
     expect((await reader.read()).value).toEqual(new TextEncoder().encode('foo\n'))
     api.writeln('bar')
@@ -44,7 +44,7 @@ describe('StreamingApi', () => {
 
     const { readable: receiverReadable, writable: receiverWritable } = new TransformStream()
 
-    const api = new StreamingApi(receiverWritable)
+    const api = new StreamingApi(receiverWritable, receiverReadable)
 
     // pipe readable to api in other scope
     ;(async () => {
@@ -52,34 +52,48 @@ describe('StreamingApi', () => {
     })()
 
     // read data from api
-    const reader = receiverReadable.getReader()
+    const reader = api.responseReadable.getReader()
     expect((await reader.read()).value).toEqual(new TextEncoder().encode('foo'))
     expect((await reader.read()).value).toEqual(new TextEncoder().encode('bar'))
   })
 
   it('close()', async () => {
     const { readable, writable } = new TransformStream()
-    const api = new StreamingApi(writable)
-    const reader = readable.getReader()
+    const api = new StreamingApi(writable, readable)
+    const reader = api.responseReadable.getReader()
     await api.close()
     expect((await reader.read()).done).toBe(true)
   })
 
   it('should not throw an error in write()', async () => {
-    const { writable } = new TransformStream()
-    const api = new StreamingApi(writable)
+    const { readable, writable } = new TransformStream()
+    const api = new StreamingApi(writable, readable)
     await api.close()
     const write = () => api.write('foo')
     expect(write).not.toThrow()
   })
 
   it('should not throw an error in close()', async () => {
-    const { writable } = new TransformStream()
-    const api = new StreamingApi(writable)
+    const { readable, writable } = new TransformStream()
+    const api = new StreamingApi(writable, readable)
     const close = async () => {
       await api.close()
       await api.close()
     }
     expect(close).not.toThrow()
+  })
+
+  it('onAbort()', async () => {
+    const { readable, writable } = new TransformStream()
+    const handleAbort1 = vi.fn()
+    const handleAbort2 = vi.fn()
+    const api = new StreamingApi(writable, readable)
+    api.onAbort(handleAbort1)
+    api.onAbort(handleAbort2)
+    expect(handleAbort1).not.toBeCalled()
+    expect(handleAbort2).not.toBeCalled()
+    await api.responseReadable.cancel()
+    expect(handleAbort1).toBeCalled()
+    expect(handleAbort2).toBeCalled()
   })
 })
