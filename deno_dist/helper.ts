bcde6b1924da89a58d5f11af03dbfa897179dab0
Yusuke Wada <yusuke@kamawada.com>
2024-01-03 21:57:43 +0900

    denoify

--- deno_dist/helper.ts
+++ deno_dist/helper.ts
@@ -1,6 +1,7 @@
 // This file is for Deno to import helpers from `hono/helper.ts`.
 export * from './helper/adapter/index.ts'
 export * from './helper/cookie/index.ts'
+export * from './helper/css/index.ts'
 export * from './helper/factory/index.ts'
 export * from './helper/html/index.ts'
 export * from './helper/streaming/index.ts'
