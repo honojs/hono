c2e3bd262c23969fcfe9f2173408b9d48d0a173d
watany <76135106+watany-dev@users.noreply.github.com>
2024-01-04 10:16:49 +0000

    enhanced type

--- deno_dist/helper/ssg/index.ts
+++ deno_dist/helper/ssg/index.ts
@@ -2,14 +2,21 @@ import { Buffer } from "node:buffer";
 import * as path from 'node:path'
 import { inspectRoutes } from '../../helper/dev/index.ts'
 import type { Hono } from '../../hono.ts'
-import type { Env } from '../../types.ts'
+import type { Env, Schema } from '../../types.ts'
 
 interface FileSystemModule {
   writeFile(path: string, data: string | Buffer): Promise<void>
   mkdir(path: string, options: { recursive: boolean }): Promise<void>
 }
 
-export const toSsg = async (app: Hono<Env>, fsModule: FileSystemModule) => {
+export const toSsg = async <
+  E extends Env = Env,
+  S extends Schema = {},
+  BasePath extends string = '/'
+>(
+  app: Hono<E, S, BasePath>,
+  fsModule: FileSystemModule
+) => {
   const routes = inspectRoutes(app).map((route) => route.path)
   const baseURL = 'http://localhost'
 
@@ -30,22 +37,3 @@ export const toSsg = async (app: Hono<Env>, fsModule: FileSystemModule) => {
 
   console.log('Static site generation completed.')
 }
-
-// export const toSsg = async (app: Hono<Env>, fsModule: FileSystemModule) => {
-//   const routes = inspectRoutes(app).map((route) => route.path)
-//   const baseURL = 'http://localhost'
-
-//   for (const route of routes) {
-//     const url = new URL(route, baseURL).toString()
-//     const response = await app.fetch(new Request(url))
-//     const html = await response.text()
-
-//     const filePath = path.join('./static', route === '/' ? 'index.html' : route + '.html')
-//     const dirPath = path.dirname(filePath)
-
-//     await fsModule.mkdir(dirPath, { recursive: true })
-//     await fsModule.writeFile(filePath, html)
-//     console.log(`File written: ${filePath}`)
-//   }
-//   console.log('Static site generation completed.')
-// }
--- src/helper/ssg/index.ts
+++ src/helper/ssg/index.ts
@@ -1,14 +1,21 @@
 import * as path from 'path'
 import { inspectRoutes } from '../../helper/dev'
 import type { Hono } from '../../hono'
-import type { Env } from '../../types'
+import type { Env, Schema } from '../../types'
 
 interface FileSystemModule {
   writeFile(path: string, data: string | Buffer): Promise<void>
   mkdir(path: string, options: { recursive: boolean }): Promise<void>
 }
 
-export const toSsg = async (app: Hono<Env>, fsModule: FileSystemModule) => {
+export const toSsg = async <
+  E extends Env = Env,
+  S extends Schema = {},
+  BasePath extends string = '/'
+>(
+  app: Hono<E, S, BasePath>,
+  fsModule: FileSystemModule
+) => {
   const routes = inspectRoutes(app).map((route) => route.path)
   const baseURL = 'http://localhost'
 
@@ -29,22 +36,3 @@ export const toSsg = async (app: Hono<Env>, fsModule: FileSystemModule) => {
 
   console.log('Static site generation completed.')
 }
-
-// export const toSsg = async (app: Hono<Env>, fsModule: FileSystemModule) => {
-//   const routes = inspectRoutes(app).map((route) => route.path)
-//   const baseURL = 'http://localhost'
-
-//   for (const route of routes) {
-//     const url = new URL(route, baseURL).toString()
-//     const response = await app.fetch(new Request(url))
-//     const html = await response.text()
-
-//     const filePath = path.join('./static', route === '/' ? 'index.html' : route + '.html')
-//     const dirPath = path.dirname(filePath)
-
-//     await fsModule.mkdir(dirPath, { recursive: true })
-//     await fsModule.writeFile(filePath, html)
-//     console.log(`File written: ${filePath}`)
-//   }
-//   console.log('Static site generation completed.')
-// }
