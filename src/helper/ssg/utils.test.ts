5f060d00cd901a03468b12510380c1c05ebd8f9d
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-01-14 12:11:58 +0000

    add

--- src/helper/ssg/utils.test.ts
+++ src/helper/ssg/utils.test.ts
@@ -3,9 +3,9 @@ import { joinPaths, dirname } from './utils'
 
 describe('joinPath', () => {
   it('Should joined path is valid.', () => {
-    expect(joinPaths('test', 'test2')).toBe('test/test2')
-    expect(joinPaths('test', 'test2', '../test3')).toBe('test/test3')
-    expect(joinPaths('.', '../')).toBe('..')
+    expect(joinPaths('test', 'test2')).toBe('test/test2') // single and single
+    expect(joinPaths('test', 'test2', '../test3')).toBe('test/test3') // single and single and parent and single
+    expect(joinPaths('.', '../')).toBe('..') // dot and parent
     expect(joinPaths('test/', 'test2/')).toBe('test/test2') // trailing slashes
     expect(joinPaths('./test', './test2')).toBe('test/test2') // dot and slash
     expect(joinPaths('', 'test')).toBe('test') // empty path
