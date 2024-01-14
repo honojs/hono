315a0b28ad6d9cdeef2931affdd2358f9c18610a
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-01-14 16:06:08 +0000

    refactor util & fix bug

--- src/helper/ssg/utils.test.ts
+++ src/helper/ssg/utils.test.ts
@@ -11,6 +11,8 @@ describe('joinPath', () => {
     expect(joinPaths('', 'test')).toBe('test') // empty path
     expect(joinPaths('/test', '/test2')).toBe('/test/test2') // root path
     expect(joinPaths('../', 'test')).toBe('../test') // parent and single
+    expect(joinPaths('test', '..', 'test2')).toBe('test2') // single triple dot and single
+    expect(joinPaths('test', '...', 'test2')).toBe('test/.../test2') // single triple dot and single
   })
   it('Should windows path is valid.', () => {
     expect(joinPaths('a\\b\\c', 'd\\e')).toBe('a/b/c/d/e')
