f9ad2faf905cf88925dc716d40a14dc434cbbe8c
Yusuke Wada <yusuke@kamawada.com>
2024-01-15 17:46:00 +0900

    feat!(cloudflare-workers): make `manifest` required
    Co-authored-by: Kei Kamikawa <Code-Hex@users.noreply.github.com>

--- src/adapter/cloudflare-workers/serve-static-module.ts
+++ src/adapter/cloudflare-workers/serve-static-module.ts
@@ -1,16 +1,10 @@
 // @denoify-ignore
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
 // For ES module mode
-import manifest from '__STATIC_CONTENT_MANIFEST'
 import type { Env } from '../../types'
 import type { ServeStaticOptions } from './serve-static'
 import { serveStatic } from './serve-static'
 
-const module = <E extends Env = Env>(
-  options: Omit<ServeStaticOptions<E>, 'namespace'> = { root: '' }
-) => {
-  options.manifest ??= manifest
+const module = <E extends Env = Env>(options: Omit<ServeStaticOptions<E>, 'namespace'>) => {
   return serveStatic<E>(options)
 }
 
