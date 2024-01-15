000bc615e4ad4009c4cf629a64aa85cfc49d04ae
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-01-15 09:29:06 +0900

    fix(ssg): fix path parser bug & refactor code (#1976)
    * refactor util & fix bug
    
    * add test case
    
    * denoify
    
    * format & denoify
    
    * faster & add test & denoify

--- src/helper/ssg/utils.ts
+++ src/helper/ssg/utils.ts
@@ -8,27 +8,40 @@ export const dirname = (path: string) => {
   return splitedPath.slice(0, -1).join('/') // Windows supports slash path
 }
 
-export const joinPaths = (...paths: string[]) => {
-  paths = paths.map((path) => {
-    return path.replace(/(\\)/g, '/').replace(/\/$/g, '')
-  })
-  const resultPaths: string[] = []
-  for (let path of paths.join('/').split('/')) {
+const normalizePath = (path: string) => {
+  return path.replace(/(\\)/g, '/').replace(/\/$/g, '')
+}
+
+const handleDotDot = (resultPaths: string[]) => {
+  if (resultPaths.length === 0) {
+    resultPaths.push('..')
+  } else {
+    resultPaths.pop()
+  }
+}
+
+const handleNonDot = (path: string, resultPaths: string[]) => {
+  path = path.replace(/^\.(?!.)/, '')
+  if (path !== '') {
+    resultPaths.push(path)
+  }
+}
+
+const handleSegments = (paths: string[], resultPaths: string[]) => {
+  for (const path of paths) {
     // Handle `..` or `../`
     if (path === '..') {
-      if (resultPaths.length === 0) {
-        resultPaths.push('..')
-      } else {
-        resultPaths.pop()
-      }
-      continue
+      handleDotDot(resultPaths)
     } else {
       // Handle `.` or `./`
-      path = path.replace(/^\./g, '')
-    }
-    if (path !== '') {
-      resultPaths.push(path)
+      handleNonDot(path, resultPaths)
     }
   }
+}
+
+export const joinPaths = (...paths: string[]) => {
+  paths = paths.map(normalizePath)
+  const resultPaths: string[] = []
+  handleSegments(paths.join('/').split('/'), resultPaths)
   return (paths[0][0] === '/' ? '/' : '') + resultPaths.join('/')
 }
