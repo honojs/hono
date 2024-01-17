f23248397eda262d2e2fb962624c45ae629e245a
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-01-17 17:39:20 +0900

    feat: Added `ssgParams` middleware (#1960)
    * feat(ssg): add ssgParams middleware
    
    * chore: add test code
    
    * chore: `yarn denoify`
    
    * fix(codestyle): import order
    
    * chore: Ran pretter to pass CI test
    
    * chore: reran denoify to pass CI test
    
    * feat: Remove hybrid feature from `ssgParams`
    
    * feat: Add context argment to generateParams
    
    * chore: Format code
    
    * chore: denoify
    
    * fix: module not found error when running on Deno
    
    * refactor: remove argment `options`
    
    * chore: denoify
    
    * refactor: add test for `ssgParams`
    
    * fix: syntax error
    
    * chore: denoify
    
    * chore: format code
    
    * chore: denoify

--- deno_dist/helper/ssg/index.ts
+++ deno_dist/helper/ssg/index.ts
@@ -1,7 +1,9 @@
 import { Buffer } from "node:buffer";
+import { replaceUrlParam } from '../../client/utils.ts'
+import type { Context } from '../../context.ts'
 import { inspectRoutes } from '../../helper/dev/index.ts'
 import type { Hono } from '../../hono.ts'
-import type { Env, Schema } from '../../types.ts'
+import type { Env, MiddlewareHandler, Schema } from '../../types.ts'
 import { getExtension } from '../../utils/mime.ts'
 import { joinPaths, dirname } from './utils.ts'
 
@@ -62,6 +64,28 @@ const determineExtension = (mimeType: string): string => {
   }
 }
 
+interface SSGParam {
+  [key: string]: string
+}
+type SSGParams = SSGParam[]
+interface SSGParamsMiddleware {
+  (generateParams: (c: Context) => SSGParams | Promise<SSGParams>): MiddlewareHandler
+}
+type AddedSSGDataRequest = Request & {
+  ssgParams?: SSGParams
+}
+/**
+ * Define SSG Route
+ */
+export const ssgParams: SSGParamsMiddleware = (generateParams) => async (c, next) => {
+  ;(c.req.raw as AddedSSGDataRequest).ssgParams = await generateParams(c)
+  await next()
+}
+
+export interface ToSSGOptions {
+  dir?: string
+}
+
 /**
  * @experimental
  * `fetchRoutesContent` is an experimental feature.
@@ -80,12 +104,25 @@ export const fetchRoutesContent = async <
   for (const route of inspectRoutes(app)) {
     if (route.isMiddleware || isDynamicRoute(route.path)) continue
 
-    const url = new URL(route.path, baseURL).toString()
-    const response = await app.fetch(new Request(url))
-    const mimeType = response.headers.get('Content-Type')?.split(';')[0] || 'text/plain'
-    const content = await parseResponseContent(response)
+    // GET Route Info
+    const thisRouteBaseURL = new URL(route.path, baseURL).toString()
+    const forGetInfoURLRequest = new Request(thisRouteBaseURL) as AddedSSGDataRequest
+    await app.fetch(forGetInfoURLRequest)
 
-    htmlMap.set(route.path, { content, mimeType })
+    if (!forGetInfoURLRequest.ssgParams) {
+      forGetInfoURLRequest.ssgParams = [{}]
+    }
+
+    for (const param of forGetInfoURLRequest.ssgParams) {
+      const replacedUrlParam = replaceUrlParam(route.path, param)
+      const response = await app.request(replacedUrlParam)
+      const mimeType = response.headers.get('Content-Type')?.split(';')[0] || 'text/plain'
+      const content = await parseResponseContent(response)
+      htmlMap.set(replacedUrlParam, {
+        mimeType,
+        content,
+      })
+    }
   }
 
   return htmlMap
@@ -136,7 +173,7 @@ export interface ToSSGInterface<
   (
     app: Hono<E, S, BasePath>,
     fsModule: FileSystemModule,
-    options?: { dir?: string }
+    options?: ToSSGOptions
   ): Promise<ToSSGResult>
 }
 
