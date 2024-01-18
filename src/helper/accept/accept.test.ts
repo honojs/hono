a6557ac1af063df74204e55dd09fe0c24616ef30
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-01-18 16:48:28 +0900

    test: add some usage cases for accept helper

--- src/helper/accept/accept.test.ts
+++ src/helper/accept/accept.test.ts
@@ -1,3 +1,4 @@
+import { Hono } from '../..'
 import type { Accept, MatchAcceptConfig, MatchAcceptOptions } from './accept'
 import { parseAccept, defaultMatch, matchAccept } from './accept'
 
@@ -103,5 +104,73 @@ describe('matchAccept', () => {
     }
     const result = matchAccept(c, options)
     expect(result).toBe('application/xml')
+  })
+})
+
+describe('Usage', () => {
+  test('decide compression by Accept-Encoding header', async () => {
+    const app = new Hono()
+    app.get('/compressed', async (c) => {
+      const encoding = matchAccept(c, {
+        header: 'Accept-Encoding',
+        supports: ['gzip', 'deflate'],
+        default: 'identity',
+      })
+      const COMPRESS_DATA = 'COMPRESS_DATA'
+      const readable = new ReadableStream({
+        start(controller) {
+          controller.enqueue(new TextEncoder().encode(COMPRESS_DATA))
+          controller.close()
+        },
+      })
+
+      if (encoding === 'gzip') {
+        c.header('Content-Encoding', 'gzip')
+        return c.body(readable.pipeThrough(new CompressionStream('gzip')))
+      }
+      if (encoding === 'deflate') {
+        c.header('Content-Encoding', 'deflate')
+        return c.body(readable.pipeThrough(new CompressionStream('deflate')))
+      }
+
+      c.body(COMPRESS_DATA)
+    })
+
+    const req1 = await app.request('/compressed', { headers: { 'Accept-Encoding': 'deflate' } })
+    const req2 = await app.request('/compressed', { headers: { 'Accept-Encoding': 'gzip' } })
+    const req3 = await app.request('/compressed', {
+      headers: { 'Accept-Encoding': 'gzip;q=0.5,deflate' },
+    })
+    const req4 = await app.request('/compressed', { headers: { 'Accept-Encoding': 'br' } })
+
+    expect(req1.headers.get('Content-Encoding')).toBe('deflate')
+    expect(req2.headers.get('Content-Encoding')).toBe('gzip')
+    expect(req3.headers.get('Content-Encoding')).toBe('deflate')
+    expect(req4.headers.get('Content-Encoding')).toBeNull()
+  })
+
+  test('decide language by Accept-Language header', async () => {
+    const app = new Hono()
+    const SUPPORTED_LANGS = ['en', 'ja', 'zh']
+    app.get('/*', async (c) => {
+      const lang = matchAccept(c, {
+        header: 'Accept-Language',
+        supports: SUPPORTED_LANGS,
+        default: 'en',
+      })
+      const isLangedPath = SUPPORTED_LANGS.some((l) => c.req.path.startsWith(`/${l}`))
+      if (isLangedPath) {
+        return c.body(`lang: ${lang}`)
+      }
+
+      return c.redirect(`/${lang}${c.req.path}`)
+    })
+
+    const req1 = await app.request('/foo', { headers: { 'Accept-Language': 'en=0.8,ja' } })
+    const req2 = await app.request('/en/foo', { headers: { 'Accept-Language': 'en' } })
+
+    expect(req1.status).toBe(302)
+    expect(req1.headers.get('Location')).toBe('/ja/foo')
+    expect(await req2.text()).toBe('lang: en')
   })
 })
