9154714789db4dfda960ef8e1d4798abb67bd8c9
watany <76135106+watany-dev@users.noreply.github.com>
2024-01-19 16:04:15 +0900

    feat!(deno): move middleware to helper (#2027)
    * move v4
    
    * revert
    
    * denoify

--- deno_dist/middleware.ts
+++ deno_dist/middleware.ts
@@ -2,12 +2,10 @@
 export * from './middleware/basic-auth/index.ts'
 export * from './middleware/bearer-auth/index.ts'
 export * from './middleware/cache/index.ts'
-export * from './helper/cookie/index.ts' // will be moved to helper.ts in v4
 export * from './middleware/compress/index.ts'
 export * from './middleware/cors/index.ts'
 export * from './middleware/csrf/index.ts'
 export * from './middleware/etag/index.ts'
-export * from './helper/html/index.ts' // will be moved to helper.ts in v4
 export * from './jsx/index.ts'
 export * from './middleware/jsx-renderer/index.ts'
 export { jwt } from './middleware/jwt/index.ts'
--- src/middleware.ts
+++ src/middleware.ts
@@ -2,12 +2,10 @@
 export * from './middleware/basic-auth'
 export * from './middleware/bearer-auth'
 export * from './middleware/cache'
-export * from './helper/cookie' // will be moved to helper.ts in v4
 export * from './middleware/compress'
 export * from './middleware/cors'
 export * from './middleware/csrf'
 export * from './middleware/etag'
-export * from './helper/html' // will be moved to helper.ts in v4
 export * from './jsx'
 export * from './middleware/jsx-renderer'
 export { jwt } from './middleware/jwt'
