197d05bb2b886fb757f6b7936a89ec4d338acf1c
watany <76135106+watany-dev@users.noreply.github.com>
2024-01-25 05:59:32 +0900

    fix:(ssg): make `files` mandatory in ToSSGResult (#2071)

--- deno_dist/helper/ssg/index.ts
+++ deno_dist/helper/ssg/index.ts
@@ -24,7 +24,7 @@ export interface FileSystemModule {
  */
 export interface ToSSGResult {
   success: boolean
-  files?: string[]
+  files: string[]
   error?: Error
 }
 
@@ -238,7 +238,7 @@ export const toSSG: ToSSGInterface = async (app, fs, options) => {
     result = { success: true, files }
   } catch (error) {
     const errorObj = error instanceof Error ? error : new Error(String(error))
-    result = { success: false, error: errorObj }
+    result = { success: false, files: [], error: errorObj }
   }
   await options?.afterGenerateHook?.(result)
   return result
