6c796a65c45fcda8bfe18751e8b25c230f84c614
Ame_x Edam <edame8080@gmail.com>
2024-01-26 06:53:48 +0000

    chore: format

--- deno_dist/helper/adapter/index.ts
+++ deno_dist/helper/adapter/index.ts
@@ -1,13 +1,6 @@
 import type { Context } from '../../context.ts'
 
-export type Runtime =
-  | 'node'
-  | 'deno'
-  | 'bun'
-  | 'workerd'
-  | 'fastly'
-  | 'edge-light'
-  | 'other'
+export type Runtime = 'node' | 'deno' | 'bun' | 'workerd' | 'fastly' | 'edge-light' | 'other'
 
 export const env = <T extends Record<string, unknown>, C extends Context = Context<{}>>(
   c: C,
