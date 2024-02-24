db5ee6acef62202a6c472f76aafb8e1c6cae0e4b
Yusuke Wada <yusuke@kamawada.com>
2024-02-24 22:38:39 +0900

    fix(serve-static): support extensionless files and refactor (#2260)

--- src/adapter/deno/serve-static.ts
+++ src/adapter/deno/serve-static.ts
@@ -1,65 +1,32 @@
-import type { Context } from '../../context'
+import type { ServeStaticOptions } from '../../middleware/serve-static'
+import { serveStatic as baseServeStatic } from '../../middleware/serve-static'
 import type { Env, MiddlewareHandler } from '../../types'
-import { getFilePath } from '../../utils/filepath'
-import { getMimeType } from '../../utils/mime'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const { open } = Deno
 
-export type ServeStaticOptions<E extends Env = Env> = {
-  root?: string
-  path?: string
-  mimes?: Record<string, string>
-  rewriteRequestPath?: (path: string) => string
-  onNotFound?: (path: string, c: Context<E>) => void | Promise<void>
-}
-
-const DEFAULT_DOCUMENT = 'index.html'
-
 export const serveStatic = <E extends Env = Env>(
-  options: ServeStaticOptions<E> = { root: '' }
+  options: ServeStaticOptions<E>
 ): MiddlewareHandler => {
-  return async (c, next) => {
-    // Do nothing if Response is already set
-    if (c.finalized) {
-      await next()
-      return
-    }
-
-    const url = new URL(c.req.url)
-    const filename = options.path ?? decodeURI(url.pathname)
-    let path = getFilePath({
-      filename: options.rewriteRequestPath ? options.rewriteRequestPath(filename) : filename,
-      root: options.root,
-      defaultDocument: DEFAULT_DOCUMENT,
-    })
-
-    if (!path) {
-      return await next()
-    }
-
-    path = `./${path}`
-
-    let file
-
-    try {
-      file = await open(path)
-    } catch (e) {
-      console.warn(`${e}`)
-    }
-
-    if (file) {
-      const mimeType = getMimeType(path)
-      if (mimeType) {
-        c.header('Content-Type', mimeType)
+  return async function serveStatic(c, next) {
+    const getContent = async (path: string) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let file: any
+      try {
+        file = await open(path)
+      } catch (e) {
+        console.warn(`${e}`)
       }
-      // Return Response object with stream
-      return c.body(file.readable)
+      return file ? file.readable : undefined
     }
-
-    await options.onNotFound?.(path, c)
-    await next()
-    return
+    const pathResolve = (path: string) => {
+      return `./${path}`
+    }
+    return baseServeStatic({
+      ...options,
+      getContent,
+      pathResolve,
+    })(c, next)
   }
 }
