da2a4f3d41814110f4f00d57f6b221de1f46e97f
Abhishek <abhi9811206369@gmail.com>
2024-02-25 20:56:02 +0530

    cancel internal stream reader on responseReadable abort

--- src/utils/stream.ts
+++ src/utils/stream.ts
@@ -12,6 +12,13 @@ export class StreamingApi {
 
     const reader = _readable.getReader()
 
+    // in case the user disconnects, let the reader know to cancel
+    // this in-turn results in responseReadable being closed
+    // and writeSSE method no longer blocks indefinitely
+    this.abortSubscribers.push(async () => {
+      await reader.cancel()
+    })
+
     this.responseReadable = new ReadableStream({
       async pull(controller) {
         const { done, value } = await reader.read()
