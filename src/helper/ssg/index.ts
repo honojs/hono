d4db45119f89abc3eee506bc97bc3c8a8ec6d3ec
berlysia <daisuke_shiohara@dwango.co.jp>
2024-02-26 16:35:48 +0900

    fix(ssg): preserve binary files as-is (#2275)
    * fix(ssg): preserve binary files as-is
    
    * denoify
    
    * test(ssg): cleanup
    
    * test(ssg): merge tests for the right content and paths

--- src/helper/ssg/index.ts
+++ src/helper/ssg/index.ts
@@ -2,7 +2,6 @@ import { replaceUrlParam } from '../../client/utils'
 import type { Context } from '../../context'
 import type { Hono } from '../../hono'
 import type { Env, MiddlewareHandler, Schema } from '../../types'
-import { bufferToString } from '../../utils/buffer'
 import { getExtension } from '../../utils/mime'
 import { joinPaths, dirname, filterStaticGenerateRoutes } from './utils'
 
@@ -202,7 +201,7 @@ export const saveContentToFiles = async (
     if (typeof content === 'string') {
       await fsModule.writeFile(filePath, content)
     } else if (content instanceof ArrayBuffer) {
-      await fsModule.writeFile(filePath, bufferToString(content))
+      await fsModule.writeFile(filePath, new Uint8Array(content))
     }
     files.push(filePath)
   }
