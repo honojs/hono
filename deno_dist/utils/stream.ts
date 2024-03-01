34fb36e960a0b48369c9f6047bc57caec7b0ea4b
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2024-03-01 17:33:03 +0900

    fix(stream): remove async from onabort (#2293)
    * fix: remove async from onAbort
    
    * chore: denoify

--- deno_dist/utils/stream.ts
+++ deno_dist/utils/stream.ts
@@ -65,7 +65,7 @@ export class StreamingApi {
     this.writer = this.writable.getWriter()
   }
 
-  async onAbort(listener: () => void | Promise<void>) {
+  onAbort(listener: () => void | Promise<void>) {
     this.abortSubscribers.push(listener)
   }
 }
