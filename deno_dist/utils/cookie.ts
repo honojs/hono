6eaa696c2f3412161ef1aa10e2e3be6d33ded9aa
Jxck <jxck@jxck.io>
2024-03-11 05:35:05 +0900

    feat(cookie): Add Cookie bis condition check (#2314)
    * throw Erro for cookie max-age longer than 400days
    
    * throw Error for cookie with expires longer than 400days
    
    * throw Error for --Secure- cookie without Secure attributes
    
    * throw Error for --Host- cookie without valid settings
    
    * throw Error for Partitioned cookie without Secure attributes
    
    * npm run denoify
    
    * npm run format:fix
    
    * bun run denoify

--- deno_dist/utils/cookie.ts
+++ deno_dist/utils/cookie.ts
@@ -114,7 +114,36 @@ export const parseSigned = async (
 const _serialize = (name: string, value: string, opt: CookieOptions = {}): string => {
   let cookie = `${name}=${value}`
 
+  if (name.startsWith('__Secure-') && !opt.secure) {
+    // FIXME: replace link to RFC
+    // https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-13#section-4.1.3.1
+    throw new Error('__Secure- Cookie must have Secure attributes')
+  }
+
+  if (name.startsWith('__Host-')) {
+    // FIXME: replace link to RFC
+    // https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-13#section-4.1.3.2
+    if (!opt.secure) {
+      throw new Error('__Host- Cookie must have Secure attributes')
+    }
+
+    if (opt.path !== '/') {
+      throw new Error('__Host- Cookie must have Path attributes with "/"')
+    }
+
+    if (opt.domain) {
+      throw new Error('__Host- Cookie must not have Domain attributes')
+    }
+  }
+
   if (opt && typeof opt.maxAge === 'number' && opt.maxAge >= 0) {
+    if (opt.maxAge > 34560000) {
+      // FIXME: replace link to RFC
+      // https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-13#section-4.1.2.2
+      throw new Error(
+        'Cookies Max-Age SHOULD NOT be greater than 400 days (34560000 seconds) in duration.'
+      )
+    }
     cookie += `; Max-Age=${Math.floor(opt.maxAge)}`
   }
 
@@ -127,6 +156,13 @@ const _serialize = (name: string, value: string, opt: CookieOptions = {}): strin
   }
 
   if (opt.expires) {
+    if (opt.expires.getTime() - Date.now() > 34560000_000) {
+      // FIXME: replace link to RFC
+      // https://datatracker.ietf.org/doc/html/draft-ietf-httpbis-rfc6265bis-13#section-4.1.2.1
+      throw new Error(
+        'Cookies Expires SHOULD NOT be greater than 400 days (34560000 seconds) in the future.'
+      )
+    }
     cookie += `; Expires=${opt.expires.toUTCString()}`
   }
 
@@ -143,6 +179,11 @@ const _serialize = (name: string, value: string, opt: CookieOptions = {}): strin
   }
 
   if (opt.partitioned) {
+    // FIXME: replace link to RFC
+    // https://www.ietf.org/archive/id/draft-cutler-httpbis-partitioned-cookies-01.html#section-2.3
+    if (!opt.secure) {
+      throw new Error('Partitioned Cookie must have Secure attributes')
+    }
     cookie += '; Partitioned'
   }
 
