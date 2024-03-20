d72ff2d5bc5cf7041d7309b3b1fa7fe2a00c2c53
Taku Amano <taku@taaas.jp>
2024-03-20 18:22:21 +0900

    chore: denoify

--- deno_dist/jsx/index.ts
+++ deno_dist/jsx/index.ts
@@ -15,6 +15,7 @@ import {
   useMemo,
   useLayoutEffect,
   useReducer,
+  useId,
   useDebugValue,
 } from './hooks/index.ts'
 import { Suspense } from './streaming.ts'
@@ -34,6 +35,7 @@ export {
   useRef,
   useCallback,
   useReducer,
+  useId,
   useDebugValue,
   use,
   startTransition,
@@ -60,6 +62,7 @@ export default {
   useRef,
   useCallback,
   useReducer,
+  useId,
   useDebugValue,
   use,
   startTransition,
