20cc38af3208080fbd827e9343a000acaf2080eb
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-04-21 01:22:46 +0000

    test(client): add types test

---
+++ src/client/types.test.ts
@@ -0,0 +1,21 @@
+import { expectTypeOf } from 'vitest'
+import { Hono } from '..'
+import { upgradeWebSocket } from '../helper'
+import { hc } from '.'
+
+describe('WebSockets', () => {
+  const app = new Hono()
+    .get('/ws', upgradeWebSocket((c) => ({})))
+    .get('/', c => c.json({}))
+  const client = hc<typeof app>('/')
+  
+  it('WebSocket route', () => {
+    expectTypeOf(client.ws).toMatchTypeOf<{
+      $ws: () => WebSocket
+    }>()
+  })
+  it('Not WebSocket Route', () => {
+    expectTypeOf<typeof client.index extends { $ws: () => WebSocket } ? false : true>()
+      .toEqualTypeOf(true)
+  })
+})
