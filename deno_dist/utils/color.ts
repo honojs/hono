b5f8117af28d278444d0a9dcf793ad76f77502c7
Miguel Oliveira <migueloliveiradev@gmail.com>
2024-05-06 21:29:49 -0300

    chore: use the previous function

--- deno_dist/utils/color.ts
+++ deno_dist/utils/color.ts
@@ -1,6 +1,14 @@
 export function getColorEnabled() {
-  const isNode = typeof process !== 'undefined' && process?.env?.NO_COLOR !== undefined
-  const isDeno = typeof Deno !== 'undefined'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const { process, Deno } = globalThis as any
 
-  return !(isNode || isDeno)
+  const isNoColor =
+    typeof process !== 'undefined'
+      ? // eslint-disable-next-line no-unsafe-optional-chaining
+        'NO_COLOR' in process?.env
+      : typeof Deno?.noColor === 'boolean'
+      ? (Deno.noColor as boolean)
+      : false
+
+  return !isNoColor
 }
