413c936c602bc793d3262beb74878253b9c5e0e4
Miguel Oliveira <88039515+mgrithm@users.noreply.github.com>
2024-05-07 17:52:36 -0300

    refactor: improve parseBody function and docs (#2628)
    * refactor: improve parsebody function and docs
    
    * refactor: Improve test and function from color function
    
    * chore: remove comments
    
    * chore: use the previous function
    
    * refactor: remove the if operator

--- src/utils/body.ts
+++ src/utils/body.ts
@@ -3,21 +3,19 @@ import { HonoRequest } from '../request'
 export type BodyData = Record<string, string | File | (string | File)[]>
 export type ParseBodyOptions = {
   /**
-   * Parse all fields with multiple values should be parsed as an array.
+   * Determines whether all fields with multiple values should be parsed as arrays.
    * @default false
    * @example
-   * ```ts
    * const data = new FormData()
    * data.append('file', 'aaa')
    * data.append('file', 'bbb')
    * data.append('message', 'hello')
-   * ```
    *
-   * If `all` is `false`:
-   * parseBody should return `{ file: 'bbb', message: 'hello' }`
+   * If all is false:
+   * parseBody should return { file: 'bbb', message: 'hello' }
    *
-   * If `all` is `true`:
-   * parseBody should return `{ file: ['aaa', 'bbb'], message: 'hello' }`
+   * If all is true:
+   * parseBody should return { file: ['aaa', 'bbb'], message: 'hello' }
    */
   all?: boolean
 }
@@ -29,22 +27,14 @@ export const parseBody = async <T extends BodyData = BodyData>(
   const headers = request instanceof HonoRequest ? request.raw.headers : request.headers
   const contentType = headers.get('Content-Type')
 
-  if (isFormDataContent(contentType)) {
+  if (
+    (contentType !== null && contentType.startsWith('multipart/form-data')) ||
+    (contentType !== null && contentType.startsWith('application/x-www-form-urlencoded'))
+  ) {
     return parseFormData<T>(request, options)
   }
 
   return {} as T
-}
-
-function isFormDataContent(contentType: string | null): boolean {
-  if (contentType === null) {
-    return false
-  }
-
-  return (
-    contentType.startsWith('multipart/form-data') ||
-    contentType.startsWith('application/x-www-form-urlencoded')
-  )
 }
 
 async function parseFormData<T extends BodyData = BodyData>(
@@ -80,23 +70,15 @@ function convertFormDataToBodyData<T extends BodyData = BodyData>(
 }
 
 const handleParsingAllValues = (form: BodyData, key: string, value: FormDataEntryValue): void => {
-  if (form[key] && isArrayField(form[key])) {
-    appendToExistingArray(form[key] as (string | File)[], value)
+  const formKey = form[key] as (string | File)[]
+
+  if (form[key] && Array.isArray(form[key])) {
+    formKey.push(value)
   } else if (form[key]) {
-    convertToNewArray(form, key, value)
+    const parsedKey = [...formKey].join('').replace(',', '')
+
+    form[key] = [parsedKey, value]
   } else {
     form[key] = value
   }
-}
-
-function isArrayField(field: unknown): field is (string | File)[] {
-  return Array.isArray(field)
-}
-
-const appendToExistingArray = (arr: (string | File)[], value: FormDataEntryValue): void => {
-  arr.push(value)
-}
-
-const convertToNewArray = (form: BodyData, key: string, value: FormDataEntryValue): void => {
-  form[key] = [form[key] as string | File, value]
 }
