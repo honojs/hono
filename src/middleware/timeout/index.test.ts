da6f77e1ff7def8aed1487e6f8e6025d98dd0329
watany <76135106+watany-dev@users.noreply.github.com>
2024-05-07 03:28:01 +0000

    setting exception

--- src/middleware/timeout/index.test.ts
+++ src/middleware/timeout/index.test.ts
@@ -1,23 +1,20 @@
+import type { Context } from '../../context'
 import { Hono } from '../../hono'
-import { timeout } from '.'
+import { HTTPException } from '../../http-exception'
+import type { ExceptionFactory } from '.'
+import { timeout, simpleTimeoutException } from '.'
 
 describe('Timeout API', () => {
   const app = new Hono()
+
   app.use('/slow-endpoint', timeout(1000))
   app.use(
     '/slow-endpoint/custom',
-    timeout(1100, {
-      message: 'Request timeout. Please try again later.',
-      code: 408,
-    })
+    timeout(1100, simpleTimeoutException(408, 'Request timeout. Please try again later.'))
   )
-  app.use(
-    '/slow-endpoint/error',
-    timeout(1200, {
-      message: 'Request timeout. Please try again later.',
-      code: 408,
-    })
-  )
+  const exception500: ExceptionFactory = (context: Context) =>
+    new HTTPException(500, { message: `Internal Server Error at ${context.req.path}` })
+  app.use('/slow-endpoint/error', timeout(1200, exception500))
   app.use('/normal-endpoint', timeout(1000))
 
   app.get('/slow-endpoint', async (c) => {
@@ -26,12 +23,12 @@ describe('Timeout API', () => {
   })
 
   app.get('/slow-endpoint/custom', async (c) => {
-    await new Promise((resolve) => setTimeout(resolve, 1100))
+    await new Promise((resolve) => setTimeout(resolve, 1200))
     return c.text('This should not show up')
   })
 
   app.get('/slow-endpoint/error', async (c) => {
-    await new Promise((resolve) => setTimeout(resolve, 1100))
+    await new Promise((resolve) => setTimeout(resolve, 1300))
     return c.text('This should not show up')
   })
 
@@ -40,28 +37,28 @@ describe('Timeout API', () => {
     return c.text('This should not show up')
   })
 
-  it('Should contain total duration', async () => {
+  it('Should trigger default timeout exception', async () => {
     const res = await app.request('http://localhost/slow-endpoint')
     expect(res).not.toBeNull()
     expect(res.status).toBe(504)
     expect(await res.text()).toContain('Gateway Timeout')
   })
 
-  it('Should apply custom error message and status code', async () => {
+  it('Should apply custom exception with function', async () => {
     const res = await app.request('http://localhost/slow-endpoint/custom')
     expect(res).not.toBeNull()
     expect(res.status).toBe(408)
     expect(await res.text()).toContain('Request timeout. Please try again later.')
   })
 
-  it('Error timeout', async () => {
+  it('Error timeout with custom status code and message', async () => {
     const res = await app.request('http://localhost/slow-endpoint/error')
     expect(res).not.toBeNull()
-    expect(res.status).toBe(200) // Confirming the endpoint does not trigger a timeout as expected
-    expect(await res.text()).toContain('This should not show up')
+    expect(res.status).toBe(500)
+    expect(await res.text()).toContain('Internal Server Error at /slow-endpoint/error')
   })
 
-  it('No Timeout', async () => {
+  it('No Timeout should pass', async () => {
     const res = await app.request('http://localhost/normal-endpoint')
     expect(res).not.toBeNull()
     expect(res.status).toBe(200)
--- src/middleware/timeout/index.ts
+++ src/middleware/timeout/index.ts
@@ -1,28 +1,27 @@
+import type { Context } from '../../context'
 import { HTTPException } from '../../http-exception'
 import type { MiddlewareHandler } from '../../types'
 import type { StatusCode } from '../../utils/http-status'
 
-interface TimeoutOptions {
-  message?: string
-  code?: StatusCode
+export type ExceptionFactory = (context: Context) => HTTPException
+
+export const simpleTimeoutException = (code: StatusCode, message: string): ExceptionFactory => {
+  return () => new HTTPException(code, { message })
 }
 
-const DEFAULT_ERROR_MESSAGE = 'Gateway Timeout'
-const DEFAULT_ERROR_CODE = 504
+const defaultTimeoutException = new HTTPException(504, {
+  message: 'Gateway Timeout',
+})
 
 export const timeout = (
   duration: number,
-  options: TimeoutOptions = {}
+  exception: ExceptionFactory | HTTPException = defaultTimeoutException
 ): MiddlewareHandler => {
-  const errorMessage = options.message ?? DEFAULT_ERROR_MESSAGE
-  const errorCode = options.code ?? DEFAULT_ERROR_CODE
-
   return async function timeout(context, next) {
     let timer: number | undefined
-
     const timeoutPromise = new Promise<void>((_, reject) => {
       timer = setTimeout(() => {
-        reject(new HTTPException(errorCode, { message: errorMessage }))
+        reject(typeof exception === 'function' ? exception(context) : exception)
       }, duration) as unknown as number
     })
 
