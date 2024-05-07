1216fd908de41a37a44cd391a5e5c60d3113defa
Miguel Oliveira <migueloliveiradev@gmail.com>
2024-05-06 21:18:21 -0300

    chore: remove comments

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,17 +1,4 @@
-// export function getColorEnabled() {
-//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//   const { process, Deno } = globalThis as any
-//   const isNoColor =
-//     typeof process !== 'undefined'
-//       ? // eslint-disable-next-line no-unsafe-optional-chaining
-//         'NO_COLOR' in process?.env
-//       : typeof Deno?.noColor === 'boolean'
-//       ? (Deno.noColor as boolean)
-//       : false
-//   return !isNoColor
-// }
-
-export function getColorEnabled(): boolean {
+export function getColorEnabled() {
   const isNode = typeof process !== 'undefined' && process?.env?.NO_COLOR !== undefined
   const isDeno = typeof Deno !== 'undefined'
 
