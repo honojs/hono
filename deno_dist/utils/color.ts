ca65c3dcf1b0783143005ea7121d96529e01b8a0
Miguel Oliveira <migueloliveiradev@gmail.com>
2024-05-06 21:13:34 -0300

    refactor: Improve test and function from color function

--- deno_dist/utils/color.ts
+++ deno_dist/utils/color.ts
@@ -1,12 +1,19 @@
-export function getColorEnabled() {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { process, Deno } = globalThis as any
-  const isNoColor =
-    typeof process !== 'undefined'
-      ? // eslint-disable-next-line no-unsafe-optional-chaining
-        'NO_COLOR' in process?.env
-      : typeof Deno?.noColor === 'boolean'
-      ? (Deno.noColor as boolean)
-      : false
-  return !isNoColor
+// export function getColorEnabled() {
+//   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const { process, Deno } = globalThis as any
+//   const isNoColor =
+//     typeof process !== 'undefined'
+//       ? // eslint-disable-next-line no-unsafe-optional-chaining
+//         'NO_COLOR' in process?.env
+//       : typeof Deno?.noColor === 'boolean'
+//       ? (Deno.noColor as boolean)
+//       : false
+//   return !isNoColor
+// }
+
+export function getColorEnabled(): boolean {
+  const isNode = typeof process !== 'undefined' && process?.env?.NO_COLOR !== undefined
+  const isDeno = typeof Deno !== 'undefined'
+
+  return !(isNode || isDeno)
 }
