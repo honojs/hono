ff18f5975f9ff486f62424d3dac8965c4f403482
fzn0x <fncolon@pm.me>
2024-05-16 05:37:56 +0700

    chore(hono): add tsdoc

--- src/hono.ts
+++ src/hono.ts
@@ -5,11 +5,24 @@ import { SmartRouter } from './router/smart-router'
 import { TrieRouter } from './router/trie-router'
 import type { BlankSchema, Env, Schema } from './types'
 
+/**
+ * The Hono class extends the functionality of the HonoBase class.
+ * It sets up routing and allows for custom options to be passed.
+ *
+ * @template E - The environment type.
+ * @template S - The schema type.
+ * @template BasePath - The base path type.
+ */
 export class Hono<
   E extends Env = Env,
   S extends Schema = BlankSchema,
   BasePath extends string = '/'
 > extends HonoBase<E, S, BasePath> {
+  /**
+   * Creates an instance of the Hono class.
+   *
+   * @param options - Optional configuration options for the Hono instance.
+   */
   constructor(options: HonoOptions<E> = {}) {
     super(options)
     this.router =
