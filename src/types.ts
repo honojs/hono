960764a9a842edf014781a4c056b065c0460a0b0
fzn0x <fncolon@pm.me>
2024-05-15 17:44:45 +0700

    chore(types): add tsdoc

--- src/types.ts
+++ src/types.ts
@@ -21,27 +21,51 @@ import type {
 ////////////////////////////////////////
 
 export type Bindings = Record<string, unknown>
+
+/**
+ * Represents a set of bindings.
+ */
 export type Variables = Record<string, unknown>
 
+/**
+ * Represents a set of variables.
+ */
 export type Env = {
   Bindings?: Bindings
   Variables?: Variables
 }
 
+/**
+ * Represents the environment configuration.
+ */
 export type Next = () => Promise<void>
 
+/**
+ * Extracts input type from a given input definition.
+ */
 export type ExtractInput<I extends Input | Input['in']> = I extends Input
   ? unknown extends I['in']
     ? {}
     : I['in']
   : I
+
+/**
+ * Defines the input and output types for a route handler.
+ */
 export type Input = {
   in?: {}
   out?: {}
   outputFormat?: ResponseFormat
 }
 
+/**
+ * Represents a blank schema.
+ */
 export type BlankSchema = {}
+
+/**
+ * Represents a blank input.
+ */
 export type BlankInput = {}
 
 ////////////////////////////////////////
@@ -50,6 +74,9 @@ export type BlankInput = {}
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Represents a route in the router.
+ */
 export interface RouterRoute {
   path: string
   method: string
@@ -62,8 +89,14 @@ export interface RouterRoute {
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Defines the response type for a route handler.
+ */
 export type HandlerResponse<O> = Response | TypedResponse<O> | Promise<Response | TypedResponse<O>>
 
+/**
+ * Represents a route handler.
+ */
 export type Handler<
   E extends Env = any,
   P extends string = any,
@@ -71,12 +104,18 @@ export type Handler<
   R extends HandlerResponse<any> = any
 > = (c: Context<E, P, I>, next: Next) => R
 
+/**
+ * Represents a middleware handler.
+ */
 export type MiddlewareHandler<
   E extends Env = any,
   P extends string = string,
   I extends Input = {}
 > = (c: Context<E, P, I>, next: Next) => Promise<Response | void>
 
+/**
+ * Represents a route or middleware handler.
+ */
 export type H<
   E extends Env = any,
   P extends string = any,
@@ -84,7 +123,14 @@ export type H<
   R extends HandlerResponse<any> = any
 > = Handler<E, P, I, R> | MiddlewareHandler<E, P, I>
 
+/**
+ * Represents a handler for "not found" errors.
+ */
 export type NotFoundHandler<E extends Env = any> = (c: Context<E>) => Response | Promise<Response>
+
+/**
+ * Represents a handler for general errors.
+ */
 export type ErrorHandler<E extends Env = any> = (
   err: Error,
   c: Context<E>
@@ -96,6 +142,9 @@ export type ErrorHandler<E extends Env = any> = (
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Interface for defining route handlers in an application.
+ */
 export interface HandlerInterface<
   E extends Env = Env,
   M extends string = string,
@@ -708,6 +757,9 @@ export interface HandlerInterface<
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Interface for defining middleware handlers in an application.
+ */
 export interface MiddlewareHandlerInterface<
   E extends Env = Env,
   S extends Schema = {},
@@ -911,6 +963,9 @@ export interface MiddlewareHandlerInterface<
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Interface for handling HTTP methods and paths in the application.
+ */
 export interface OnHandlerInterface<
   E extends Env = Env,
   S extends Schema = {},
@@ -1587,6 +1642,9 @@ export interface OnHandlerInterface<
   ): Hono<E, S & ToSchema<string, string, I, MergeTypedResponse<R>>, BasePath>
 }
 
+/**
+ * Extracts the key type from a given schema.
+ */
 type ExtractKey<S> = S extends Record<infer Key, unknown>
   ? Key extends string
     ? Key
@@ -1599,6 +1657,9 @@ type ExtractKey<S> = S extends Record<infer Key, unknown>
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Prettifies the schema by applying the given types.
+ */
 export type ToSchema<
   M extends string,
   P extends string,
@@ -1634,12 +1695,18 @@ export type ToSchema<
   }
 }>
 
+/**
+ * Represents the schema for routing.
+ */
 export type Schema = {
   [Path: string]: {
     [Method: `$${Lowercase<string>}`]: Endpoint
   }
 }
 
+/**
+ * Represents an endpoint in the routing schema.
+ */
 export type Endpoint = {
   input: Partial<ValidationTargets>
   output: any
@@ -1647,6 +1714,9 @@ export type Endpoint = {
   status: StatusCode
 }
 
+/**
+ * Extracts parameters from a path string.
+ */
 type ExtractParams<Path extends string> = string extends Path
   ? Record<string, string>
   : Path extends `${infer _Start}:${infer Param}/${infer Rest}`
@@ -1655,14 +1725,23 @@ type ExtractParams<Path extends string> = string extends Path
   ? { [K in Param]: string }
   : never
 
+/**
+ * Flattens the intersection of types.
+ */
 type FlattenIfIntersect<T> = T extends infer O ? { [K in keyof O]: O[K] } : never
 
+/**
+ * Merges the original schema with a sub-path.
+ */
 export type MergeSchemaPath<OrigSchema extends Schema, SubPath extends string> = Prettify<{
   [P in keyof OrigSchema as MergePath<SubPath, P & string>]: {
     [M in keyof OrigSchema[P]]: MergeEndpointParamsWithPath<OrigSchema[P][M], SubPath>
   }
 }>
 
+/**
+ * Merges endpoint parameters with a sub-path.
+ */
 type MergeEndpointParamsWithPath<T, SubPath extends string> = T extends {
   input: infer Input
   output: infer Output
@@ -1699,14 +1778,23 @@ type MergeEndpointParamsWithPath<T, SubPath extends string> = T extends {
     }
   : never
 
+/**
+ * Adds parameters to the input type.
+ */
 export type AddParam<I, P extends string> = ParamKeys<P> extends never
   ? I
   : I extends { param: infer _ }
   ? I
   : I & { param: UnionToIntersection<ParamKeyToRecord<ParamKeys<P>>> }
 
+/**
+ * Converts a string to a lowercase string with a dollar sign prefix.
+ */
 type AddDollar<T extends string> = `$${Lowercase<T>}`
 
+/**
+ * Merges two paths into one.
+ */
 export type MergePath<A extends string, B extends string> = B extends ''
   ? MergePath<A, '/'>
   : A extends ''
@@ -1729,9 +1817,19 @@ export type MergePath<A extends string, B extends string> = B extends ''
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Known response formats.
+ */
 export type KnownResponseFormat = 'json' | 'text'
+
+/**
+ * Represents the response format.
+ */
 export type ResponseFormat = KnownResponseFormat | string
 
+/**
+ * Represents a typed response.
+ */
 export type TypedResponse<
   T = unknown,
   U extends StatusCode = StatusCode,
@@ -1746,6 +1844,9 @@ export type TypedResponse<
   format: F
 }
 
+/**
+ * Merges the typed response.
+ */
 type MergeTypedResponse<T> = T extends Promise<infer T2>
   ? T2 extends TypedResponse
     ? T2
@@ -1760,6 +1861,9 @@ type MergeTypedResponse<T> = T extends Promise<infer T2>
 //////                             /////
 ////////////////////////////////////////
 
+/**
+ * Represents validation targets for various input sources.
+ */
 export type ValidationTargets = {
   json: any
   form: Record<string, string | File>
@@ -1775,20 +1879,32 @@ export type ValidationTargets = {
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Extracts the parameter name from a component.
+ */
 type ParamKeyName<NameWithPattern> = NameWithPattern extends `${infer Name}{${infer Rest}`
   ? Rest extends `${infer _Pattern}?`
     ? `${Name}?`
     : Name
   : NameWithPattern
 
+/**
+ * Extracts the parameter key from a component.
+ */
 type ParamKey<Component> = Component extends `:${infer NameWithPattern}`
   ? ParamKeyName<NameWithPattern>
   : never
 
+/**
+ * Extracts parameter keys from a path.
+ */
 export type ParamKeys<Path> = Path extends `${infer Component}/${infer Rest}`
   ? ParamKey<Component> | ParamKeys<Rest>
   : ParamKey<Path>
 
+/**
+ * Converts a parameter key to a record type.
+ */
 export type ParamKeyToRecord<T extends string> = T extends `${infer R}?`
   ? Record<R, string | undefined>
   : { [K in T]: string }
@@ -1799,6 +1915,9 @@ export type ParamKeyToRecord<T extends string> = T extends `${infer R}?`
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Extracts data by target from the input.
+ */
 export type InputToDataByTarget<
   T extends Input['out'],
   Target extends keyof ValidationTargets
@@ -1808,6 +1927,9 @@ export type InputToDataByTarget<
   ? R
   : never
 
+/**
+ * Removes the question mark from a string type.
+ */
 export type RemoveQuestion<T> = T extends `${infer R}?` ? R : T
 
 ////////////////////////////////////////
@@ -1816,11 +1938,21 @@ export type RemoveQuestion<T> = T extends `${infer R}?` ? R : T
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Extracts the schema type from the Hono instance.
+ */
 export type ExtractSchema<T> = UnionToIntersection<
   T extends Hono<infer _, infer S, any> ? S : never
 >
 
+/**
+ * Ensures the environment type is not `any`.
+ */
 type EnvOrEmpty<T> = T extends Env ? (Env extends T ? {} : T) : T
+
+/**
+ * Intersects non-any types in a tuple.
+ */
 type IntersectNonAnyTypes<T extends any[]> = T extends [infer Head, ...infer Rest]
   ? IfAnyThenEmptyObject<EnvOrEmpty<Head>> & IntersectNonAnyTypes<Rest>
   : {}
@@ -1831,6 +1963,9 @@ type IntersectNonAnyTypes<T extends any[]> = T extends [infer Head, ...infer Res
 //////                            //////
 ////////////////////////////////////////
 
+/**
+ * Abstract class representing a fetch event.
+ */
 export abstract class FetchEventLike {
   abstract readonly request: Request
   abstract respondWith(promise: Response | Promise<Response>): void
