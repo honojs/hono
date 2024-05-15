27e9f6f1cbdc2f89f76e6b1f3b543125effdc1ec
fzn0x <fncolon@pm.me>
2024-05-16 05:35:44 +0700

    chore(http-exception): improve tsdoc

--- src/http-exception.ts
+++ src/http-exception.ts
@@ -1,5 +1,11 @@
 import type { StatusCode } from './utils/http-status'
 
+/**
+ * Options for creating an `HTTPException`.
+ * @property res - Optional response object to use.
+ * @property message - Optional custom error message.
+ * @property cause - Optional cause of the error.
+ */
 type HTTPExceptionOptions = {
   res?: Response
   message?: string
@@ -28,12 +34,22 @@ export class HTTPException extends Error {
   readonly res?: Response
   readonly status: StatusCode
 
+  /**
+   * Creates an instance of `HTTPException`.
+   * @param status - HTTP status code for the exception. Defaults to 500.
+   * @param options - Additional options for the exception.
+   */
   constructor(status: StatusCode = 500, options?: HTTPExceptionOptions) {
     super(options?.message, { cause: options?.cause })
     this.res = options?.res
     this.status = status
   }
 
+  /**
+   * Returns the response object associated with the exception.
+   * If a response object is not provided, a new response is created with the error message and status code.
+   * @returns The response object.
+   */
   getResponse(): Response {
     if (this.res) {
       return this.res
