09d333f1609b2f645fe18679554f8dc76ee9c8cd
fzn0x <fncolon@pm.me>
2024-05-16 05:34:17 +0700

    chore(index): add tsdoc for completeness

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // @denoify-ignore
 
 import { Hono } from './hono'
+
+/**
+ * Types for environment variables, error handlers, handlers, middleware handlers, and more.
+ */
 export type {
   Env,
   ErrorHandler,
@@ -14,8 +18,23 @@ export type {
   ToSchema,
   TypedResponse,
 } from './types'
+
+/**
+ * Types for context, context variable map, context renderer, and execution context.
+ */
 export type { Context, ContextVariableMap, ContextRenderer, ExecutionContext } from './context'
+
+/**
+ * Type for HonoRequest.
+ */
 export type { HonoRequest } from './request'
+
+/**
+ * Types for inferring request and response types and client request options.
+ */
 export type { InferRequestType, InferResponseType, ClientRequestOptions } from './client'
 
+/**
+ * Hono framework for building web applications.
+ */
 export { Hono }
