60a6e5a802b6c2c2a416c5a04a8cc0292124d61d
watany <76135106+watany-dev@users.noreply.github.com>
2024-05-16 02:35:14 +0000

    denoify

--- deno_dist/middleware/timeout/index.ts
+++ deno_dist/middleware/timeout/index.ts
@@ -8,6 +8,28 @@ const defaultTimeoutException = new HTTPException(504, {
   message: 'Gateway Timeout',
 })
 
+/**
+ * Timeout middleware for Hono.
+ *
+ * @param {number} duration - The timeout duration in milliseconds.
+ * @param {HTTPExceptionFunction | HTTPException} [exception=defaultTimeoutException] - The exception to throw when the timeout occurs. Can be a function that returns an HTTPException or an HTTPException object.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use(
+ *   '/long-request',
+ *   timeout(5000) // Set timeout to 5 seconds
+ * )
+ *
+ * app.get('/long-request', async (c) => {
+ *   await someLongRunningFunction()
+ *   return c.text('Completed within time limit')
+ * })
+ * ```
+ */
 export const timeout = (
   duration: number,
   exception: HTTPExceptionFunction | HTTPException = defaultTimeoutException
