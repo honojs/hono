f79832d81289e2355e96e60404462bf3996d88be
Taku Amano <taku@taaas.jp>
2024-05-17 06:02:27 +0900

    feat: decode percent-encoded path in `getPath`

--- benchmarks/utils/src/get-path.ts
+++ benchmarks/utils/src/get-path.ts
@@ -5,15 +5,60 @@ bench('noop', () => {})
 const request = new Request('http://localhost/about/me')
 
 group('getPath', () => {
+  bench('slice + indexOf : w/o decodeURI', () => {
+    const url = request.url
+    const queryIndex = url.indexOf('?', 8)
+    return url.slice(url.indexOf('/', 8), queryIndex === -1 ? undefined : queryIndex)
+  })
+
+  bench('regexp : w/o decodeURI', () => {
+    const match = request.url.match(/^https?:\/\/[^/]+(\/[^?]*)/)
+    return match ? match[1] : ''
+  })
+
   bench('slice + indexOf', () => {
     const url = request.url
     const queryIndex = url.indexOf('?', 8)
-    url.slice(url.indexOf('/', 8), queryIndex === -1 ? undefined : queryIndex)
+    const path = url.slice(url.indexOf('/', 8), queryIndex === -1 ? undefined : queryIndex)
+    return path.includes('%') ? decodeURIComponent(path) : path
   })
 
-  bench('regexp', () => {
-    const match = request.url.match(/^https?:\/\/[^/]+(\/[^?]*)/)
-    match ? match[1] : ''
+  bench('slice + for-loop + flag', () => {
+    const url = request.url
+    let start = url.indexOf('/', 8)
+    let i = start
+    let hasPercentEncoding = false
+    for (; i < url.length; i++) {
+      const charCode = url.charCodeAt(i)
+      if (charCode === 37) {
+        // '%'
+        hasPercentEncoding = true
+      } else if (charCode === 63) {
+        // '?'
+        break
+      }
+    }
+    return hasPercentEncoding ? decodeURIComponent(url.slice(start, i)) : url.slice(start, i)
+  })
+
+  bench('slice + for-loop + immediate return', () => {
+    const url = request.url
+    const start = url.indexOf('/', 8)
+    let i = start
+    for (; i < url.length; i++) {
+      const charCode = url.charCodeAt(i)
+      if (charCode === 37) {
+        // '%'
+        // If the path contains percent encoding, use `indexOf()` to find '?' and return the result immediately.
+        // Although this is a performance disadvantage, it is acceptable since we prefer cases that do not include percent encoding.
+        const queryIndex = url.indexOf('?', i)
+        return decodeURIComponent(url.slice(start, queryIndex === -1 ? undefined : queryIndex))
+      } else if (charCode === 63) {
+        // '?'
+        break
+      }
+    }
+    return url.slice(start, i)
   })
 })
 
