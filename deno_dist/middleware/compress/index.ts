4a0a09a4a756e1fadb591321575f32f54bfa835e
Kengo Watanabe <ken5@kwtc.jp>
2024-05-18 08:46:09 +0900

    Compress Middleware

--- deno_dist/middleware/compress/index.ts
+++ deno_dist/middleware/compress/index.ts
@@ -6,6 +6,22 @@ interface CompressionOptions {
   encoding?: (typeof ENCODING_TYPES)[number]
 }
 
+/**
+ * Compress middleware for Hono.
+ *
+ * @see {@link https://hono.dev/middleware/builtin/compress}
+ *
+ * @param {CompressionOptions} [options] - The options for the compress middleware.
+ * @param {'gzip' | 'deflate'} [options.encoding] - The compression scheme to allow for response compression. Either 'gzip' or 'deflate'. If not defined, both are allowed and will be used based on the Accept-Encoding header. 'gzip' is prioritized if this option is not provided and the client provides both in the Accept-Encoding header.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use(compress())
+ * ```
+ */
 export const compress = (options?: CompressionOptions): MiddlewareHandler => {
   return async function compress(ctx, next) {
     await next()
