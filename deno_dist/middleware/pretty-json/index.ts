a2cab6bc9fc10f3fbbd00145d325b0cd42808875
Kengo Watanabe <121782456+goisaki@users.noreply.github.com>
2024-05-19 06:57:27 +0900

    docs(jsdoc): Pretty JSON Middleware (#2705)

--- deno_dist/middleware/pretty-json/index.ts
+++ deno_dist/middleware/pretty-json/index.ts
@@ -4,6 +4,25 @@ type prettyOptions = {
   space: number
 }
 
+/**
+ * Pretty JSON middleware for Hono.
+ *
+ * @see {@link https://hono.dev/middleware/builtin/pretty-json}
+ *
+ * @param {prettyOptions} [options] - The options for the pretty JSON middleware.
+ * @param {number} [options.space=2] - Number of spaces for indentation.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use(prettyJSON()) // With options: prettyJSON({ space: 4 })
+ * app.get('/', (c) => {
+ *   return c.json({ message: 'Hono!' })
+ * })
+ * ```
+ */
 export const prettyJSON = (options: prettyOptions = { space: 2 }): MiddlewareHandler => {
   return async function prettyJSON(c, next) {
     const pretty = c.req.query('pretty') || c.req.query('pretty') === '' ? true : false
