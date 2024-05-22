872fdbd6d4b3ce2a8743c68c67ef8fe7322589c4
Kengo Watanabe <121782456+goisaki@users.noreply.github.com>
2024-05-23 05:28:13 +0900

    docs(jsdoc): JWT Auth Middleware (#2709)
    * JSX Renderer Middleware
    
    * add `[]`
    
    ---------
    
    Co-authored-by: Yusuke Wada <yusuke@kamawada.com>

--- deno_dist/middleware/jwt/index.ts
+++ deno_dist/middleware/jwt/index.ts
@@ -13,6 +13,33 @@ declare module '../../context.ts' {
   }
 }
 
+/**
+ * JWT Auth middleware for Hono.
+ *
+ * @see {@link https://hono.dev/middleware/builtin/jwt}
+ *
+ * @param {object} options - The options for the JWT middleware.
+ * @param {string} options.secret - A value of your secret key.
+ * @param {string} [options.cookie] - If this value is set, then the value is retrieved from the cookie header using that value as a key, which is then validated as a token.
+ * @param {SignatureAlgorithm} [options.alg=HS256] - An algorithm type that is used for verifying. Available types are `HS256` | `HS384` | `HS512` | `RS256` | `RS384` | `RS512` | `PS256` | `PS384` | `PS512` | `ES256` | `ES384` | `ES512` | `EdDSA`.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use(
+ *   '/auth/*',
+ *   jwt({
+ *     secret: 'it-is-very-secret',
+ *   })
+ * )
+ *
+ * app.get('/auth/page', (c) => {
+ *   return c.text('You are authorized')
+ * })
+ * ```
+ */
 export const jwt = (options: {
   secret: string
   cookie?: string
