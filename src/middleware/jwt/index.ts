7fc0ec31f8c7b7a423ff01116787537073feae4e
Yusuke Wada <yusuke@kamawada.com>
2024-05-23 00:13:42 +0900

    fixed the type error

--- src/middleware/jwt/index.ts
+++ src/middleware/jwt/index.ts
@@ -1,5 +1,6 @@
 import type { JwtVariables } from './jwt'
 export { jwt, verify, decode, sign } from './jwt'
+import type {} from '../..'
 
 declare module '../..' {
   interface ContextVariableMap extends JwtVariables {}
