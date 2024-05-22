365507943769a64c8162aee2ebfa48d4986db6b8
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-05-23 05:39:18 +0900

    feat: Introduce ConnInfo helper/adapter (#2595)
    * feat: Introduce ConnInfo helper/adapter
    
    * chore: format code
    
    * chore: run ESLint and add `// @denoify-ignore`
    
    * chore: denoify
    
    * feat: export conninfo types
    
    * fix: fix test
    
    * feat(conninfo/bun): support that env be {server: server}
    
    * chore: eslint
    
    * chore: `lint:fix`
    
    * fix(conninfo/bun): English syntax for error message
    
    * fix: don't use singleQuote in string

--- deno_dist/adapter/deno/index.ts
+++ deno_dist/adapter/deno/index.ts
@@ -1,3 +1,4 @@
 export { serveStatic } from './serve-static.ts'
 export { toSSG, denoFileSystemModule } from './ssg.ts'
 export { upgradeWebSocket } from './websocket.ts'
+export { getConnInfo } from './conninfo.ts'
--- src/adapter/deno/index.ts
+++ src/adapter/deno/index.ts
@@ -1,3 +1,4 @@
 export { serveStatic } from './serve-static'
 export { toSSG, denoFileSystemModule } from './ssg'
 export { upgradeWebSocket } from './websocket'
+export { getConnInfo } from './conninfo'
