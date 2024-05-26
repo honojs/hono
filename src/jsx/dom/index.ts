30b1b124f42c8259de9d0a818d4d6dcafedc7b75
Taku Amano <taku@taaas.jp>
2024-05-26 22:55:30 +0900

    feat(jsx/dom): introduce react-dom/client APIs and React.version (#2795)
    * feat(jsx): export version string from jsx
    
    * feat(jsx/dom): introduce react-dom/client APIs
    
    * docs(jsx/dom/client): add module description
    
    * chore: add jsx/dom/client to jsr.json
    
    * refactor(jsx/dom): declare types explicitly.

--- src/jsx/dom/index.ts
+++ src/jsx/dom/index.ts
@@ -3,7 +3,7 @@
  * This module provides APIs for `hono/jsx/dom`.
  */
 
-import { isValidElement, memo } from '../base'
+import { isValidElement, memo, reactAPICompatVersion } from '../base'
 import type { Child, DOMAttributes, JSX, JSXNode, Props } from '../base'
 import { Children } from '../children'
 import { useContext } from '../context'
@@ -72,6 +72,7 @@ const cloneElement = <T extends JSXNode | JSX.Element>(
 }
 
 export {
+  reactAPICompatVersion as version,
   createElement as jsx,
   useState,
   useEffect,
@@ -109,6 +110,7 @@ export {
 }
 
 export default {
+  version: reactAPICompatVersion,
   useState,
   useEffect,
   useRef,
