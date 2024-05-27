65016cc54dc31e8267440aae2b0f732667fbc82c
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-05-27 08:31:43 +0000

    test: add test

---
+++ src/adapter/bun/server.test.ts
@@ -0,0 +1,16 @@
+import { Context } from '../../context'
+import { HonoRequest } from '../../request'
+import { type BunServer, getBunServer } from './server'
+
+describe('getBunServer', () => {
+  it('Should success to pick Server', () => {
+    const server = {} as BunServer
+
+    expect(getBunServer(new Context(new HonoRequest(new Request('/')), { env: server }))).toBe(
+      server
+    )
+    expect(getBunServer(new Context(new HonoRequest(new Request('/')), { env: { server } }))).toBe(
+      server
+    )
+  })
+})
