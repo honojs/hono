f03021f789a1ca224e382c2c55a3eae0ba391a6f
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-05-27 23:12:09 +0000

    chore: format

--- src/hono-base.ts
+++ src/hono-base.ts
@@ -247,12 +247,12 @@ class Hono<E extends Env = Env, S extends Schema = {}, BasePath extends string =
   /**
    * `.mount()` allows mount other request handlers.
    * @see {@link https://hono.dev/api/hono#mount}
-   * 
+   *
    * @param {string} path - base Path
    * @param {Function} applicationHandler - other Request Handler
    * @param {Function | undefined} optionHandler - other Request Handler with Hono Context
    * @returns {Hono} mounted Hono instance
-   * 
+   *
    * @example
    * ```ts
    * import { Router as IttyRouter } from 'itty-router'
