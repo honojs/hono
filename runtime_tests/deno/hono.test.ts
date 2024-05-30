43e0467a6ae1f33805182e6825203f60d46719f1
JetLu <i@lufei.im>
2024-05-30 12:23:12 +0800

    fix: The onopen event cannot be triggered during delayed operations

--- runtime_tests/deno/hono.test.ts
+++ runtime_tests/deno/hono.test.ts
@@ -1,3 +1,4 @@
+import {upgradeWebSocket} from '../../src/adapter/deno/websocket.js'
 import { Context } from '../../src/context.ts'
 import { env, getRuntimeKey } from '../../src/helper/adapter/index.ts'
 import { Hono } from '../../src/hono.ts'
@@ -32,4 +33,23 @@ Deno.test('environment variables', () => {
   const c = new Context(new HonoRequest(new Request('http://localhost/')))
   const { NAME } = env<{ NAME: string }>(c)
   assertEquals(NAME, 'Deno')
+})
+
+Deno.test('trigger the onOpen event of upgradeWebSocket', async () => {
+  const app = new Hono()
+  const open = new Promise<boolean>(resolve => {
+    app.get('/ws', upgradeWebSocket(async (c) => {
+      await new Promise(resolve => setTimeout(resolve, 5e2))
+      return {
+        onOpen: () => resolve(true)
+      }
+    }))
+    const server = Deno.serve(app.fetch)
+    const socket = new WebSocket('http://127.1:8000/ws')
+    socket.onopen = () => {
+      server.shutdown()
+      socket.close()
+    }
+  })
+  assertEquals(await open, true)
 })
