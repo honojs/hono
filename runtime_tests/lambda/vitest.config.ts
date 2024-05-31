20665af17667b5144514d4e71c91b8ff834c6f07
TATSUNO “Taz” Yasuhiro <ytatsuno.jp@gmail.com>
2024-06-01 04:17:47 +0900

    ci: Report coverage with CodeCov (#2862)
    * Output coverage per each test
    
    * Collect coverage
    
    * Exclude test-only files
    
    * update comment
    
    * exclude types
    
    * Add config
    
    * Update bun comment
    
    * Generate coverage in Deno

--- runtime_tests/lambda/vitest.config.ts
+++ runtime_tests/lambda/vitest.config.ts
@@ -1,5 +1,6 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
+import config from '../../vitest.config'
 
 export default defineConfig({
   test: {
@@ -9,5 +10,9 @@ export default defineConfig({
     globals: true,
     include: ['**/runtime_tests/lambda/**/*.+(ts|tsx|js)'],
     exclude: ['**/runtime_tests/lambda/vitest.config.ts', '**/runtime_tests/lambda/mock.ts'],
+    coverage: {
+      ...config.test?.coverage,
+      reportsDirectory: './coverage/raw/runtime-lambda',
+    },
   },
 })
--- runtime_tests/wrangler/vitest.config.ts
+++ runtime_tests/wrangler/vitest.config.ts
@@ -1,10 +1,15 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config'
+import config from '../../vitest.config'
 
 export default defineConfig({
   test: {
     globals: true,
     include: ['**/runtime_tests/wrangler/**/(*.)+(test).+(ts|tsx)'],
     exclude: ['**/runtime_tests/wrangler/vitest.config.ts'],
+    coverage: {
+      ...config.test?.coverage,
+      reportsDirectory: './coverage/raw/runtime-wrangler',
+    },
   },
 })
