35e8468458c60cefc8c0c860bca689ac80fea0e8
Yusuke Wada <yusuke@kamawada.com>
2024-06-02 22:37:27 +0900

    test(workerd): rename the runtime test `wrangler` to `workerd`

--- runtime_tests/workerd/vitest.config.ts
+++ runtime_tests/workerd/vitest.config.ts
@@ -5,11 +5,11 @@ import config from '../../vitest.config'
 export default defineConfig({
   test: {
     globals: true,
-    include: ['**/runtime_tests/wrangler/**/(*.)+(test).+(ts|tsx)'],
-    exclude: ['**/runtime_tests/wrangler/vitest.config.ts'],
+    include: ['**/runtime_tests/workerd/**/(*.)+(test).+(ts|tsx)'],
+    exclude: ['**/runtime_tests/workerd/vitest.config.ts'],
     coverage: {
       ...config.test?.coverage,
-      reportsDirectory: './coverage/raw/runtime-wrangler',
+      reportsDirectory: './coverage/raw/runtime-workerd',
     },
   },
 })
