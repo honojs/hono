f634c824510f265976f4f15312e098ab37aefb21
Yusuke Wada <yusuke@kamawada.com>
2024-06-02 22:49:40 +0900

    test(workerd): rename the runtime test `wrangler` to `workerd` (#2888)
    * test(workerd): rename the runtime test `wrangler` to `workerd`
    
    * fixed ci.yml
    
    * fixed a typo
    
    * use node 20.x

--- runtime_tests/workerd/index.ts
+++ runtime_tests/workerd/index.ts
@@ -1,9 +1,9 @@
-import { env } from '../../src/helper/adapter'
+import { env, getRuntimeKey } from '../../src/helper/adapter'
 import { Hono } from '../../src/hono'
 
 const app = new Hono()
 
-app.get('/', (c) => c.text('Hello Wrangler!'))
+app.get('/', (c) => c.text(`Hello from ${getRuntimeKey()}`))
 
 app.get('/env', (c) => {
   const { NAME } = env<{ NAME: string }>(c)
