b3ff51931b77c731575f50f0d424183328fa7d88
Yusuke Wada <yusuke@kamawada.com>
2024-06-02 22:49:40 +0900

    test(workerd): rename the runtime test `wrangler` to `workerd` (#2888)
    * test(workerd): rename the runtime test `wrangler` to `workerd`
    
    * fixed ci.yml
    
    * fixed a typo
    
    * use node 20.x

--- runtime_tests/workerd/index.test.ts
+++ runtime_tests/workerd/index.test.ts
@@ -1,13 +1,13 @@
 import { unstable_dev } from 'wrangler'
 import type { UnstableDevWorker } from 'wrangler'
 
-describe('Wrangler', () => {
+describe('workerd', () => {
   let worker: UnstableDevWorker
 
   beforeAll(async () => {
-    worker = await unstable_dev('./runtime_tests/wrangler/index.ts', {
+    worker = await unstable_dev('./runtime_tests/workerd/index.ts', {
       vars: {
-        NAME: 'Cloudflare',
+        NAME: 'Hono',
       },
       experimental: { disableExperimentalWarning: true },
     })
@@ -17,15 +17,15 @@ describe('Wrangler', () => {
     await worker.stop()
   })
 
-  it('Should return Hello World', async () => {
+  it('Should return 200 response with the runtime key', async () => {
     const res = await worker.fetch('/')
     expect(res.status).toBe(200)
-    expect(await res.text()).toBe('Hello Wrangler!')
+    expect(await res.text()).toBe('Hello from workerd')
   })
 
-  it('Should return the environment variable', async () => {
+  it('Should return 200 response with the environment variable', async () => {
     const res = await worker.fetch('/env')
     expect(res.status).toBe(200)
-    expect(await res.text()).toBe('Cloudflare')
+    expect(await res.text()).toBe('Hono')
   })
 })
