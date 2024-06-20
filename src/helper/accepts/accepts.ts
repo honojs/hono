25fb9503a0bcdd4b5aee2b2147e8e495d841b2f4
Antony David <antonydavid945@gmail.com>
2024-06-20 11:28:39 +0200

    fix: format

--- src/helper/accepts/accepts.ts
+++ src/helper/accepts/accepts.ts
@@ -27,27 +27,27 @@ export interface acceptsOptions extends acceptsConfig {
 
 export const parseAccept = (acceptHeader: string): Accept[] => {
   // Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
-  const accepts = acceptHeader.split(',');
-    return accepts.map((accept) => {
-      const parts = accept.trim().split(';'); // ['text/html', 'q=0.9', 'image/webp']
-      const type = parts[0]; // text/html
-      const params = parts.slice(1); // ['q=0.9', 'image/webp']
-      const q = params.find((param) => param.startsWith('q='));
-  
-      const paramsObject = params.reduce((acc, param) => {
-        const keyValue = param.split('=');
-        const key = keyValue[0].trim();
-        const value = keyValue[1].trim();
-        acc[key] = value;
-        return acc;
-      }, {} as { [key: string]: string });
-  
-      return {
-        type: type,
-        params: paramsObject,
-        q: q ? parseFloat(q.split('=')[1]) : 1,
-      };
-    });
+  const accepts = acceptHeader.split(',')
+  return accepts.map((accept) => {
+    const parts = accept.trim().split(';') // ['text/html', 'q=0.9', 'image/webp']
+    const type = parts[0] // text/html
+    const params = parts.slice(1) // ['q=0.9', 'image/webp']
+    const q = params.find((param) => param.startsWith('q='))
+
+    const paramsObject = params.reduce((acc, param) => {
+      const keyValue = param.split('=')
+      const key = keyValue[0].trim()
+      const value = keyValue[1].trim()
+      acc[key] = value
+      return acc
+    }, {} as { [key: string]: string })
+
+    return {
+      type: type,
+      params: paramsObject,
+      q: q ? parseFloat(q.split('=')[1]) : 1,
+    }
+  })
 }
 
 export const defaultMatch = (accepts: Accept[], config: acceptsConfig): string => {
