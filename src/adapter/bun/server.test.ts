ae3a67beb199a52fa1771780f450ede9b7bbef99
Yusuke Wada <yusuke@kamawada.com>
2024-06-27 10:44:37 +0900

    perf(context): improve initializing `Context` (#3046)
    * perf(context): improve initializing `Context`
    
    * update to pass tests and CI
    
    * removed not used module
    
    * remove not used module

--- src/adapter/bun/server.test.ts
+++ src/adapter/bun/server.test.ts
@@ -1,5 +1,4 @@
 import { Context } from '../../context'
-import { HonoRequest } from '../../request'
 import { getBunServer } from './server'
 import type { BunServer } from './server'
 
@@ -7,13 +6,11 @@ describe('getBunServer', () => {
   it('Should success to pick Server', () => {
     const server = {} as BunServer
 
-    expect(
-      getBunServer(new Context(new HonoRequest(new Request('http://localhost/')), { env: server }))
-    ).toBe(server)
-    expect(
-      getBunServer(
-        new Context(new HonoRequest(new Request('http://localhost/')), { env: { server } })
-      )
-    ).toBe(server)
+    expect(getBunServer(new Context(new Request('http://localhost/'), { env: server }))).toBe(
+      server
+    )
+    expect(getBunServer(new Context(new Request('http://localhost/'), { env: { server } }))).toBe(
+      server
+    )
   })
 })
