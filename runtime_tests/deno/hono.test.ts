5d62863e6386682317ab73cd2b2ee3c2a985cfcf
Yusuke Wada <yusuke@kamawada.com>
2024-06-27 09:15:03 +0900

    removed not used module

--- runtime_tests/deno/hono.test.ts
+++ runtime_tests/deno/hono.test.ts
@@ -1,7 +1,6 @@
 import { Context } from '../../src/context.ts'
 import { env, getRuntimeKey } from '../../src/helper/adapter/index.ts'
 import { Hono } from '../../src/hono.ts'
-import { HonoRequest } from '../../src/request.ts'
 import { assertEquals } from './deps.ts'
 
 // Test just only minimal patterns.
