6110379d6b525489668188274a20b00844b75742
Jonathan Haines <jonno.haines@gmail.com>
2024-06-29 18:35:20 +1000

    feat(cloudflare-pages): Add Cloudflare Pages middleware handler (#3028)
    * Add Cloudflare Pages middleware handler
    
    * fix: handle HTTPException
    
    * fix: handle context.error
    
    * fix: remove HonoRequest from conninfo test

--- src/adapter/cloudflare-pages/handler.ts
+++ src/adapter/cloudflare-pages/handler.ts
@@ -1,5 +1,7 @@
+import { Context } from '../../context'
 import type { Hono } from '../../hono'
-import type { MiddlewareHandler } from '../../types'
+import { HTTPException } from '../../http-exception'
+import type { Env, Input, MiddlewareHandler } from '../../types'
 
 // Ref: https://github.com/cloudflare/workerd/blob/main/types/defines/pages.d.ts
 
@@ -18,6 +20,13 @@ export type EventContext<Env = {}, P extends string = any, Data = {}> = {
   data: Data
 }
 
+declare type PagesFunction<
+  Env = unknown,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  Params extends string = any,
+  Data extends Record<string, unknown> = Record<string, unknown>
+> = (context: EventContext<Env, Params, Data>) => Response | Promise<Response>
+
 interface HandleInterface {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (app: Hono<any, any, any>): (eventContext: EventContext) => Response | Promise<Response>
@@ -32,6 +41,54 @@ export const handle: HandleInterface = (app: Hono) => (eventContext: EventContex
       passThroughOnException: eventContext.passThroughOnException,
     }
   )
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function handleMiddleware<E extends Env = any, P extends string = any, I extends Input = {}>(
+  middleware: MiddlewareHandler<E, P, I>
+): PagesFunction {
+  return async (executionCtx) => {
+    const context = new Context(executionCtx.request, {
+      env: executionCtx.env,
+      executionCtx,
+    })
+
+    let response: Response | void = undefined
+
+    try {
+      response = await middleware(context, async () => {
+        try {
+          context.res = await executionCtx.next()
+        } catch (error) {
+          if (error instanceof Error) {
+            context.error = error
+          } else {
+            throw error
+          }
+        }
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        context.error = error
+      } else {
+        throw error
+      }
+    }
+
+    if (response) {
+      return response
+    }
+
+    if (context.error instanceof HTTPException) {
+      return context.error.getResponse()
+    }
+
+    if (context.error) {
+      throw context.error
+    }
+
+    return context.res
+  }
 }
 
 declare abstract class FetcherLike {
