20bbcd6ba9c05f14c3f632add30ebe1008700748
ryu <114303361+ryuapp@users.noreply.github.com>
2024-07-09 06:57:42 +0900

    fix(vercel): remove deprecated address type (#3115)

--- src/adapter/vercel/conninfo.ts
+++ src/adapter/vercel/conninfo.ts
@@ -4,6 +4,5 @@ export const getConnInfo: GetConnInfo = (c) => ({
   remote: {
     // https://github.com/vercel/vercel/blob/b70bfb5fbf28a4650d4042ce68ca5c636d37cf44/packages/edge/src/edge-headers.ts#L10-L12C32
     address: c.req.header('x-real-ip'),
-    addressType: 'unknown',
   },
 })
