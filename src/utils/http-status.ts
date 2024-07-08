ec995cb0d6b486da3f9dcc1f29ec6c712a03bc62
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-08 21:53:39 +0900

    refactor(http-status): remove unnecessary line of types and use common types (#3110)
    * refactor(http-status): remove unnecessary line of types
    
    * refactor

--- src/utils/http-status.ts
+++ src/utils/http-status.ts
@@ -55,7 +55,7 @@ export type UnofficialStatusCode = -1
  * @deprecated
  * Use `UnofficialStatusCode` instead.
  */
-export type UnOfficalStatusCode = -1
+export type UnOfficalStatusCode = UnofficialStatusCode
 
 /**
  * If you want to use an unofficial status, use `UnofficialStatusCode`.
@@ -67,4 +67,3 @@ export type StatusCode =
   | ClientErrorStatusCode
   | ServerErrorStatusCode
   | UnofficialStatusCode
-  | UnOfficalStatusCode
