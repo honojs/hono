0b65e54bc9f96423dd323a879fb371411c0253c1
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2024-07-11 02:00:46 +0000

    fix(client/factory): not includes never and change generics

--- src/client/types.ts
+++ src/client/types.ts
@@ -143,19 +143,25 @@ export type InferRequestOptionsType<T> = T extends (
   ? NonNullable<R>
   : never
 
+type FilterNeverKeys<T> = {
+  [K in keyof T as (T[K] extends never ? never : T ) extends never ? never : K]: T[K]
+};
+
 type PathToChain<
   Path extends string,
   E extends Schema,
   Original extends string = Path
 > = Path extends `/${infer P}`
   ? PathToChain<P, E, Path>
-  : Path extends `${infer P}/${infer R}`
+  : (Path extends `${infer P}/${infer R}`
   ? { [K in P]: PathToChain<R, E, Original> }
   : {
       [K in Path extends '' ? 'index' : Path]: ClientRequest<
         E extends Record<string, unknown> ? E[Original] : never
       >
-    }
+    }) extends infer R ? {
+      [K in keyof R as (R[K] extends never ? never : R ) extends never ? never : K]: R[K]
+    } : never
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type Client<T> = T extends Hono<any, infer S, any>
