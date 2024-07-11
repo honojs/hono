c2698fa2e0e391a23bcf38089e6bd16c960dc4df
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-07-11 17:16:52 +0900

    feat: Introduce Service Worker Adapter (#3062)
    * feat: Intoroduce service worker adapter
    
    * test: add test
    
    * chore: format code
    
    * refactor: handler.ts
    
    * chore: remove unused import
    
    * fix: add webworker to jsr.json
    
    * fix: triple slash directives to tsconfig.json
    
    * feat: add types.ts to define worker types

---
+++ src/adapter/service-worker/handler.test.ts
@@ -0,0 +1,57 @@
+import { Hono } from '../../hono'
+import { handle } from './handler'
+import type { FetchEvent } from './types'
+
+describe('handle', () => {
+  it('Success to fetch', async () => {
+    const app = new Hono()
+    app.get('/', (c) => {
+      return c.json({ hello: 'world' })
+    })
+    const handler = handle(app)
+    const json = await new Promise<Response>((resolve) => {
+      handler({
+        request: new Request('http://localhost/'),
+        respondWith(res) {
+          resolve(res)
+        },
+      } as FetchEvent)
+    }).then((res) => res.json())
+    expect(json).toStrictEqual({ hello: 'world' })
+  })
+  it('Fallback 404', async () => {
+    const app = new Hono()
+    const handler = handle(app, {
+      async fetch() {
+        return new Response('hello world')
+      },
+    })
+    const text = await new Promise<Response>((resolve) => {
+      handler({
+        request: new Request('http://localhost/'),
+        respondWith(res) {
+          resolve(res)
+        },
+      } as FetchEvent)
+    }).then((res) => res.text())
+    expect(text).toBe('hello world')
+  })
+  it('Do not fallback 404 when fetch is undefined', async () => {
+    const app = new Hono()
+    app.get('/', (c) => c.text('Not found', 404))
+    const handler = handle(app, {
+      fetch: undefined,
+    })
+    const result = await new Promise<Response>((resolve) =>
+      handler({
+        request: new Request('https://localhost/'),
+        respondWith(r) {
+          resolve(r)
+        },
+      } as FetchEvent)
+    )
+
+    expect(result.status).toBe(404)
+    expect(await result.text()).toBe('Not found')
+  })
+})
