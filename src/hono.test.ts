1cee7281935d0bb971e6f3856a2260d8cd04f4cb
ottomated <31470743+ottomated@users.noreply.github.com>
2024-07-16 05:41:38 -0700

    feat(types): allow passing `interface`s as Bindings / Variables (#3136)
    * feat(types): allow passing `interface`s as Bindings / Variables
    
    * test(context): add test for c.var
    
    * fix lint warning
    
    * test(types): add test for Bindings types

--- src/hono.test.ts
+++ src/hono.test.ts
@@ -3585,3 +3585,36 @@ describe('Compatible with extended Hono classes, such Zod OpenAPI Hono.', () => 
     expect(res.status).toBe(200)
   })
 })
+
+describe('Generics for Bindings and Variables', () => {
+  interface CloudflareBindings {
+    MY_VARIABLE: string
+  }
+
+  it('Should not throw type errors', () => {
+    // @ts-expect-error Bindings should extend object
+    new Hono<{
+      Bindings: number
+    }>()
+
+    const appWithInterface = new Hono<{
+      Bindings: CloudflareBindings
+    }>()
+
+    appWithInterface.get('/', (c) => {
+      expectTypeOf(c.env.MY_VARIABLE).toMatchTypeOf<string>()
+      return c.text('/')
+    })
+
+    const appWithType = new Hono<{
+      Bindings: {
+        foo: string
+      }
+    }>()
+
+    appWithType.get('/', (c) => {
+      expectTypeOf(c.env.foo).toMatchTypeOf<string>()
+      return c.text('Hello Hono!')
+    })
+  })
+})
