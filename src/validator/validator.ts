3f26110dfe1c4921acfd431abf5754009bb84bd4
Gaspard Wierzbinski <contact@cpluspatch.com>
2024-07-28 09:22:52 +0200

    fix(validator): Add double quotation marks to multipart checker regex (#3195)

--- src/validator/validator.ts
+++ src/validator/validator.ts
@@ -24,7 +24,7 @@ export type ValidationFunction<
 type ExcludeResponseType<T> = T extends Response & TypedResponse<any> ? never : T
 
 const jsonRegex = /^application\/([a-z-\.]+\+)?json$/
-const multipartRegex = /^multipart\/form-data(; boundary=[A-Za-z0-9'()+_,\-./:=?]+)?$/
+const multipartRegex = /^multipart\/form-data(; boundary=[A-Za-z0-9'"()+_,\-./:=?]+)?$/
 const urlencodedRegex = /^application\/x-www-form-urlencoded$/
 
 export const validator = <
