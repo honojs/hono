227287bbf93249e73030bb4c60435401a5990b42
nitedani <67418286+nitedani@users.noreply.github.com>
2024-08-28 10:03:44 +0200

    feat(compress): improve compress middleware (#3317)
    * improve compression middleware
    
    * run lint & format
    
    * improve test
    
    * refactor
    
    * fix setting headers
    
    * fix ctx.res setter
    add tests
    
    * revert change
    
    * minor
    
    * remove workaround and simplify
    
    * remove diff
    
    * update test
    
    * improvements

--- src/middleware/compress/index.ts
+++ src/middleware/compress/index.ts
@@ -6,9 +6,13 @@
 import type { MiddlewareHandler } from '../../types'
 
 const ENCODING_TYPES = ['gzip', 'deflate'] as const
+const cacheControlNoTransformRegExp = /(?:^|,)\s*?no-transform\s*?(?:,|$)/i
+const compressibleContentTypeRegExp =
+  /^\s*(?:text\/[^;\s]+|application\/(?:javascript|json|xml|xml-dtd|ecmascript|dart|postscript|rtf|tar|toml|vnd\.dart|vnd\.ms-fontobject|vnd\.ms-opentype|wasm|x-httpd-php|x-javascript|x-ns-proxy-autoconfig|x-sh|x-tar|x-virtualbox-hdd|x-virtualbox-ova|x-virtualbox-ovf|x-virtualbox-vbox|x-virtualbox-vdi|x-virtualbox-vhd|x-virtualbox-vmdk|x-www-form-urlencoded)|font\/(?:otf|ttf)|image\/(?:bmp|vnd\.adobe\.photoshop|vnd\.microsoft\.icon|vnd\.ms-dds|x-icon|x-ms-bmp)|message\/rfc822|model\/gltf-binary|x-shader\/x-fragment|x-shader\/x-vertex|[^;\s]+?\+(?:json|text|xml|yaml))(?:[;\s]|$)/i
 
 interface CompressionOptions {
   encoding?: (typeof ENCODING_TYPES)[number]
+  threshold?: number
 }
 
 /**
@@ -18,6 +22,7 @@ interface CompressionOptions {
  *
  * @param {CompressionOptions} [options] - The options for the compress middleware.
  * @param {'gzip' | 'deflate'} [options.encoding] - The compression scheme to allow for response compression. Either 'gzip' or 'deflate'. If not defined, both are allowed and will be used based on the Accept-Encoding header. 'gzip' is prioritized if this option is not provided and the client provides both in the Accept-Encoding header.
+ * @param {number} [options.threshold=1024] - The minimum size in bytes to compress. Defaults to 1024 bytes.
  * @returns {MiddlewareHandler} The middleware handler function.
  *
  * @example
@@ -28,19 +33,47 @@ interface CompressionOptions {
  * ```
  */
 export const compress = (options?: CompressionOptions): MiddlewareHandler => {
+  const threshold = options?.threshold ?? 1024
+
   return async function compress(ctx, next) {
     await next()
+
+    const contentLength = ctx.res.headers.get('Content-Length')
+
+    // Check if response should be compressed
+    if (
+      ctx.res.headers.has('Content-Encoding') || // already encoded
+      ctx.req.method === 'HEAD' || // HEAD request
+      (contentLength && Number(contentLength) < threshold) || // content-length below threshold
+      !shouldCompress(ctx.res) || // not compressible type
+      !shouldTransform(ctx.res) // cache-control: no-transform
+    ) {
+      return
+    }
+
     const accepted = ctx.req.header('Accept-Encoding')
     const encoding =
       options?.encoding ?? ENCODING_TYPES.find((encoding) => accepted?.includes(encoding))
     if (!encoding || !ctx.res.body) {
       return
     }
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
+
+    // Compress the response
     const stream = new CompressionStream(encoding)
     ctx.res = new Response(ctx.res.body.pipeThrough(stream), ctx.res)
     ctx.res.headers.delete('Content-Length')
     ctx.res.headers.set('Content-Encoding', encoding)
   }
+}
+
+const shouldCompress = (res: Response) => {
+  const type = res.headers.get('Content-Type')
+  return type && compressibleContentTypeRegExp.test(type)
+}
+
+const shouldTransform = (res: Response) => {
+  const cacheControl = res.headers.get('Cache-Control')
+  // Don't compress for Cache-Control: no-transform
+  // https://tools.ietf.org/html/rfc7234#section-5.2.2.4
+  return !cacheControl || !cacheControlNoTransformRegExp.test(cacheControl)
 }
