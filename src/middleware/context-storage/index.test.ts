02ffa25fc3f73e4a358e366647755515c4e4c44e
Marcel Overdijk <marceloverdijk@gmail.com>
2024-08-30 07:59:56 +0200

    Updated Context Storage Middleware after review feedback

--- src/middleware/context-storage/index.test.ts
+++ src/middleware/context-storage/index.test.ts
@@ -8,41 +8,22 @@ describe('Context Storage Middleware', () => {
     }
   }
 
-  let app: Hono<Env>
+  const app = new Hono<Env>()
 
-  beforeEach(() => {
-    const { setContext } = contextStorage<Env>()
-    app = new Hono()
-    app.use(setContext)
-    app.use(async (c, next) => {
-      c.set('message', 'Hono is cool!!')
-      await next()
-    })
+  app.use(contextStorage())
+  app.use(async (c, next) => {
+    c.set('message', 'Hono is cool!!')
+    await next()
+  })
+  app.get('/', (c) => {
+    return c.text(getMessage())
   })
 
-  it('Should get context via getContext from contextStorage', async () => {
-    const { getContext } = contextStorage<Env>()
-    app.get('/', (c) => {
-      return c.text(getMessage())
-    })
+  const getMessage = () => {
+    return getContext<Env>().var.message
+  }
 
-    const getMessage = () => {
-      return getContext().var.message
-    }
-
-    const res = await app.request('/')
-    expect(await res.text()).toBe('Hono is cool!!')
-  })
-
-  it('Should get context via getContext directly imported', async () => {
-    app.get('/', (c) => {
-      return c.text(getMessage())
-    })
-
-    const getMessage = () => {
-      return getContext<Env>().var.message
-    }
-
+  it('Should get context', async () => {
     const res = await app.request('/')
     expect(await res.text()).toBe('Hono is cool!!')
   })
--- src/middleware/context-storage/index.ts
+++ src/middleware/context-storage/index.ts
@@ -4,8 +4,8 @@
  */
 
 import type { Context } from '../../context'
-import type { Env } from '../../types'
-import { createMiddleware } from '../../helper/factory'
+import type { Env, MiddlewareHandler } from '../../types'
+// import { createMiddleware } from '../../helper/factory'
 import { AsyncLocalStorage } from 'node:async_hooks'
 
 const asyncLocalStorage = new AsyncLocalStorage<Context>()
@@ -15,8 +15,7 @@ const asyncLocalStorage = new AsyncLocalStorage<Context>()
  *
  * @see {@link https://hono.dev/docs/middleware/builtin/context-storage}
  *
- * @template E - The environment type.
- * @return The context storage object containing the 'setContext' middleware handler.
+ * @returns {MiddlewareHandler} The middleware handler function.
  *
  * @example
  * ```ts
@@ -28,11 +27,7 @@ const asyncLocalStorage = new AsyncLocalStorage<Context>()
  *
  * const app = new Hono<Env>()
  *
- * // create the middleware
- * const { setContext, getContext } = contextStorage<Env>()
- *
- * // add the middleware to your router
- * app.use(setContext)
+ * app.use(contextStorage())
  *
  * app.use(async (c, next) => {
  *   c.set('message', 'Hono is cool!!)
@@ -42,25 +37,20 @@ const asyncLocalStorage = new AsyncLocalStorage<Context>()
  * app.get('/', async (c) => { c.text(getMessage()) })
  *
  * const getMessage = () => {
- *   return getContext().var.message
+ *   return getContext<Env>().var.message
  * }
  * ```
  */
-export const contextStorage = <E extends Env = Env>() => {
-  return {
-    setContext: createMiddleware(async (c, next) => {
-      return asyncLocalStorage.run(c, next)
-    }),
-    getContext: () => {
-      const context = asyncLocalStorage.getStore() as Context<E>
-      if (!context) {
-        throw new Error('Context is not available')
-      }
-      return context
-    },
+export const contextStorage = (): MiddlewareHandler => {
+  return async (c, next) => {
+    await asyncLocalStorage.run(c, next)
   }
 }
 
 export const getContext = <E extends Env = Env>() => {
-  return contextStorage<E>().getContext()
+  const context = asyncLocalStorage.getStore() as Context<E>
+  if (!context) {
+    throw new Error('Context is not available')
+  }
+  return context
 }
