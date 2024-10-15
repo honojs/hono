90833d24670a8a679112fbcae0b8b0a09683333d
Yusuke Wada <yusuke@kamawada.com>
2024-10-15 17:50:12 +0900

    docs(powered-by): add JSDoc (#3520)

--- src/middleware/powered-by/index.ts
+++ src/middleware/powered-by/index.ts
@@ -5,9 +5,28 @@
 import type { MiddlewareHandler } from '../../types'
 
 type PoweredByOptions = {
+  /**
+   * The value for X-Powered-By header.
+   * @default Hono
+   */
   serverName?: string
 }
 
+/**
+ * Powered By Middleware for Hono.
+ *
+ * @param options - The options for the Powered By Middleware.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * import { poweredBy } from 'hono/powered-by'
+ *
+ * const app = new Hono()
+ *
+ * app.use(poweredBy()) // With options: poweredBy({ serverName: "My Server" })
+ * ```
+ */
 export const poweredBy = (options?: PoweredByOptions): MiddlewareHandler => {
   return async function poweredBy(c, next) {
     await next()
