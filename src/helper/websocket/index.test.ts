294db0dd2c9c16b27d0c1adc7f573113b8906fc0
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-10-19 13:13:22 +0000

    feat(ws): Make WebSocket adapter more changeable

--- src/helper/websocket/index.test.ts
+++ src/helper/websocket/index.test.ts
@@ -1,4 +1,6 @@
-import { createWSMessageEvent } from '.'
+import { createWSMessageEvent, defineWebSocketHelper } from '.'
+import { Context } from '../../context'
+import { HonoRequest } from '../../request'
 
 describe('`createWSMessageEvent`', () => {
   it('Should `createWSMessageEvent` is working for string', () => {
@@ -10,5 +12,20 @@ describe('`createWSMessageEvent`', () => {
   it('Should `createWSMessageEvent` type is `message`', () => {
     const event = createWSMessageEvent('')
     expect(event.type).toBe('message')
+  })
+})
+describe('defineWebSocketHelper', () => {
+  it('defineWebSocketHelper should work', async () => {
+    const upgradeWebSocket = defineWebSocketHelper(() => {
+      return new Response('Hello World', {
+        status: 200,
+      })
+    })
+    const response = await upgradeWebSocket(() => ({}))(
+      new Context(new Request('http://localhost')),
+      () => Promise.resolve()
+    )
+    expect(response).toBeTruthy()
+    expect((response as Response).status).toBe(200)
   })
 })
