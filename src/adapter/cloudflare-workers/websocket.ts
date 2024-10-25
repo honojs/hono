abc4af3d86da4d21381454cb9aaa48b68bfc9234
Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
2024-10-25 16:09:36 +0900

    feat(ws): Make WebSocket adapter more changeable (#3531)
    * feat(ws): Make WebSocket adapter more changeable
    
    * lint
    
    * format code
    
    * test: add tests
    
    * format code
    
    * lint:fix
    
    * format code
    
    * test: add more tests

--- src/adapter/cloudflare-workers/websocket.ts
+++ src/adapter/cloudflare-workers/websocket.ts
@@ -1,50 +1,51 @@
-import type { UpgradeWebSocket, WSContext, WSReadyState } from '../../helper/websocket'
+import { WSContext, defineWebSocketHelper } from '../../helper/websocket'
+import type { UpgradeWebSocket, WSReadyState } from '../../helper/websocket'
 
 // Based on https://github.com/honojs/hono/issues/1153#issuecomment-1767321332
-export const upgradeWebSocket: UpgradeWebSocket<WebSocket> = (createEvents) => async (c, next) => {
-  const events = await createEvents(c)
+export const upgradeWebSocket: UpgradeWebSocket<WebSocket> = defineWebSocketHelper(
+  async (c, events) => {
+    const upgradeHeader = c.req.header('Upgrade')
+    if (upgradeHeader !== 'websocket') {
+      return
+    }
 
-  const upgradeHeader = c.req.header('Upgrade')
-  if (upgradeHeader !== 'websocket') {
-    return await next()
-  }
+    // @ts-expect-error WebSocketPair is not typed
+    const webSocketPair = new WebSocketPair()
+    const client: WebSocket = webSocketPair[0]
+    const server: WebSocket = webSocketPair[1]
 
-  // @ts-expect-error WebSocketPair is not typed
-  const webSocketPair = new WebSocketPair()
-  const client: WebSocket = webSocketPair[0]
-  const server: WebSocket = webSocketPair[1]
+    const wsContext = new WSContext<WebSocket>({
+      close: (code, reason) => server.close(code, reason),
+      get protocol() {
+        return server.protocol
+      },
+      raw: server,
+      get readyState() {
+        return server.readyState as WSReadyState
+      },
+      url: server.url ? new URL(server.url) : null,
+      send: (source) => server.send(source),
+    })
 
-  const wsContext: WSContext<WebSocket> = {
-    binaryType: 'arraybuffer',
-    close: (code, reason) => server.close(code, reason),
-    get protocol() {
-      return server.protocol
-    },
-    raw: server,
-    get readyState() {
-      return server.readyState as WSReadyState
-    },
-    url: server.url ? new URL(server.url) : null,
-    send: (source) => server.send(source),
-  }
-  if (events.onOpen) {
-    server.addEventListener('open', (evt: Event) => events.onOpen?.(evt, wsContext))
-  }
-  if (events.onClose) {
-    server.addEventListener('close', (evt: CloseEvent) => events.onClose?.(evt, wsContext))
-  }
-  if (events.onMessage) {
-    server.addEventListener('message', (evt: MessageEvent) => events.onMessage?.(evt, wsContext))
-  }
-  if (events.onError) {
-    server.addEventListener('error', (evt: Event) => events.onError?.(evt, wsContext))
-  }
+    if (events.onOpen) {
+      server.addEventListener('open', (evt: Event) => events.onOpen?.(evt, wsContext))
+    }
+    if (events.onClose) {
+      server.addEventListener('close', (evt: CloseEvent) => events.onClose?.(evt, wsContext))
+    }
+    if (events.onMessage) {
+      server.addEventListener('message', (evt: MessageEvent) => events.onMessage?.(evt, wsContext))
+    }
+    if (events.onError) {
+      server.addEventListener('error', (evt: Event) => events.onError?.(evt, wsContext))
+    }
 
-  // @ts-expect-error - server.accept is not typed
-  server.accept?.()
-  return new Response(null, {
-    status: 101,
-    // @ts-expect-error - webSocket is not typed
-    webSocket: client,
-  })
-}
+    // @ts-expect-error - server.accept is not typed
+    server.accept?.()
+    return new Response(null, {
+      status: 101,
+      // @ts-expect-error - webSocket is not typed
+      webSocket: client,
+    })
+  }
+)
