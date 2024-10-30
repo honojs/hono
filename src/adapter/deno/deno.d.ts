ac51ea325cf5697d84d18ffb5832f2356327a5a2
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2024-10-30 12:56:53 +0900

    refactor jsdoc

--- src/adapter/deno/deno.d.ts
+++ src/adapter/deno/deno.d.ts
@@ -4,9 +4,9 @@ declare namespace Deno {
   }
 
   /**
-   * Open file with the specified path.
+   * Open the file using the specified path.
    *
-   * @param path The path to open file.
+   * @param path The path to open the file.
    * @returns FileHandle object.
    */
   export function open(path: string): Promise<FileHandleLike>
@@ -36,13 +36,13 @@ declare namespace Deno {
    * Write a new file, with the specified path and data.
    *
    * @param path The path to the file to write.
-   * @param data The data to write to the file.
+   * @param data The data to write into the file.
    * @returns A promise that resolves when the file is written.
    */
   export function writeFile(path: string, data: Uint8Array): Promise<void>
 
   /**
-   * errors of Deno
+   * Errors of Deno
    */
   export const errors: Record<string, Function>
 
@@ -59,7 +59,7 @@ declare namespace Deno {
    */
   export interface UpgradeWebSocketOptions {
     /**
-     * Sets the `.protocol` property on the client side web socket to the
+     * Sets the `.protocol` property on the client-side web socket to the
      * value provided here, which should be one of the strings specified in the
      * `protocols` parameter when requesting the web socket. This is intended
      * for clients and servers to specify sub-protocols to use to communicate to
@@ -69,7 +69,7 @@ declare namespace Deno {
     /**
      * If the client does not respond to this frame with a
      * `pong` within the timeout specified, the connection is deemed
-     * unhealthy and is closed. The `close` and `error` event will be emitted.
+     * unhealthy and is closed. The `close` and `error` events will be emitted.
      *
      * The unit is seconds, with a default of 30.
      * Set to `0` to disable timeouts.
