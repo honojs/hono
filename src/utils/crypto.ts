d0bcf543b930d38cb9598805d324d9fca92b0875
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2024-11-04 14:03:47 +0900

    fix(utils/crypto): fix types of crypto utility (#3614)
    * fix(utils/crypto): fix types of crypto utility
    
    * fix lint

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -3,12 +3,14 @@
  * Crypto utility.
  */
 
+import type { JSONValue } from './types'
+
 type Algorithm = {
   name: string
   alias: string
 }
 
-type Data = string | boolean | number | object | ArrayBufferView | ArrayBuffer
+type Data = string | boolean | number | JSONValue | ArrayBufferView | ArrayBuffer
 
 export const sha256 = async (data: Data): Promise<string | null> => {
   const algorithm: Algorithm = { name: 'SHA-256', alias: 'sha256' }
