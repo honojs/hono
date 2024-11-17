f458f5e8d395dffa35a36004c9bd2af234e428c4
k2tzumi <katzumi+github@gmail.com>
2024-11-17 12:29:19 +0900

    Refactoring to convert to octocov custom metrics format

--- perf-measures/bundle-check/scripts/process-results.ts
+++ perf-measures/bundle-check/scripts/process-results.ts
@@ -1,14 +1,22 @@
 import * as fs from 'node:fs/promises'
 
 async function main() {
-    const currentResult = (await fs.readFile('./generated/after.js')).byteLength
-    let previousResult: number | null = null
-    try {
-        previousResult = (await fs.readFile('./generated/before.js')).byteLength
-    } catch (e) {}
-    const table = ['| | Current | Previous |', '| --- | --- | --- |']
-    table.push(`| Bundle Size | ${currentResult} | ${previousResult || 'N/A'} |`)
-    console.log(table.join('\n'))
+    const args = process.argv.slice(2);
+    const filePath = args[0];
+
+    const bundleSize = (await fs.readFile(filePath)).byteLength
+    const metrics = []
+    metrics.push({
+        key: 'bundle-size',
+        name: 'Bundle Size',
+        value: bundleSize,
+    })    
+    const diagnostics = {
+        key: "bundle-check",
+        name: "Bundle check",
+        metrics
+    }
+    console.log(JSON.stringify(diagnostics, null, 2))
 }
 
 main()
