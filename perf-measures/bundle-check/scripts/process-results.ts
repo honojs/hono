9e54a2f5993d086170ff20db2ddd255a0aee4ad3
Yusuke Wada <yusuke@kamawada.com>
2024-11-18 15:40:02 +0900

    chore: enable formatting `perf-measures`

--- perf-measures/bundle-check/scripts/process-results.ts
+++ perf-measures/bundle-check/scripts/process-results.ts
@@ -1,14 +1,14 @@
 import * as fs from 'node:fs/promises'
 
 async function main() {
-    const currentResult = (await fs.readFile('./generated/after.js')).byteLength
-    let previousResult: number | null = null
-    try {
-        previousResult = (await fs.readFile('./generated/before.js')).byteLength
-    } catch (e) {}
-    const table = ['| | Current | Previous |', '| --- | --- | --- |']
-    table.push(`| Bundle Size | ${currentResult} | ${previousResult || 'N/A'} |`)
-    console.log(table.join('\n'))
+  const currentResult = (await fs.readFile('./generated/after.js')).byteLength
+  let previousResult: number | null = null
+  try {
+    previousResult = (await fs.readFile('./generated/before.js')).byteLength
+  } catch (e) {}
+  const table = ['| | Current | Previous |', '| --- | --- | --- |']
+  table.push(`| Bundle Size | ${currentResult} | ${previousResult || 'N/A'} |`)
+  console.log(table.join('\n'))
 }
 
 main()
