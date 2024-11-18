28fcd2c500e0059f93bc21703e543227a4a88557
k2tzumi <katzumi+github@gmail.com>
2024-11-18 23:40:31 +0900

    Fix code style

--- perf-measures/bundle-check/scripts/check-bundle-size.ts
+++ perf-measures/bundle-check/scripts/check-bundle-size.ts
@@ -1,43 +1,43 @@
-import * as path from 'path';
-import * as os from 'os';
+import * as path from 'path'
+import * as os from 'os'
 import * as fs from 'node:fs'
-import * as esbuild from 'esbuild';
+import * as esbuild from 'esbuild'
 
 async function main() {
-    const tempDir = os.tmpdir();
-    const tempFilePath = path.join(tempDir, 'bundle.tmp.js');
+  const tempDir = os.tmpdir()
+  const tempFilePath = path.join(tempDir, 'bundle.tmp.js')
 
-    try {
-        await esbuild.build({ 
-            entryPoints: ['dist/index.js'],
-            bundle: true,
-            minify: true,
-            format: 'esm' as esbuild.Format,
-            target: 'es2022',
-            outfile: tempFilePath
-         });
+  try {
+    await esbuild.build({
+      entryPoints: ['dist/index.js'],
+      bundle: true,
+      minify: true,
+      format: 'esm' as esbuild.Format,
+      target: 'es2022',
+      outfile: tempFilePath,
+    })
 
-         const bundleSize = fs.statSync(tempFilePath).size
-         const metrics = []
-         metrics.push({
-            key: 'bundle-size',
-            name: 'Bundle Size',
-            value: bundleSize,
-            unit: 'B'
-         })
-         const benchmark = {
-            key: "bundle-size-check",
-            name: "Bundle size check",
-            metrics
-         }
-         console.log(JSON.stringify(benchmark, null, 2))
-    } catch (error) {
-        console.error('Build failed:', error); 
-    } finally {
-        if (fs.existsSync(tempFilePath)) { 
-            fs.unlinkSync(tempFilePath); 
-        } 
+    const bundleSize = fs.statSync(tempFilePath).size
+    const metrics = []
+    metrics.push({
+      key: 'bundle-size',
+      name: 'Bundle Size',
+      value: bundleSize,
+      unit: 'B',
+    })
+    const benchmark = {
+      key: 'bundle-size-check',
+      name: 'Bundle size check',
+      metrics,
     }
+    console.log(JSON.stringify(benchmark, null, 2))
+  } catch (error) {
+    console.error('Build failed:', error)
+  } finally {
+    if (fs.existsSync(tempFilePath)) {
+      fs.unlinkSync(tempFilePath)
+    }
+  }
 }
 
 main()
