1238e6494200ce79c4419231ecc4941419e9ef35
Yusuke Wada <yusuke@kamawada.com>
2024-11-20 07:08:56 +0900

    fix(base): define `errorHandler` with `private` not use `#`

--- src/hono-base.ts
+++ src/hono-base.ts
@@ -182,7 +182,8 @@ class Hono<E extends Env = Env, S extends Schema = {}, BasePath extends string =
   }
 
   #notFoundHandler: NotFoundHandler = notFoundHandler
-  #errorHandler: ErrorHandler = errorHandler
+  // Cannot use `#` because it requires visibility at JavaScript runtime.
+  private errorHandler: ErrorHandler = errorHandler
 
   /**
    * `.route()` allows grouping other Hono instance in routes.
@@ -214,11 +215,11 @@ class Hono<E extends Env = Env, S extends Schema = {}, BasePath extends string =
     const subApp = this.basePath(path)
     app.routes.map((r) => {
       let handler
-      if (app.#errorHandler === errorHandler) {
+      if (app.errorHandler === errorHandler) {
         handler = r.handler
       } else {
         handler = async (c: Context, next: Next) =>
-          (await compose<Context>([], app.#errorHandler)(c, () => r.handler(c, next))).res
+          (await compose<Context>([], app.errorHandler)(c, () => r.handler(c, next))).res
         ;(handler as any)[COMPOSED_HANDLER] = r.handler
       }
 
@@ -263,7 +264,7 @@ class Hono<E extends Env = Env, S extends Schema = {}, BasePath extends string =
    * ```
    */
   onError = (handler: ErrorHandler<E>): Hono<E, S, BasePath> => {
-    this.#errorHandler = handler
+    this.errorHandler = handler
     return this
   }
 
@@ -382,7 +383,7 @@ class Hono<E extends Env = Env, S extends Schema = {}, BasePath extends string =
 
   #handleError(err: unknown, c: Context<E>) {
     if (err instanceof Error) {
-      return this.#errorHandler(err, c)
+      return this.errorHandler(err, c)
     }
     throw err
   }
@@ -431,7 +432,7 @@ class Hono<E extends Env = Env, S extends Schema = {}, BasePath extends string =
         : res ?? this.#notFoundHandler(c)
     }
 
-    const composed = compose<Context>(matchResult[0], this.#errorHandler, this.#notFoundHandler)
+    const composed = compose<Context>(matchResult[0], this.errorHandler, this.#notFoundHandler)
 
     return (async () => {
       try {
