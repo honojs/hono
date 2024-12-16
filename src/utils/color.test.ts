a2b51749bb68e3a9ade01823400bd863570096d5
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2024-12-16 02:09:09 +0000

    add tests

--- src/utils/color.test.ts
+++ src/utils/color.test.ts
@@ -1,12 +1,29 @@
 import { getColorEnabled } from './color'
+import { Context } from '../context'
 
 describe('getColorEnabled() - With colors enabled', () => {
+  const mockContext = new Context(new Request('Hono is cool'))
+
   it('should return true', async () => {
-    expect(getColorEnabled()).toBe(true)
+    expect(getColorEnabled(mockContext)).toBe(true)
+  })
+})
+
+describe('getColorEnabled() - With colors disabled in Edge', () => {
+  const edgeContext = new Context(new Request('Hono is cool'), {
+    env: {
+      NO_COLOR: false,
+    },
+  })
+
+  it('should return false', async () => {
+    expect(getColorEnabled(edgeContext)).toBe(false)
   })
 })
 
 describe('getColorEnabled() - With NO_COLOR environment variable set', () => {
+  const mockContext = new Context(new Request('Hono is cool'))
+
   beforeAll(() => {
     vi.stubEnv('NO_COLOR', '1')
   })
@@ -16,6 +33,6 @@ describe('getColorEnabled() - With NO_COLOR environment variable set', () => {
   })
 
   it('should return false', async () => {
-    expect(getColorEnabled()).toBe(false)
+    expect(getColorEnabled(mockContext)).toBe(false)
   })
 })
