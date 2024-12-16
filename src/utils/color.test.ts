bd394f275c195f12643c901b90caa58b46e50fef
EdamAmex <121654029+EdamAme-x@users.noreply.github.com>
2024-12-16 11:55:45 +0900

    Update color.test.ts

--- src/utils/color.test.ts
+++ src/utils/color.test.ts
@@ -8,7 +8,7 @@ describe('getColorEnabled() - With colors enabled', () => {
 })
 
 describe('getColorEnabled() - With colors disabled in Edge', () => {
-  const edgeContext = new Context(new Request('/'), {
+  const edgeContext = new Context(new Request('http://localhost/'), {
     env: {
       NO_COLOR: false,
     },
@@ -20,7 +20,7 @@ describe('getColorEnabled() - With colors disabled in Edge', () => {
 })
 
 describe('getColorEnabled() - With NO_COLOR environment variable set', () => {
-  const mockContext = new Context(new Request('/'))
+  const mockContext = new Context(new Request('http://localhost/'))
 
   beforeAll(() => {
     vi.stubEnv('NO_COLOR', '1')
