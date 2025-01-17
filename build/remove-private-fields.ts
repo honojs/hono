2ba85bed7698a17b205fc86f3ec9e837631d3d01
miyaji255 <84168445+miyaji255@users.noreply.github.com>
2025-01-17 19:20:23 +0900

    perf(build): Use WebWorker when removing private fields (#3821)
    * perf(build): Use WebWorker when removing private fields
    
    * Update build/remove-private-fields.ts
    
    Co-authored-by: Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Shotaro Nakamura <79000684+nakasyou@users.noreply.github.com>

--- build/remove-private-fields.ts
+++ build/remove-private-fields.ts
@@ -1,52 +1,40 @@
-import * as ts from 'typescript'
+import { cpus } from 'node:os'
+import type { WorkerInput, WorkerOutput } from './remove-private-fields-worker'
 
-const removePrivateTransformer = <T extends ts.Node>(ctx: ts.TransformationContext) => {
-  const visit: ts.Visitor = (node) => {
-    if (ts.isClassDeclaration(node)) {
-      const newMembers = node.members.filter((elem) => {
-        if (ts.isPropertyDeclaration(elem) || ts.isMethodDeclaration(elem)) {
-          for (const modifier of elem.modifiers ?? []) {
-            if (modifier.kind === ts.SyntaxKind.PrivateKeyword) {
-              return false
-            }
+const workers = Array.from({ length: Math.ceil(cpus().length / 2) }).map(
+  () => new Worker(`${import.meta.dirname}/remove-private-fields-worker.ts`, { type: 'module' })
+)
+let workerIndex = 0
+let taskId = 0
+
+export async function removePrivateFields(file: string): Promise<string> {
+  const currentTaskId = taskId++
+  const worker = workers[workerIndex]
+  workerIndex = (workerIndex + 1) % workers.length
+
+  return new Promise<string>((resolve, reject) => {
+    const abortController = new AbortController()
+    worker.addEventListener(
+      'message',
+      ({ data: { type, value, taskId } }: { data: WorkerOutput }) => {
+        if (taskId === currentTaskId) {
+          if (type === 'success') {
+            resolve(value)
+          } else {
+            reject(value)
           }
-        }
-        if (elem.name && ts.isPrivateIdentifier(elem.name)) {
-          return false
-        }
-        return true
-      })
-      return ts.factory.createClassDeclaration(
-        node.modifiers,
-        node.name,
-        node.typeParameters,
-        node.heritageClauses,
-        newMembers
-      )
-    }
-    return ts.visitEachChild(node, visit, ctx)
-  }
 
-  return (node: T) => {
-    const visited = ts.visitNode(node, visit)
-    if (!visited) {
-      throw new Error('The result visited is undefined.')
-    }
-    return visited
-  }
+          abortController.abort()
+        }
+      },
+      { signal: abortController.signal }
+    )
+    worker.postMessage({ file, taskId: currentTaskId } satisfies WorkerInput)
+  })
 }
 
-export const removePrivateFields = (tsPath: string) => {
-  const program = ts.createProgram([tsPath], {
-    target: ts.ScriptTarget.ESNext,
-    module: ts.ModuleKind.ESNext,
-  })
-  const file = program.getSourceFile(tsPath)
-
-  const transformed = ts.transform(file!, [removePrivateTransformer])
-  const printer = ts.createPrinter()
-  const transformedSourceFile = transformed.transformed[0] as ts.SourceFile
-  const code = printer.printFile(transformedSourceFile)
-  transformed.dispose()
-  return code
+export function cleanupWorkers() {
+  for (const worker of workers) {
+    worker.terminate()
+  }
 }
