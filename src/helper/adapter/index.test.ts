a682b1b55e37f104a3d6593a91b8d59d018cfc20
Yusuke Wada <yusuke@kamawada.com>
2025-01-26 10:04:06 +0900

    fix(helper/adapter): `env` should set `c` type correctly

--- src/helper/adapter/index.test.ts
+++ src/helper/adapter/index.test.ts
@@ -1,9 +1,29 @@
-import { getRuntimeKey } from '.'
+import { Hono } from '../../hono'
+import { env, getRuntimeKey } from '.'
 
 describe('getRuntimeKey', () => {
   it('Should return the current runtime key', () => {
     // Now, using the `bun run test` command.
     // But `vitest` depending Node.js will run this test so the RuntimeKey will be `node`.
     expect(getRuntimeKey()).toBe('node')
+  })
+})
+
+describe('env', () => {
+  describe('Types', () => {
+    type Env = {
+      Bindings: {
+        MY_VAR: string
+      }
+    }
+    const app = new Hono<Env>()
+
+    it('Should set the type of the Context correctly and not throw a type error')
+    app.get('/var', (c) => {
+      const { MY_VAR } = env<{ MY_VAR: string }>(c)
+      return c.json({
+        var: MY_VAR,
+      })
+    })
   })
 })
