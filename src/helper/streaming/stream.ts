def6f5acd9b0bdc3aafa68afcfa0022aa814dd4a
Jarred Sumner <jarred@jarredsumner.com>
2025-01-27 01:38:40 -0800

    refactor(helper/streaming): Avoid attaching AbortSignal on newer versions of Bun (#3859)
    * refactor(helper/streaming): Avoid attaching AbortSignal on newer versions of Bun
    
    * fixed the tests and refactored
    
    ---------
    
    Co-authored-by: Yusuke Wada <yusuke@kamawada.com>

--- src/helper/streaming/stream.ts
+++ src/helper/streaming/stream.ts
@@ -1,7 +1,9 @@
 import type { Context } from '../../context'
 import { StreamingApi } from '../../utils/stream'
+import { isOldBunVersion } from './utils'
 
 const contextStash: WeakMap<ReadableStream, Context> = new WeakMap<ReadableStream, Context>()
+
 export const stream = (
   c: Context,
   cb: (stream: StreamingApi) => Promise<void>,
@@ -10,12 +12,15 @@ export const stream = (
   const { readable, writable } = new TransformStream()
   const stream = new StreamingApi(writable, readable)
 
-  // bun does not cancel response stream when request is canceled, so detect abort by signal
-  c.req.raw.signal.addEventListener('abort', () => {
-    if (!stream.closed) {
-      stream.abort()
-    }
-  })
+  // Until Bun v1.1.27, Bun didn't call cancel() on the ReadableStream for Response objects from Bun.serve()
+  if (isOldBunVersion()) {
+    c.req.raw.signal.addEventListener('abort', () => {
+      if (!stream.closed) {
+        stream.abort()
+      }
+    })
+  }
+
   // in bun, `c` is destroyed when the request is returned, so hold it until the end of streaming
   contextStash.set(stream.responseReadable, c)
   ;(async () => {
