be0267ccf137322dfae1bff8584bed50a9c59f22
Beyondo <58893646+Beyondo@users.noreply.github.com>
2025-01-29 12:34:50 +0200

    Update index.ts

--- src/middleware/jwt/index.ts
+++ src/middleware/jwt/index.ts
@@ -1,6 +1,7 @@
 import type { JwtVariables } from './jwt'
 export type { JwtVariables }
 export { jwt, verify, decode, sign } from './jwt'
+import type {} from '../..'
 
 declare module '../..' {
   interface ContextVariableMap extends JwtVariables {}
