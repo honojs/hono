6837649cd104b1886e6cb6653bf26d0700c4c3e6
Beyondo <58893646+Beyondo@users.noreply.github.com>
2025-02-06 15:16:29 +0200

    feat(hono/jwk): JWK Auth Middleware (#3826)
    * Update cookie.ts
    
    * Integrated `priority` option into setCookie serialization tests
    
    * Add kid' to TokenHeader, fix Jwt.sign ignoring privateKey.alg with keyAlg fallback, add decodeHeaders utility
    
    - Added "kid" (Key ID) for TokenHeader
    - Fixed Jwt.sign() ignoring privateKey.alg
    - Renamed `alg` parameter to `KEYAlg` to differentiate between privateKey.alg
    - Added utility function `decodeHeaders` to decode only JWT headers
    
    * Add ./src/middleware/jwk/jwk.ts to jsr.json
    
    * Add hono/jwk to exports
    
    * feat(hono/jwk)
    
    Hono JWK Middleware main features:
    - Ability to provide a list of public JWKs to the keys parameter as a simple javascript array []
    - Ability to provide a URL in the jwks_uri parameter to fetch keys from + an optional RequestInit (useful for caching if your cloud provider has a modified fetch that supports it, or if you simply want to modify the request)
    - Ability to provide an async function that returns an array to the keys parameter instead of a direct array, so that it is possible to implement own custom caching layer without a separate middleware
    - Allows setting a keys directory for multi-key auth systems
    - Allows auth endpoints to be always updated with the Auth provider's public JWKs directory (often `.well-known/jwks.json`) which makes key rotations without disruptions possible
    
    Todo:
    - More tests.
    
    * (feat/Jwt.verifyFromJwks) / batteries included util
    
    * Update index.ts
    
    * add JwtHeaderRequiresKid exception
    
    * using Jwt.verifyFromJwks now
    
    * improved jsdoc and formatting
    
    * jsdoc update
    
    * formatting
    
    * testing jwk's `keys` receiving an async function
    
    * removed redundancy
    
    * add 'Should authorize Keys function' test
    
    Added /auth-keys-fn/* & /.well-known/jwks.json testing endpoints to the router.
    
    * added jwks_uri test + improved test descriptions
    
    * test naming consistency
    
    * explicit return fix + moving global declaration merging to own interface
    
    * cleaner jsdoc @example
    
    * removed commented-out tests unnecessarily inflating changes
    
    * ExtendedJsonWebKey -> HonoJsonWebKey
    
    * Refactor test to use msw per @yusukebe's suggestion
    
    * removed stray log + added minor validation w/ @Code-Hex
    
    Also removed redundant "import type {} from '../..'" from 3 different files:
    - jwk/index.ts
    - jwt/index.ts
    - request-id/index.ts
    
    * Update index.ts
    
    * add more test coverage
    
    * more test coverage
    
    * lint & format
    
    * typo
    
    * 100/100 test coverage
    
    Note: Moved more code from `hono/jwk` to the `verifyFromJwks` function for backends that require JWK verification logic beyond just a middleware
    
    * final touch
    
    * added eslint-disable + type export

---
+++ src/middleware/jwk/jwk.ts
@@ -0,0 +1,153 @@
+/**
+ * @module
+ * JWK Auth Middleware for Hono.
+ */
+
+import type { Context } from '../../context'
+import { getCookie, getSignedCookie } from '../../helper/cookie'
+import { HTTPException } from '../../http-exception'
+import type { MiddlewareHandler } from '../../types'
+import type { CookiePrefixOptions } from '../../utils/cookie'
+import { Jwt } from '../../utils/jwt'
+import '../../context'
+import type { HonoJsonWebKey } from '../../utils/jwt/jws'
+
+/**
+ * JWK Auth Middleware for Hono.
+ *
+ * @see {@link https://hono.dev/docs/middleware/builtin/jwk}
+ *
+ * @param {object} options - The options for the JWK middleware.
+ * @param {HonoJsonWebKey[] | (() => Promise<HonoJsonWebKey[]>)} [options.keys] - The values of your public keys, or a function that returns them.
+ * @param {string} [options.jwks_uri] - If this value is set, attempt to fetch JWKs from this URI, expecting a JSON response with `keys` which are added to the provided options.keys
+ * @param {string} [options.cookie] - If this value is set, then the value is retrieved from the cookie header using that value as a key, which is then validated as a token.
+ * @param {RequestInit} [init] - Optional initialization options for the `fetch` request when retrieving JWKS from a URI.
+ * @returns {MiddlewareHandler} The middleware handler function.
+ *
+ * @example
+ * ```ts
+ * const app = new Hono()
+ *
+ * app.use("/auth/*", jwk({ jwks_uri: "https://example-backend.hono.dev/.well-known/jwks.json" }))
+ *
+ * app.get('/auth/page', (c) => {
+ *   return c.text('You are authorized')
+ * })
+ * ```
+ */
+
+export const jwk = (
+  options: {
+    keys?: HonoJsonWebKey[] | (() => Promise<HonoJsonWebKey[]>)
+    jwks_uri?: string
+    cookie?:
+      | string
+      | { key: string; secret?: string | BufferSource; prefixOptions?: CookiePrefixOptions }
+  },
+  init?: RequestInit
+): MiddlewareHandler => {
+  if (!options || !(options.keys || options.jwks_uri)) {
+    throw new Error('JWK auth middleware requires options for either "keys" or "jwks_uri" or both')
+  }
+
+  if (!crypto.subtle || !crypto.subtle.importKey) {
+    throw new Error('`crypto.subtle.importKey` is undefined. JWK auth middleware requires it.')
+  }
+
+  return async function jwk(ctx, next) {
+    const credentials = ctx.req.raw.headers.get('Authorization')
+    let token
+    if (credentials) {
+      const parts = credentials.split(/\s+/)
+      if (parts.length !== 2) {
+        const errDescription = 'invalid credentials structure'
+        throw new HTTPException(401, {
+          message: errDescription,
+          res: unauthorizedResponse({
+            ctx,
+            error: 'invalid_request',
+            errDescription,
+          }),
+        })
+      } else {
+        token = parts[1]
+      }
+    } else if (options.cookie) {
+      if (typeof options.cookie == 'string') {
+        token = getCookie(ctx, options.cookie)
+      } else if (options.cookie.secret) {
+        if (options.cookie.prefixOptions) {
+          token = await getSignedCookie(
+            ctx,
+            options.cookie.secret,
+            options.cookie.key,
+            options.cookie.prefixOptions
+          )
+        } else {
+          token = await getSignedCookie(ctx, options.cookie.secret, options.cookie.key)
+        }
+      } else {
+        if (options.cookie.prefixOptions) {
+          token = getCookie(ctx, options.cookie.key, options.cookie.prefixOptions)
+        } else {
+          token = getCookie(ctx, options.cookie.key)
+        }
+      }
+    }
+
+    if (!token) {
+      const errDescription = 'no authorization included in request'
+      throw new HTTPException(401, {
+        message: errDescription,
+        res: unauthorizedResponse({
+          ctx,
+          error: 'invalid_request',
+          errDescription,
+        }),
+      })
+    }
+
+    let payload
+    let cause
+    try {
+      payload = await Jwt.verifyFromJwks(token, options, init)
+    } catch (e) {
+      cause = e
+    }
+
+    if (!payload) {
+      if (cause instanceof Error && cause.constructor === Error) {
+        throw cause
+      }
+      throw new HTTPException(401, {
+        message: 'Unauthorized',
+        res: unauthorizedResponse({
+          ctx,
+          error: 'invalid_token',
+          statusText: 'Unauthorized',
+          errDescription: 'token verification failure',
+        }),
+        cause,
+      })
+    }
+
+    ctx.set('jwtPayload', payload)
+
+    await next()
+  }
+}
+
+function unauthorizedResponse(opts: {
+  ctx: Context
+  error: string
+  errDescription: string
+  statusText?: string
+}) {
+  return new Response('Unauthorized', {
+    status: 401,
+    statusText: opts.statusText,
+    headers: {
+      'WWW-Authenticate': `Bearer realm="${opts.ctx.req.url}",error="${opts.error}",error_description="${opts.errDescription}"`,
+    },
+  })
+}
