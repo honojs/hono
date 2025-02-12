e80ce9b3b64a7c5ed0a6492c9b66b5d5ad130670
Ame_x Edam <edame8080@gmail.com>
2025-02-12 02:14:06 +0000

    fix(lambda-edge): remove

--- src/adapter/aws-lambda/handler.ts
+++ src/adapter/aws-lambda/handler.ts
@@ -1,4 +1,3 @@
-import crypto from 'node:crypto'
 import type { Hono } from '../../hono'
 import type { Env, Schema } from '../../types'
 import { decodeBase64, encodeBase64 } from '../../utils/encode'
@@ -9,10 +8,6 @@ import type {
   Handler,
   LambdaContext,
 } from './types'
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-globalThis.crypto ??= crypto
 
 export type LambdaEvent = APIGatewayProxyEvent | APIGatewayProxyEventV2 | ALBProxyEvent
 
