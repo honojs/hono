be61355e26611f2f45b4eefe00dd117f2fda22a2
ryu <114303361+ryuapp@users.noreply.github.com>
2025-04-20 13:39:44 +0900

    feat(logger): support for `NO_COLOR` on cloudflare workers

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -12,7 +12,7 @@
  */
 export function getColorEnabled(): boolean {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { process, Deno } = globalThis as any
+  const { process, Deno, navigator } = globalThis as any
 
   const isNoColor =
     typeof Deno?.noColor === 'boolean'
@@ -20,6 +20,10 @@ export function getColorEnabled(): boolean {
       : process !== undefined
       ? // eslint-disable-next-line no-unsafe-optional-chaining
         'NO_COLOR' in process?.env
+      : navigator.userAgent === 'Cloudflare Workers'
+      ? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        'NO_COLOR' in (import('cloudflare:workers').then((module) => module.env) ?? {})
       : false
 
   return !isNoColor
