afe400d30ce366a527bd9c719be741f0dedcbf9a
Fuxiao Peng <xx1124961758@126.com>
2025-05-31 20:20:02 +0800

    fix(hono/context):  content-type connot be overridden by default response when append headers

--- src/context.ts
+++ src/context.ts
@@ -670,7 +670,7 @@ export class Context<
       this.#res.headers.forEach((v, k) => {
         if (k === 'set-cookie') {
           this.#headers?.append(k, v)
-        } else {
+        } else if (this.finalized || k !== 'content-type') {
           this.#headers?.set(k, v)
         }
       })
