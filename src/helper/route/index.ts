38e556c10ad649ff73b382078d5de1a2f6a9409d
Taku Amano <taku@taaas.jp>
2025-06-08 08:40:41 +0900

    feat: make "matchedRoutes" and "routePath" helper functions, and set @deprecated flag in request.ts

--- src/helper/route/index.ts
+++ src/helper/route/index.ts
@@ -1,5 +1,6 @@
 import type { Context } from '../../context'
+import { GET_MATCH_RESULT } from '../../request/constants'
 
-export const basePath = (c: Context) => {
-  return c.req.matchedRoutes[c.req.routeIndex].basePath
-}
+export const matchedRoutes = (c: Context) => c.req[GET_MATCH_RESULT][0].map(([[, route]]) => route)
+export const routePath = (c: Context) => matchedRoutes(c)[c.req.routeIndex].path
+export const basePath = (c: Context) => matchedRoutes(c)[c.req.routeIndex].basePath
