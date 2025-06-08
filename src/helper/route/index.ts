5890add3aea8608cef20316f4078dd3827e37b73
Taku Amano <taku@taaas.jp>
2025-06-08 09:22:11 +0900

    docs: add TSDoc for route helpers

--- src/helper/route/index.ts
+++ src/helper/route/index.ts
@@ -1,6 +1,69 @@
 import type { Context } from '../../context'
 import { GET_MATCH_RESULT } from '../../request/constants'
 
+/**
+ * Get matched routes in the handler
+ *
+ * @param {Context} c - The context object
+ * @returns An array of matched routes
+ *
+ * @example
+ * ```ts
+ * import { matchedRoutes } from 'hono/route'
+ *
+ * app.use('*', async function logger(c, next) {
+ *   await next()
+ *   matchedRoutes(c).forEach(({ handler, method, path }, i) => {
+ *     const name = handler.name || (handler.length < 2 ? '[handler]' : '[middleware]')
+ *     console.log(
+ *       method,
+ *       ' ',
+ *       path,
+ *       ' '.repeat(Math.max(10 - path.length, 0)),
+ *       name,
+ *       i === c.req.routeIndex ? '<- respond from here' : ''
+ *     )
+ *   })
+ * })
+ * ```
+ */
 export const matchedRoutes = (c: Context) => c.req[GET_MATCH_RESULT][0].map(([[, route]]) => route)
+
+/**
+ * Get the route path registered within the handler
+ *
+ * @param {Context} c - The context object
+ * @returns The route path registered within the handler
+ *
+ * @example
+ * ```ts
+ * import { routePath } from 'hono/route'
+ *
+ * app.get('/posts/:id', (c) => {
+ *   return c.text(routePath(c)) // '/posts/:id'
+ * })
+ * ```
+ */
 export const routePath = (c: Context) => matchedRoutes(c)[c.req.routeIndex].path
+
+/**
+ * Get the basePath for the matched route
+ *
+ * @param {Context} c - The context object
+ * @returns The basePath for the matched route
+ *
+ * @example
+ * ```ts
+ * import { basePath } from 'hono/route'
+ *
+ * const app = new Hono()
+ *
+ * const subApp = new Hono()
+ * subApp.get('/posts/:id', (c) => {
+ *   return c.text(basePath(c)) // '/sub'
+ * })
+ *
+ * app.route('/sub', subApp)
+ * ```
+ */
 export const basePath = (c: Context) => matchedRoutes(c)[c.req.routeIndex].basePath
