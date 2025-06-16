7ccb276ad1d4f8211262e865eda0f2bcdc8d191f
Taku Amano <taku@taaas.jp>
2025-06-16 14:15:30 +0900

    feat: support http+unix scheme (#4148)

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -105,7 +105,12 @@ const tryDecodeURI = (str: string) => tryDecode(str, decodeURI)
 
 export const getPath = (request: Request): string => {
   const url = request.url
-  const start = url.indexOf('/', 8)
+  const start = url.indexOf(
+    '/',
+    url.charCodeAt(9) === 58
+      ? 13 // http+unix://
+      : 8 // http:// or https://
+  )
   let i = start
   for (; i < url.length; i++) {
     const charCode = url.charCodeAt(i)
