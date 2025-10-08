4e711452a94ab251e2ab340e4617b134d813936c
Taku Amano <taku@taaas.jp>
2025-10-08 21:45:33 +0900

    feat: introduce hono command

---
+++ bin/serve.ts
@@ -0,0 +1,42 @@
+import { serve } from '@hono/node-server'
+import { serveStatic } from '@hono/node-server/serve-static'
+import { parseArgs } from 'node:util'
+import { proxy } from '../src/helper/proxy'
+import { Hono } from '../src/hono'
+import { basicAuth } from '../src/middleware/basic-auth'
+import { logger } from '../src/middleware/logger'
+// import all the middleware and helpers
+
+[
+  basicAuth,
+  logger,
+  serveStatic,
+  proxy,
+].forEach((f) => {
+  if (typeof f === 'function') {
+    // useless process to avoid being deleted by bundler
+  }
+})
+
+const { values } = parseArgs({
+  options: {
+    port: { type: 'string' },
+    use: { type: 'string', multiple: true },
+  },
+})
+
+const app = new Hono()
+
+values.use?.forEach((use) => {
+  app.use((eval(use)))
+})
+
+serve(
+  {
+    fetch: app.fetch,
+    port: values.port ? Number.parseInt(values.port) : undefined,
+  },
+  (info) => {
+    console.log(`Listening on http://localhost:${info.port}`)
+  }
+)
