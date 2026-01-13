3c0773d881369de7baa378221c429d48ce33ddf7
artemtam <artemtam1408@gmail.com>
2026-01-13 13:29:54 +0100

    feat(bun): export getBunServer

--- src/adapter/bun/server.ts
+++ src/adapter/bun/server.ts
@@ -5,26 +5,9 @@
 import type { Context } from '../../context'
 
 /**
- * Bun Server Object
- */
-export interface BunServer {
-  requestIP?: (req: Request) => {
-    address: string
-    family: string
-    port: number
-  } | null
-  upgrade<T>(
-    req: Request,
-    options?: {
-      data: T
-    }
-  ): boolean
-}
-
-/**
  * Get Bun Server Object from Context
  * @param c Context
  * @returns Bun Server
  */
-export const getBunServer = (c: Context): BunServer | undefined =>
-  ('server' in c.env ? c.env.server : c.env) as BunServer | undefined
+export const getBunServer = <ServerT>(c: Context): ServerT | undefined =>
+  ('server' in c.env ? c.env.server : c.env) as ServerT | undefined
