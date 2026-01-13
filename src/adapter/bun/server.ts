6b3e108e458c1bc974d88da2d1eeea28b096b3c3
artemtam <artemtam1408@gmail.com>
2026-01-13 13:32:50 +0100

    fix: add generic to jsdoc

--- src/adapter/bun/server.ts
+++ src/adapter/bun/server.ts
@@ -6,8 +6,9 @@ import type { Context } from '../../context'
 
 /**
  * Get Bun Server Object from Context
+ * @template T - The type of Bun Server
  * @param c Context
  * @returns Bun Server
  */
-export const getBunServer = <ServerT>(c: Context): ServerT | undefined =>
-  ('server' in c.env ? c.env.server : c.env) as ServerT | undefined
+export const getBunServer = <T>(c: Context): T | undefined =>
+  ('server' in c.env ? c.env.server : c.env) as T | undefined
