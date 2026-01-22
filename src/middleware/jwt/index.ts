61498bb163b73c5cc3a624e84f6fae3c9d8aacd7
Yusuke Wada <yusuke@kamawada.com>
2026-01-22 10:06:55 +0900

    feat(jwt): export `AlgorithmTypes` (#4642)

--- src/middleware/jwt/index.ts
+++ src/middleware/jwt/index.ts
@@ -1,6 +1,7 @@
 import type { JwtVariables } from './jwt'
 export type { JwtVariables }
 export { jwt, verifyWithJwks, verify, decode, sign } from './jwt'
+export { AlgorithmTypes } from '../../utils/jwt/jwa'
 import type {} from '../..'
 
 declare module '../..' {
