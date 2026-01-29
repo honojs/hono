4c29f0cb54be47ab4d0f3332afdef992ad9358ce
Redas Jefisovas <114405740+holoflash@users.noreply.github.com>
2026-01-29 16:04:00 +0100

    chore: add docs to exported utils

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -12,18 +12,54 @@ type Algorithm = {
 
 type Data = string | boolean | number | JSONValue | ArrayBufferView | ArrayBuffer
 
+/**
+ * Calculates the SHA-256 digest of the given data.
+ *
+ * @param data - The data to calculate the SHA-256 digest for.
+ * @returns The SHA-256 digest of the data, or null if the Web Crypto API is not available.
+ *
+ * @example
+ * ```ts
+ * const hash = await sha256('hello')
+ * console.log(hash) // 2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824
+ * ```
+ */
 export const sha256 = async (data: Data): Promise<string | null> => {
   const algorithm: Algorithm = { name: 'SHA-256', alias: 'sha256' }
   const hash = await createHash(data, algorithm)
   return hash
 }
 
+/**
+ * Calculates the SHA-1 digest of the given data.
+ *
+ * @param data - The data to calculate the SHA-1 digest for.
+ * @returns The SHA-1 digest of the data, or null if the Web Crypto API is not available.
+ *
+ * @example
+ * ```ts
+ * const hash = await sha1('hello')
+ * console.log(hash) // aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d
+ * ```
+ */
 export const sha1 = async (data: Data): Promise<string | null> => {
   const algorithm: Algorithm = { name: 'SHA-1', alias: 'sha1' }
   const hash = await createHash(data, algorithm)
   return hash
 }
 
+/**
+ * Calculates the MD5 digest of the given data.
+ *
+ * @param data - The data to calculate the MD5 digest for.
+ * @returns The MD5 digest of the data, or null if the Web Crypto API is not available.
+ *
+ * @example
+ * ```ts
+ * const hash = await md5('hello')
+ * console.log(hash) // 5d41402abc4b2a76b9719d911017c592
+ * ```
+ */
 export const md5 = async (data: Data): Promise<string | null> => {
   const algorithm: Algorithm = { name: 'MD5', alias: 'md5' }
   const hash = await createHash(data, algorithm)
