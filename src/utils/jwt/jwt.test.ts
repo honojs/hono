96d781f770cdb75881784e0b38de5cd53b96e748
hiendv <hien.dv.neo@gmail.com>
2026-04-23 13:54:10 +0700

    test: remove useless tests for pem

--- src/utils/jwt/jwt.test.ts
+++ src/utils/jwt/jwt.test.ts
@@ -1587,7 +1587,7 @@ describe('PEM parsing', async () => {
   const base64Key = Buffer.from(exported).toString('base64')
   const privateKey = `-----BEGIN PRIVATE KEY-----\n${base64Key}\n-----END PRIVATE KEY-----`
 
-  it('should sign with a normal private PEM key', async () => {
+  it('should sign & verify with a normal private PEM key', async () => {
     const payload = { sub: '123' }
 
     const token = await JWT.sign(payload, privateKey, 'EdDSA')
@@ -1596,31 +1596,12 @@ describe('PEM parsing', async () => {
     expect(verified).toEqual(payload)
   })
 
-  it('should verify with a normal private PEM key', async () => {
-    const payload = { sub: '123' }
-
-    const token = await JWT.sign(payload, privateKey, 'EdDSA')
-    const verified = await JWT.verify(token, privateKey, 'EdDSA')
-
-    expect(verified).toEqual(payload)
-  })
-
-  it('should sign with a single-line private PEM key', async () => {
+  it('should sign & verify with a single-line private PEM key', async () => {
     const singleLinePrivateKey = privateKey.replace(/\n/g, '')
     const payload = { sub: '123' }
 
     const token = await JWT.sign(payload, singleLinePrivateKey, 'EdDSA')
     const verified = await JWT.verify(token, privateKey, 'EdDSA')
-
-    expect(verified).toEqual(payload)
-  })
-
-  it('should verify with a single-line private PEM key', async () => {
-    const singleLinePrivateKey = privateKey.replace(/\n/g, '')
-    const payload = { sub: '123' }
-
-    const token = await JWT.sign(payload, privateKey, 'EdDSA')
-    const verified = await JWT.verify(token, singleLinePrivateKey, 'EdDSA')
 
     expect(verified).toEqual(payload)
   })
