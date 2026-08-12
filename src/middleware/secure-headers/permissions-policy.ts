025c8fa331fd8d5f39c65a0af9438ab4f3c4689e
Saif Ali <this.saif.email@gmail.com>
2026-08-13 04:03:02 +0530

    feat(secure-headers): add missing W3C Permissions-Policy directives and spec references

--- src/middleware/secure-headers/permissions-policy.ts
+++ src/middleware/secure-headers/permissions-policy.ts
@@ -1,4 +1,8 @@
-// https://github.com/w3c/webappsec-permissions-policy/blob/main/features.md
+// Main registry: https://github.com/w3c/webappsec-permissions-policy/blob/main/features.md
+// Additional feature specs for directives not yet indexed in features.md:
+// - chUaFormFactors: https://wicg.github.io/ua-client-hints/#sec-ch-ua-form-factors
+// - subapps: https://wicg.github.io/sub-apps/
+// - webAppInstallation: https://github.com/WICG/web-app-installation
 
 export type PermissionsPolicyDirective =
   | StandardizedFeatures
@@ -19,7 +23,7 @@ type StandardizedFeatures =
   | 'chUa'
   | 'chUaArch'
   | 'chUaBitness'
-  | 'chUaFormFactors'
+  | 'chUaFormFactors' // https://wicg.github.io/ua-client-hints/#sec-ch-ua-form-factors
   | 'chUaFullVersion'
   | 'chUaFullVersionList'
   | 'chUaHighEntropyValues'
@@ -44,6 +48,7 @@ type StandardizedFeatures =
   | 'keyboardMap'
   | 'magnetometer'
   | 'mediaSession'
+  | 'mediasession' // W3C spec name: https://w3c.github.io/mediasession/#permissions-policy
   | 'microphone'
   | 'midi'
   | 'navigationOverride'
@@ -63,11 +68,20 @@ type StandardizedFeatures =
  * These features have been proposed, but the definitions have not yet been integrated into their respective specs.
  */
 type ProposedFeatures =
+  | 'autofill' // https://github.com/explainers-by-googlers/safe-text-input/blob/main/autofill.md
   | 'clipboardRead'
   | 'clipboardWrite'
+  | 'deferredFetch'
   | 'gamepad'
+  | 'languageDetector' // https://github.com/webmachinelearning/translation-api
+  | 'languageModel' // https://github.com/webmachinelearning/prompt-api
+  | 'manualText' // https://github.com/explainers-by-googlers/safe-text-input/blob/main/manual-text.md
+  | 'rewriter' // https://github.com/webmachinelearning/writing-assistance-apis
   | 'sharedAutofill'
   | 'speakerSelection'
+  | 'summarizer' // https://github.com/webmachinelearning/writing-assistance-apis
+  | 'translator' // https://github.com/webmachinelearning/translation-api
+  | 'writer' // https://github.com/webmachinelearning/writing-assistance-apis
 
 /**
  * These features generally have an explainer only, but may be available for experimentation by web developers.
@@ -77,17 +91,19 @@ type ExperimentalFeatures =
   | 'browsingTopics'
   | 'capturedSurfaceControl'
   | 'conversionMeasurement'
-  | 'deferredFetch'
+  | 'digitalCredentialsCreate' // https://w3c-fedid.github.io/digital-credentials/
   | 'digitalCredentialsGet'
   | 'focusWithoutUserActivation'
   | 'joinAdInterestGroup'
   | 'localFonts'
+  | 'monetization' // https://webmonetization.org/specification/#permissions-policy
   | 'otpCredentials'
   | 'runAdAuction'
   | 'smartCard'
-  | 'subapps'
+  | 'subapps' // https://wicg.github.io/sub-apps/
   | 'syncScript'
+  | 'tools' // https://webmachinelearning.github.io/webmcp/#permissions-policy
   | 'trustTokenRedemption'
   | 'unload'
   | 'verticalScroll'
-  | 'webAppInstallation'
+  | 'webAppInstallation' // https://github.com/WICG/web-app-installation
