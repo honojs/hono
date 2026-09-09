da433997c1d020461dc9795b7dbcf9648d5df6ba
adityajha77 <adityakartikey1259@gmail.com>
2026-09-09 23:56:39 +0530

    test(jsx): add test for cloneElement with array children

--- src/jsx/base.test.tsx
+++ src/jsx/base.test.tsx
@@ -41,6 +41,17 @@ describe('cloneElement', () => {
     expect(element.toString()).toBe('<img src="foo.png"/>')
     expect(clonedElement.toString()).toBe('<img src="foo.png" alt="bar"/>')
   })
+
+  it('should clone an element with array children', () => {
+    const element = (
+      <div>
+        <span>1</span>
+        <span>2</span>
+      </div>
+    )
+    const clonedElement = cloneElement(element, { className: 'cloned' })
+    expect(clonedElement.toString()).toBe('<div class="cloned"><span>1</span><span>2</span></div>')
+  })
 })
 
 describe('createElement', () => {
