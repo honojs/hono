fcd351bc95864c885e1eceb54bd227347f9aaba2
BlankParticle <hello@blankparticle.com>
2026-09-25 13:52:35 +0530

    chore: convert build scripts into plugins

--- build/validate-package-exports.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs'
-import { validateExports } from './validate-exports.ts'
-
-const readJsonExports = (path: string) => JSON.parse(fs.readFileSync(path, 'utf-8')).exports
-
-const [packageJsonExports, jsrJsonExports] = ['./package.json', './jsr.json'].map(readJsonExports)
-
-validateExports(packageJsonExports, jsrJsonExports, 'jsr.json')
-validateExports(jsrJsonExports, packageJsonExports, 'package.json')
